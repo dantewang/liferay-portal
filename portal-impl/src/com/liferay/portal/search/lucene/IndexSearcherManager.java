ce0bea21782467b8af40a063916ca98eada545bf
Tina Tian <tina.tian@liferay.com>
2014-03-06 11:49:54 +0800

    temp2

--- portal-impl/src/com/liferay/portal/search/lucene/IndexSearcherManager.java
+++ portal-impl/src/com/liferay/portal/search/lucene/IndexSearcherManager.java
@@ -14,7 +14,7 @@
 
 package com.liferay.portal.search.lucene;
 
-import com.liferay.portal.kernel.executor.PortalExecutorManagerUtil;
+import com.liferay.portal.kernel.concurrent.ThreadPoolExecutor;
 
 import java.io.IOException;
 
@@ -95,9 +95,6 @@ public class IndexSearcherManager {
 		_indexSearcher = null;
 
 		release(indexSearcher);
-
-		PortalExecutorManagerUtil.shutdown(
-			IndexSearcherManager.class.getName());
 	}
 
 	public synchronized void invalidate() {
@@ -112,51 +109,21 @@ public class IndexSearcherManager {
 		IndexReader indexReader = indexSearcher.getIndexReader();
 
 		indexReader.decRef();
+
+		if (indexReader.getRefCount() == 0) {
+			LiferayIndexSearcher liferayIndexSearcher =
+				(LiferayIndexSearcher)indexSearcher;
+
+			ExecutorService executorService =
+				liferayIndexSearcher.getExecutorService();
+
+			executorService.shutdown();
+		}
 	}
 
 	private IndexSearcher _createIndexSearcher(IndexReader indexReader) {
-		final ExecutorService executorService =
-			PortalExecutorManagerUtil.getPortalExecutor(
-				IndexSearcherManager.class.getName());
-
-		IndexSearcher indexSearcher = new IndexSearcher(
-			indexReader, executorService) {
-
-				@Override
-				public int docFreq(final Term term) throws IOException {
-					List<Callable<Integer>> callables =
-						new ArrayList<Callable<Integer>>();
-
-					for (int i = 0; i < subReaders.length; i++) {
-						final IndexSearcher indexSearcher = subSearchers[i];
-
-						callables.add(new Callable<Integer>() {
-
-							@Override
-							public Integer call() throws Exception {
-								return indexSearcher.docFreq(term);
-							}
-
-						});
-					}
-
-					try {
-						int docFreq = 0;
-
-						for (Future<Integer> future :
-								executorService.invokeAll(callables)) {
-
-							docFreq += future.get();
-						}
-
-						return docFreq;
-					}
-					catch (Exception e) {
-						throw new RuntimeException(e);
-					}
-				}
-
-			};
+		IndexSearcher indexSearcher = new LiferayIndexSearcher(
+			indexReader, new ThreadPoolExecutor(1, 10));
 
 		indexSearcher.setDefaultFieldSortScoring(true, false);
 		indexSearcher.setSimilarity(new FieldWeightSimilarity());
@@ -166,5 +133,57 @@ public class IndexSearcherManager {
 
 	private volatile IndexSearcher _indexSearcher;
 	private volatile boolean _invalid;
+
+	private class LiferayIndexSearcher extends IndexSearcher {
+
+		public LiferayIndexSearcher(
+			IndexReader indexReader, ExecutorService executorService) {
+
+			super(indexReader, executorService);
+
+			_executorService = executorService;
+		}
+
+		@Override
+		public int docFreq(final Term term) throws IOException {
+			List<Callable<Integer>> callables =
+				new ArrayList<Callable<Integer>>();
+
+			for (int i = 0; i < subReaders.length; i++) {
+				final IndexSearcher indexSearcher = subSearchers[i];
+
+				callables.add(new Callable<Integer>() {
+
+					@Override
+					public Integer call() throws Exception {
+						return indexSearcher.docFreq(term);
+					}
+
+				});
+			}
+
+			try {
+				int docFreq = 0;
+
+				for (Future<Integer> future :
+						_executorService.invokeAll(callables)) {
+
+					docFreq += future.get();
+				}
+
+				return docFreq;
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		public ExecutorService getExecutorService() {
+			return _executorService;
+		}
+
+		private ExecutorService _executorService;
+
+	}
 
 }
