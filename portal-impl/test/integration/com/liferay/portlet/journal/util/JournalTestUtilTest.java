805220de02fc3ef9c745bef10f6a81fe81da0813
shuyangzhou <shuyang.zhou@liferay.com>
2014-03-28 17:32:36 -0700

    temp

--- portal-impl/test/integration/com/liferay/portlet/journal/util/JournalTestUtilTest.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/util/JournalTestUtilTest.java
@@ -189,7 +189,8 @@ public class JournalTestUtilTest {
 				contents, LanguageUtil.getLanguageId(LocaleUtil.US));
 
 			String content = JournalUtil.transform(
-				null, getTokens(), Constants.VIEW, "en_US", xml, null,
+				null, getTokens(), Constants.VIEW, "en_US",
+				SAXReaderUtil.read(xml), null,
 				JournalTestUtil.getSampleTemplateXSL(),
 				TemplateConstants.LANG_TYPE_VM);
 
@@ -223,8 +224,8 @@ public class JournalTestUtilTest {
 			"name", "Joe Bloggs");
 
 		String content = JournalUtil.transform(
-			null, getTokens(), Constants.VIEW, "en_US", xml, null,
-			JournalTestUtil.getSampleTemplateXSL(),
+			null, getTokens(), Constants.VIEW, "en_US", SAXReaderUtil.read(xml),
+			null, JournalTestUtil.getSampleTemplateXSL(),
 			TemplateConstants.LANG_TYPE_VM);
 
 		Assert.assertEquals("Joe Bloggs", content);
