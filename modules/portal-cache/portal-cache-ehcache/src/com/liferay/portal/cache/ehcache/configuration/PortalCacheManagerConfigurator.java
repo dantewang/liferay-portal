465b1dcf277f67e33781a1424155e21392fb4f04
Dante Wang <dante.wang@liferay.com>
2015-09-18 16:48:37 +0800

    Add new Interface to decouple cache library configuration processing and portal cache configuration processing

---
+++ modules/portal-cache/portal-cache-ehcache/src/com/liferay/portal/cache/ehcache/configuration/PortalCacheManagerConfigurator.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cache.ehcache.configuration;
+
+import com.liferay.portal.kernel.cache.configuration.PortalCacheManagerConfiguration;
+
+/**
+ * @author Dante Wang
+ */
+public interface PortalCacheManagerConfigurator
+	<T extends CacheManagerConfigurator<?>> {
+
+	public T getCacheManagerConfigurator();
+
+	public PortalCacheManagerConfiguration getPortalCacheManagerConfiguration();
+
+}
