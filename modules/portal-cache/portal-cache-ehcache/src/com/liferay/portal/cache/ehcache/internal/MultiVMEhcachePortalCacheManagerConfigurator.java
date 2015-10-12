d034aff9b4989a0724e9092cfd47050ea7b08917
Dante Wang <dante.wang@liferay.com>
2015-10-12 17:54:44 +0800

    LPS-5 add configurator subclasses as services and update reference

---
+++ modules/portal-cache/portal-cache-ehcache/src/com/liferay/portal/cache/ehcache/internal/MultiVMEhcachePortalCacheManagerConfigurator.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cache.ehcache.internal;
+
+import com.liferay.portal.kernel.cache.PortalCacheManager;
+import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
+import com.liferay.portal.kernel.util.Props;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Dante Wang
+ */
+@Component(
+	immediate = true,
+	property = PortalCacheManager.PORTAL_CACHE_MANAGER_NAME + "=" + PortalCacheManagerNames.MULTI_VM,
+	service = EhcachePortalCacheManagerConfigurator.class
+)
+public class MultiVMEhcachePortalCacheManagerConfigurator
+	extends EhcachePortalCacheManagerConfigurator {
+
+	@Activate
+	protected void activate() {
+		super.activate();
+	}
+
+	@Reference(unbind = "-")
+	protected void setProps(Props props) {
+		this.props = props;
+	}
+
+}
