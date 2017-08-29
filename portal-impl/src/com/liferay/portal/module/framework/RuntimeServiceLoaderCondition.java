7d99b7399c2eb90722ac12e5b89b2037a76fb3a8
Dante Wang <dante.wang@liferay.com>
2017-08-29 18:51:43 +0800

    LPS-70534 Use URI and Path to handle coversion between File and URL

--- portal-impl/src/com/liferay/portal/module/framework/RuntimeServiceLoaderCondition.java
+++ portal-impl/src/com/liferay/portal/module/framework/RuntimeServiceLoaderCondition.java
@@ -18,9 +18,12 @@ import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.ServiceLoaderCondition;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.URLCodec;
 import com.liferay.portal.util.PropsValues;
 
+import java.io.File;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
 /**
@@ -41,6 +44,19 @@ public class RuntimeServiceLoaderCondition implements ServiceLoaderCondition {
 		String moduleFrameworkBaseDirName =
 			PropsValues.MODULE_FRAMEWORK_BASE_DIR.replace(
 				StringPool.DOUBLE_SLASH, StringPool.SLASH);
+
+		File moduleFrameworkBaseDirFile = new File(moduleFrameworkBaseDirName);
+
+		URI moduleFrameworkBaseDirURI = moduleFrameworkBaseDirFile.toURI();
+
+		try {
+			URL moduleFrameworkBaseDirURL = moduleFrameworkBaseDirURI.toURL();
+
+			moduleFrameworkBaseDirName = moduleFrameworkBaseDirURL.getPath();
+		}
+		catch (MalformedURLException murle) {
+			return false;
+		}
 
 		return path.contains(moduleFrameworkBaseDirName);
 	}
