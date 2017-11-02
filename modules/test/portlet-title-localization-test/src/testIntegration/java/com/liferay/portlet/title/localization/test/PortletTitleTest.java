9e73bfb68d4a4a17611bd697d72af3f10681be11
Dante Wang <dante.wang@liferay.com>
2017-11-02 17:47:09 +0800

    LPS-72054 Add a whitelist for developer to skip the test for certain portlet

--- modules/test/portlet-title-localization-test/src/testIntegration/java/com/liferay/portlet/title/localization/test/PortletTitleTest.java
+++ modules/test/portlet-title-localization-test/src/testIntegration/java/com/liferay/portlet/title/localization/test/PortletTitleTest.java
@@ -20,17 +20,24 @@ import com.liferay.portal.kernel.portlet.PortletBagPool;
 import com.liferay.portal.kernel.portlet.PortletConfigFactoryUtil;
 import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.util.ClassLoaderUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import java.io.IOException;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import javax.portlet.PortletConfig;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -45,12 +52,29 @@ public class PortletTitleTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
+	@Before
+	public void setUp() throws IOException {
+		ClassLoader contextClassloader =
+			ClassLoaderUtil.getContextClassLoader();
+
+		List<String> whitelist = new ArrayList<>();
+
+		StringUtil.readLines(
+			contextClassloader.getResourceAsStream("whitelist.txt"), whitelist);
+
+		_portletIdWhitelist.addAll(whitelist);
+	}
+
 	@Test
 	public void testPortletTitles() throws Exception {
 		List<String> portletIdsWithMissingTitles = new ArrayList<>();
 
 		for (Portlet portlet : PortletLocalServiceUtil.getPortlets()) {
 			String rootPortletId = portlet.getRootPortletId();
+
+			if (_isPortletCheckNeeded(rootPortletId)) {
+				continue;
+			}
 
 			PortletBag portletBag = PortletBagPool.get(rootPortletId);
 
@@ -74,5 +98,21 @@ public class PortletTitleTest {
 				"following portlets: ", portletIdsWithMissingTitles.toString()),
 			portletIdsWithMissingTitles.isEmpty());
 	}
+
+	private boolean _isPortletCheckNeeded(String rootPortletId) {
+		if (_portletIdWhitelist.contains(rootPortletId)) {
+			return true;
+		}
+
+		rootPortletId = StringUtil.lowerCase(rootPortletId);
+
+		if (rootPortletId.contains("test")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private final Set<String> _portletIdWhitelist = new HashSet<>();
 
 }
