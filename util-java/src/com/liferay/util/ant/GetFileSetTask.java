8409b68f0165df9859c13d136d57a342d08692ed
Lily.chi <lily.chi@liferay.com>
2017-11-22 11:21:48 +0800

    LPS-74937 Auto SF

--- util-java/src/com/liferay/util/ant/GetFileSetTask.java
+++ util-java/src/com/liferay/util/ant/GetFileSetTask.java
@@ -65,7 +65,7 @@ public class GetFileSetTask extends Task {
 				Level.WARNING, "Class files for {0} were not found!",
 				notFoundClassNames.toString());
 
-			if(classResultList.isEmpty()){
+			if (classResultList.isEmpty()) {
 				return;
 			}
 		}
@@ -157,8 +157,7 @@ public class GetFileSetTask extends Task {
 						String fileName = fileNamePath.toString();
 
 						for (String className : classNames) {
-							String targetSrcName = className.concat(
-								".java");
+							String targetSrcName = className.concat(".java");
 
 							if (_matchClassName(className, fileName)) {
 								classFileList.add(file);
