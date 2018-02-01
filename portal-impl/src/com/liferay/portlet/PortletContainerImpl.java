9c651362f6eeef84346cee5a603baed042fa53e6
leon.chi <leon.chi@liferay.com>
2018-02-01 18:11:40 +0800

    LPS-X When the redirect occur, set the action scope id to the portletURL and pass it to a event request

--- portal-impl/src/com/liferay/portlet/PortletContainerImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletContainerImpl.java
@@ -428,6 +428,14 @@ public class PortletContainerImpl implements PortletContainer {
 					portletURL.setParameter(key, value);
 				}
 
+				if (actionScopeId != null) {
+					portletURL.setParameter(
+						PortletRequest.ACTION_SCOPE_ID, actionScopeId);
+
+					request.setAttribute(
+						PortletRequest.ACTION_SCOPE_ID, actionScopeId);
+				}
+
 				redirectLocation = portletURL.toString();
 			}
 			else if (Validator.isNull(redirectLocation)) {
