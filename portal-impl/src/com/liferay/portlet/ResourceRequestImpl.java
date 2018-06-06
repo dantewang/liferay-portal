d88fe9455312482ee72d4a5238519da554226f47
Dante Wang <dante.wang@liferay.com>
2018-06-06 16:27:44 +0800

    LPS-81670 Portlet 3.0 Async: apply the interface

--- portal-impl/src/com/liferay/portlet/ResourceRequestImpl.java
+++ portal-impl/src/com/liferay/portlet/ResourceRequestImpl.java
@@ -20,6 +20,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.PublicRenderParameter;
 import com.liferay.portal.kernel.portlet.InvokerPortlet;
+import com.liferay.portal.kernel.portlet.LiferayPortletAsyncContext;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -195,7 +196,7 @@ public class ResourceRequestImpl
 	}
 
 	private String _cacheablity;
-	private PortletAsyncContext _portletAsyncContext;
+	private LiferayPortletAsyncContext _portletAsyncContext;
 	private String _resourceID;
 
 }
