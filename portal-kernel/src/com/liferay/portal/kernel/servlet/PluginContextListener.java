152285824d9ffd416a295c8b966448d036c8e736
Dante Wang <dante.wang@liferay.com>
2018-08-09 17:18:17 +0800

    LPS-84130 Register plugin class loader with servlet context name to the new pool

--- portal-kernel/src/com/liferay/portal/kernel/servlet/PluginContextListener.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/PluginContextListener.java
@@ -14,12 +14,12 @@
 
 package com.liferay.portal.kernel.servlet;
 
-import com.liferay.petra.lang.ClassLoaderPool;
 import com.liferay.portal.kernel.deploy.hot.HotDeployEvent;
 import com.liferay.portal.kernel.deploy.hot.HotDeployUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.BasePortalLifecycle;
+import com.liferay.portal.kernel.util.ServletContextClassLoaderPool;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextAttributeEvent;
@@ -110,7 +110,8 @@ public class PluginContextListener
 	public void contextDestroyed(ServletContextEvent servletContextEvent) {
 		ServletContext servletContext = servletContextEvent.getServletContext();
 
-		ClassLoaderPool.unregister(servletContext.getServletContextName());
+		ServletContextClassLoaderPool.unregister(
+			servletContext.getServletContextName());
 
 		portalDestroy();
 	}
@@ -123,7 +124,7 @@ public class PluginContextListener
 
 		pluginClassLoader = currentThread.getContextClassLoader();
 
-		ClassLoaderPool.register(
+		ServletContextClassLoaderPool.register(
 			servletContext.getServletContextName(), pluginClassLoader);
 
 		servletContext.setAttribute(PLUGIN_CLASS_LOADER, pluginClassLoader);
