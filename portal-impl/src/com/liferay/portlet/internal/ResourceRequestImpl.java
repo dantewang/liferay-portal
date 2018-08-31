76d5db597f17ceb54a20e35e3be82f933c5b3528
tina.tian <tina.tian@liferay.com>
2018-08-30 17:29:30 -0700

    LPS-81670 SF, move checking to where it is needed

--- portal-impl/src/com/liferay/portlet/internal/ResourceRequestImpl.java
+++ portal-impl/src/com/liferay/portlet/internal/ResourceRequestImpl.java
@@ -213,14 +213,6 @@ public class ResourceRequestImpl
 			throw new IllegalStateException();
 		}
 
-		boolean hasOriginalRequestAndResponse = false;
-
-		if ((resourceRequest == this) &&
-			(resourceResponse == _resourceResponse)) {
-
-			hasOriginalRequestAndResponse = true;
-		}
-
 		HttpServletRequest httpServletRequest = _getHttpServletRequest(
 			resourceRequest);
 
@@ -241,6 +233,14 @@ public class ResourceRequestImpl
 
 		if (_portletAsyncContextImpl == null) {
 			_portletAsyncContextImpl = new PortletAsyncContextImpl();
+		}
+
+		boolean hasOriginalRequestAndResponse = false;
+
+		if ((resourceRequest == this) &&
+			(resourceResponse == _resourceResponse)) {
+
+			hasOriginalRequestAndResponse = true;
 		}
 
 		_portletAsyncContextImpl.initialize(
