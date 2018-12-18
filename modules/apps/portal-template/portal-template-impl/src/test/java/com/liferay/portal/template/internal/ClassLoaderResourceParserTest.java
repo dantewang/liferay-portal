6d9f0d0f63fd8f5f88bdf66362ed6061c86405bb
Dante Wang <dante.wang@liferay.com>
2018-12-18 15:44:33 +0800

    LPS-87059 Move test

--- modules/apps/portal-template/portal-template-impl/src/test/java/com/liferay/portal/template/internal/ClassLoaderResourceParserTest.java
+++ modules/apps/portal-template/portal-template-impl/src/test/java/com/liferay/portal/template/internal/ClassLoaderResourceParserTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.template;
+package com.liferay.portal.template.internal;
 
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.test.CaptureHandler;
