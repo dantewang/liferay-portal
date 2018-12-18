f84b61564fe41b45f1b533fa5d7569e33a2159ff
Dante Wang <dante.wang@liferay.com>
2018-12-18 15:44:33 +0800

    LPS-87059 Move ClassLoaderResourceParserTest to portal-template-impl module

--- modules/apps/portal-template/portal-template-impl/src/test/java/com/liferay/portal/template/internal/ClassLoaderResourceParserTest.java
+++ modules/apps/portal-template/portal-template-impl/src/test/java/com/liferay/portal/template/internal/ClassLoaderResourceParserTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.template;
+package com.liferay.portal.template.internal;
 
 import com.liferay.petra.lang.ClassLoaderPool;
 import com.liferay.petra.string.StringBundler;
