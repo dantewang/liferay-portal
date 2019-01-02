06d7f75da8384132d8e85844dd2f7ffdc676d87f
hai.yu <8219754@liferay.com>
2019-01-02 12:30:40 +0800

    LPS-88710 Remove schedulerEngineHelper reference from AbortedMultipartUploadCleaner(AbortedMultipartUploadMessageListener)

--- modules/apps/portal-store/portal-store-s3/src/main/java/com/liferay/portal/store/s3/AbortedMultipartUploadCleaner.java
+++ modules/apps/portal-store/portal-store-s3/src/main/java/com/liferay/portal/store/s3/AbortedMultipartUploadCleaner.java
@@ -22,12 +22,14 @@ import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
-import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
 import com.liferay.portal.kernel.scheduler.SchedulerEntry;
 import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
 import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.scheduler.messaging.SchedulerEventMessageListener;
+import com.liferay.portal.kernel.scheduler.messaging.SchedulerEventMessageListenerWrapper;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -35,11 +37,13 @@ import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 import java.util.Date;
+import java.util.Dictionary;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -65,16 +69,13 @@ public class AbortedMultipartUploadCleaner {
 					"(&(", Constants.OBJECTCLASS, "=", Store.class.getName(),
 					")(store.type=", S3Store.class.getName(), "))")),
 			new S3StoreServiceTrackerCustomizer(
-				bundleContext, _schedulerEngineHelper, _triggerFactory));
+				bundleContext, _triggerFactory));
 	}
 
 	@Deactivate
 	protected void deactivate() {
 		_serviceTracker.close();
 	}
-
-	@Reference(unbind = "-")
-	private SchedulerEngineHelper _schedulerEngineHelper;
 
 	private ServiceTracker<S3Store, MessageListener> _serviceTracker;
 
@@ -136,9 +137,24 @@ public class AbortedMultipartUploadCleaner {
 			SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
 				className, trigger);
 
-			_schedulerEngineHelper.register(
-				messageListener, schedulerEntry,
-				DestinationNames.SCHEDULER_DISPATCH);
+			Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+			properties.put(
+				"destination.name", DestinationNames.SCHEDULER_DISPATCH);
+
+			SchedulerEventMessageListenerWrapper
+				schedulerEventMessageListenerWrapper =
+					new SchedulerEventMessageListenerWrapper();
+
+			schedulerEventMessageListenerWrapper.setMessageListener(
+				messageListener);
+
+			schedulerEventMessageListenerWrapper.setSchedulerEntry(
+				schedulerEntry);
+
+			_serviceRegistration = _bundleContext.registerService(
+				SchedulerEventMessageListener.class,
+				schedulerEventMessageListenerWrapper, properties);
 
 			return messageListener;
 		}
@@ -154,21 +170,19 @@ public class AbortedMultipartUploadCleaner {
 			ServiceReference<S3Store> serviceReference,
 			MessageListener messageListener) {
 
-			_schedulerEngineHelper.unregister(messageListener);
+			_serviceRegistration.unregister();
 		}
 
 		private S3StoreServiceTrackerCustomizer(
-			BundleContext bundleContext,
-			SchedulerEngineHelper schedulerEngineHelper,
-			TriggerFactory triggerFactory) {
+			BundleContext bundleContext, TriggerFactory triggerFactory) {
 
 			_bundleContext = bundleContext;
-			_schedulerEngineHelper = schedulerEngineHelper;
 			_triggerFactory = triggerFactory;
 		}
 
 		private final BundleContext _bundleContext;
-		private final SchedulerEngineHelper _schedulerEngineHelper;
+		private ServiceRegistration<SchedulerEventMessageListener>
+			_serviceRegistration;
 		private final TriggerFactory _triggerFactory;
 
 	}
