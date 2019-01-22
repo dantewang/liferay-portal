7b4c58a148153ba1f481cad4258bb5d639ec3796
hai.yu <8219754@liferay.com>
2019-01-22 13:36:25 +0800

    LPS-88710 Let Wrapper class only wraps the SchedulerEventMessageListener, using Adapter class converts messagelistener to SchedulerEventMessageListener for wrapper class use.

--- portal-kernel/src/com/liferay/portal/kernel/scheduler/messaging/SchedulerEventMessageListenerWrapper.java
+++ portal-kernel/src/com/liferay/portal/kernel/scheduler/messaging/SchedulerEventMessageListenerWrapper.java
@@ -47,7 +47,7 @@ public class SchedulerEventMessageListenerWrapper
 
 	@Override
 	public SchedulerEntry getSchedulerEntry() {
-		return _schedulerEntry;
+		return _schedulerEventMessageListener.getSchedulerEntry();
 	}
 
 	@Override
@@ -58,7 +58,9 @@ public class SchedulerEventMessageListenerWrapper
 		String groupName = message.getString(SchedulerEngine.GROUP_NAME);
 
 		if (destinationName.equals(DestinationNames.SCHEDULER_DISPATCH)) {
-			Trigger trigger = _schedulerEntry.getTrigger();
+			SchedulerEntry schedulerEntry = getSchedulerEntry();
+
+			Trigger trigger = schedulerEntry.getTrigger();
 
 			if (!jobName.equals(trigger.getJobName()) ||
 				!groupName.equals(trigger.getGroupName())) {
@@ -116,12 +118,33 @@ public class SchedulerEventMessageListenerWrapper
 		_jobName = jobName;
 	}
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public void setMessageListener(MessageListener messageListener) {
+		_schedulerEventMessageListenerAdapter.setMessageListener(
+			messageListener);
+
+		setSchedulerEventMessageListener(_schedulerEventMessageListenerAdapter);
+
 		_messageListener = messageListener;
 	}
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public void setSchedulerEntry(SchedulerEntry schedulerEntry) {
+		_schedulerEventMessageListenerAdapter.setSchedulerEntry(schedulerEntry);
+
 		_schedulerEntry = schedulerEntry;
+	}
+
+	public void setSchedulerEventMessageListener(
+		SchedulerEventMessageListener schedulerEventMessageListener) {
+
+		_schedulerEventMessageListener = schedulerEventMessageListener;
 	}
 
 	protected void handleException(Message message, Exception exception) {
@@ -138,7 +161,7 @@ public class SchedulerEventMessageListenerWrapper
 		throws MessageListenerException {
 
 		try {
-			_messageListener.receive(message);
+			_schedulerEventMessageListener.receive(message);
 		}
 		catch (Exception e) {
 			handleException(message, e);
@@ -218,7 +241,24 @@ public class SchedulerEventMessageListenerWrapper
 	private String _jobName;
 
 	private final Lock _lock = new ReentrantLock();
+
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
+	@SuppressWarnings("unused")
 	private MessageListener _messageListener;
+
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
+	@SuppressWarnings("unused")
 	private volatile SchedulerEntry _schedulerEntry;
+
+	private SchedulerEventMessageListener _schedulerEventMessageListener;
+	private final SchedulerEventMessageListenerAdapter
+		_schedulerEventMessageListenerAdapter =
+			new SchedulerEventMessageListenerAdapter();
 
 }
