7078cc3f7bcab31aa747492f9946342ff5ad34ad
Dante Wang <dante.wang@liferay.com>
2019-03-12 14:53:54 +0800

    LPS-92088 Move search integration tests from portal-impl to module

--- modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/test/BaseIndexerGetFullQueryTest.java
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/test/BaseIndexerGetFullQueryTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.kernel.search.test;
+package com.liferay.portal.search.test;
 
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.portal.kernel.search.BaseIndexer;
