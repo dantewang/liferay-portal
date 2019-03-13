c623ce7b8fbdb9bc0663044cfe1e8f50c2610fb8
hai.yu <8219754@liferay.com>
2019-03-13 15:41:18 +0800

    upgrade-test The test doesn't use service builder so it doesn't exist indexes.sql. Override the method and it can avoid NPE exception.

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/util/test/UpgradeResourceBlockTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/util/test/UpgradeResourceBlockTest.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.upgrade.util.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
@@ -23,13 +24,18 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.DBAssertionUtil;
 import com.liferay.portal.kernel.test.util.RoleTestUtil;
+import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.upgrade.util.BaseUpgradeResourceBlock;
 
+import java.io.IOException;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Types;
+
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -162,6 +168,14 @@ public class UpgradeResourceBlockTest extends BaseUpgradeResourceBlock {
 	@Override
 	protected String getClassName() {
 		return UpgradeResourceBlockTest.class.getName();
+	}
+
+	@Override
+	protected List<ObjectValuePair<String, IndexMetadata>> getIndexesSQL(
+			ClassLoader classLoader, String tableName)
+		throws IOException {
+
+		return null;
 	}
 
 	@Override
