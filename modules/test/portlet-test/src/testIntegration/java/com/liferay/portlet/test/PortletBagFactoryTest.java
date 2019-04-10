3fe944aaeb2bec3192c44c16a975a90cc5878d70
Dante Wang <dante.wang@liferay.com>
2019-04-10 17:08:59 +0800

    LPS-93724 SF, extract common test logic

--- modules/test/portlet-test/src/testIntegration/java/com/liferay/portlet/test/PortletBagFactoryTest.java
+++ modules/test/portlet-test/src/testIntegration/java/com/liferay/portlet/test/PortletBagFactoryTest.java
@@ -46,48 +46,21 @@ public class PortletBagFactoryTest {
 
 	@Test
 	public void test1() throws Exception {
-		try {
-			PortletBagFactory portletBagFactory = _createPortletBagFactory(
-				null, null, null);
-
-			portletBagFactory.create(new PortletImpl());
-
-			Assert.fail();
-		}
-		catch (IllegalStateException ise) {
-		}
+		_testValidate(null, null, null);
 	}
 
 	@Test
 	public void test2() throws Exception {
-		try {
-			Class<?> clazz = getClass();
+		Class<?> clazz = getClass();
 
-			PortletBagFactory portletBagFactory = _createPortletBagFactory(
-				clazz.getClassLoader(), null, null);
-
-			portletBagFactory.create(new PortletImpl());
-
-			Assert.fail();
-		}
-		catch (IllegalStateException ise) {
-		}
+		_testValidate(clazz.getClassLoader(), null, null);
 	}
 
 	@Test
 	public void test3() throws Exception {
-		try {
-			Class<?> clazz = getClass();
+		Class<?> clazz = getClass();
 
-			PortletBagFactory portletBagFactory = _createPortletBagFactory(
-				clazz.getClassLoader(), new MockServletContext(), null);
-
-			portletBagFactory.create(new PortletImpl());
-
-			Assert.fail();
-		}
-		catch (IllegalStateException ise) {
-		}
+		_testValidate(clazz.getClassLoader(), new MockServletContext(), null);
 	}
 
 	@Test
@@ -133,6 +106,23 @@ public class PortletBagFactoryTest {
 		}
 
 		return portletBagFactory;
+	}
+
+	private void _testValidate(
+			ClassLoader classLoader, ServletContext servletContext,
+			Boolean warFile)
+		throws Exception {
+
+		try {
+			PortletBagFactory portletBagFactory = _createPortletBagFactory(
+				classLoader, servletContext, warFile);
+
+			portletBagFactory.create(new PortletImpl());
+
+			Assert.fail();
+		}
+		catch (IllegalStateException ise) {
+		}
 	}
 
 }
