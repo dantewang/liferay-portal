c94a8d78a0ff8a1b7b1d5cd5179f47644e5d6870
Dante Wang <dante.wang@liferay.com>
2019-04-10 17:13:44 +0800

    LPS-93724 SF, simplify

--- modules/test/portlet-test/src/testIntegration/java/com/liferay/portlet/test/PortletBagFactoryTest.java
+++ modules/test/portlet-test/src/testIntegration/java/com/liferay/portlet/test/PortletBagFactoryTest.java
@@ -50,10 +50,8 @@ public class PortletBagFactoryTest {
 
 		portletImpl.setPortletApp(new PortletAppImpl(StringPool.BLANK));
 
-		Class<?> clazz = getClass();
-
 		PortletBagFactory portletBagFactory = _createPortletBagFactory(
-			clazz.getClassLoader(), new MockServletContext(), false);
+			_classLoader, new MockServletContext(), false);
 
 		portletBagFactory.create(portletImpl, new MVCPortlet(), false);
 	}
@@ -65,10 +63,8 @@ public class PortletBagFactoryTest {
 		portletImpl.setPortletApp(new PortletAppImpl(StringPool.BLANK));
 		portletImpl.setPortletClass(MVCPortlet.class.getName());
 
-		Class<?> clazz = getClass();
-
 		PortletBagFactory portletBagFactory = _createPortletBagFactory(
-			clazz.getClassLoader(), new MockServletContext(), false);
+			_classLoader, new MockServletContext(), false);
 
 		portletBagFactory.create(portletImpl);
 	}
@@ -76,11 +72,8 @@ public class PortletBagFactoryTest {
 	@Test
 	public void testValidate() throws Exception {
 		_testValidate(null, null, null);
-
-		Class<?> clazz = getClass();
-
-		_testValidate(clazz.getClassLoader(), null, null);
-		_testValidate(clazz.getClassLoader(), new MockServletContext(), null);
+		_testValidate(_classLoader, null, null);
+		_testValidate(_classLoader, new MockServletContext(), null);
 	}
 
 	private PortletBagFactory _createPortletBagFactory(
@@ -115,5 +108,8 @@ public class PortletBagFactoryTest {
 		catch (IllegalStateException ise) {
 		}
 	}
+
+	private final ClassLoader _classLoader =
+		PortletBagFactoryTest.class.getClassLoader();
 
 }
