231dce2529f481b3b95b7be9315c962d970d5a5b
Dante Wang <dante.wang@liferay.com>
2019-04-16 17:42:04 +0800

    LPS-93724 Fix test, avoid NPE during portlet deploying

--- modules/apps/portal/portal-portlet-test/src/testIntegration/java/com/liferay/portal/portlet/test/service/test/PortletPreferencesLocalServiceTest.java
+++ modules/apps/portal/portal-portlet-test/src/testIntegration/java/com/liferay/portal/portlet/test/service/test/PortletPreferencesLocalServiceTest.java
@@ -49,9 +49,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -69,6 +72,21 @@ public class PortletPreferencesLocalServiceTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
+	@BeforeClass
+	public static void setUpClass() {
+		_portletApp.setServletContext(
+			(ServletContext)ProxyUtil.newProxyInstance(
+				PortletPreferencesLocalServiceTest.class.getClassLoader(),
+				new Class<?>[] {ServletContext.class},
+				(proxy, method, args) -> {
+					if ("getServletContextName".equals(method.getName())) {
+						return StringPool.BLANK;
+					}
+
+					return null;
+				}));
+	}
+
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
@@ -79,12 +97,12 @@ public class PortletPreferencesLocalServiceTest {
 
 		_portlet = PortletLocalServiceUtil.getPortletById(
 			_layout.getCompanyId(), String.valueOf(_PORTLET_ID));
+
+		_portlet.setPortletApp(_portletApp);
 	}
 
 	@After
 	public void tearDown() {
-		_portlet.setPortletApp(_portletApp);
-
 		PortletLocalServiceUtil.destroyPortlet(_portlet);
 	}
 
