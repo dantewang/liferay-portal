36d2d06f61f2b63ccdc8c7b3edbf9efff96b883c
hai.yu <8219754@liferay.com>
2019-04-23 10:52:01 +0800

    portal-service-test Move GroupServiceTest of the com.liferay.portal.service package to site-test module

--- modules/apps/site/site-test/src/testIntegration/java/com/liferay/site/service/test/GroupServiceTest.java
+++ modules/apps/site/site-test/src/testIntegration/java/com/liferay/site/service/test/GroupServiceTest.java
@@ -12,8 +12,9 @@
  * details.
  */
 
-package com.liferay.portal.service;
+package com.liferay.site.service.test;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
@@ -35,10 +36,12 @@ import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Drew Brokke
  */
+@RunWith(Arquillian.class)
 public class GroupServiceTest {
 
 	@ClassRule
