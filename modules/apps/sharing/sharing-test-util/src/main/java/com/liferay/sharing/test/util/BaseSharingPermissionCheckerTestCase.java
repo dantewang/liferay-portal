1f0db9b17edf2208b5673eda1e7f6fd28e65061a
Dante Wang <dante.wang@liferay.com>
2019-06-21 16:40:11 +0800

    Revert "LPS-96736 Making permission checker test generic and using in dl"
    This reverts commit 69d47e64

--- modules/apps/sharing/sharing-test-util/src/main/java/com/liferay/sharing/test/util/BaseSharingPermissionCheckerTestCase.java
+++ modules/apps/sharing/sharing-test-util/src/main/java/com/liferay/sharing/test/util/BaseSharingPermissionCheckerTestCase.java
@@ -14,10 +14,13 @@
 
 package com.liferay.sharing.test.util;
 
+import com.liferay.document.library.kernel.model.DLFileEntryConstants;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.ClassedModel;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
@@ -25,18 +28,22 @@ import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.RoleConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.context.ContextUserReplace;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -54,8 +61,7 @@ import org.junit.Test;
 /**
  * @author Sergio González
  */
-public abstract class BaseSharingPermissionCheckerTestCase
-	<T extends ClassedModel> {
+public abstract class BaseSharingPermissionCheckerTestCase {
 
 	@ClassRule
 	@Rule
@@ -77,10 +83,22 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		_groupUser = UserTestUtil.addGroupUser(
 			_group, RoleConstants.POWER_USER);
 
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), _user.getUserId());
+
+		serviceContext.setAddGuestPermissions(false);
+		serviceContext.setAddGroupPermissions(false);
+
+		_fileEntry = _dlAppLocalService.addFileEntry(
+			_user.getUserId(), _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			StringUtil.randomString(), "text/plain", StringUtil.randomString(),
+			StringUtil.randomString(), StringPool.BLANK, "test".getBytes(),
+			serviceContext);
+
 		_powerUserRole = _roleLocalService.getRole(
 			_company.getCompanyId(), RoleConstants.POWER_USER);
-
-		_sharingPermissionChecker = getSharingPermissionChecker();
 	}
 
 	@Test
@@ -91,12 +109,10 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_user, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertTrue(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.ADD_DISCUSSION)));
 		}
 	}
@@ -109,12 +125,10 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_user, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertTrue(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.UPDATE)));
 		}
 	}
@@ -127,12 +141,10 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_user, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertTrue(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.VIEW)));
 		}
 	}
@@ -144,18 +156,16 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(_groupUser);
 
-		try (AddModelResourcePermission addModelResourcePermission =
-				new AddModelResourcePermission(
+		try (AddDLFileEntryResourcePermission addDLFileEntryResourcePermission =
+				new AddDLFileEntryResourcePermission(
 					_powerUserRole, ActionKeys.ADD_DISCUSSION);
 			ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertFalse(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.UPDATE)));
 		}
 	}
@@ -167,18 +177,16 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(_groupUser);
 
-		try (AddModelResourcePermission addModelResourcePermission =
-				new AddModelResourcePermission(
+		try (AddDLFileEntryResourcePermission addDLFileEntryResourcePermission =
+				new AddDLFileEntryResourcePermission(
 					_powerUserRole, ActionKeys.ADD_DISCUSSION);
 			ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertFalse(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.VIEW)));
 		}
 	}
@@ -190,18 +198,16 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(_groupUser);
 
-		try (AddModelResourcePermission addModelResourcePermission =
-				new AddModelResourcePermission(
+		try (AddDLFileEntryResourcePermission addDLFileEntryResourcePermission =
+				new AddDLFileEntryResourcePermission(
 					_powerUserRole, ActionKeys.ADD_DISCUSSION);
 			ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertTrue(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.ADD_DISCUSSION)));
 		}
 	}
@@ -216,12 +222,10 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertFalse(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.ADD_DISCUSSION)));
 		}
 	}
@@ -236,12 +240,10 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertFalse(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.UPDATE)));
 		}
 	}
@@ -256,12 +258,10 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertFalse(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.VIEW)));
 		}
 	}
@@ -273,18 +273,16 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(_groupUser);
 
-		try (AddModelResourcePermission addModelResourcePermission =
-				new AddModelResourcePermission(
+		try (AddDLFileEntryResourcePermission addDLFileEntryResourcePermission =
+				new AddDLFileEntryResourcePermission(
 					_powerUserRole, ActionKeys.UPDATE);
 			ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertFalse(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.ADD_DISCUSSION)));
 		}
 	}
@@ -296,18 +294,16 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(_groupUser);
 
-		try (AddModelResourcePermission addModelResourcePermission =
-				new AddModelResourcePermission(
+		try (AddDLFileEntryResourcePermission addDLFileEntryResourcePermission =
+				new AddDLFileEntryResourcePermission(
 					_powerUserRole, ActionKeys.UPDATE);
 			ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertFalse(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.VIEW)));
 		}
 	}
@@ -319,18 +315,16 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(_groupUser);
 
-		try (AddModelResourcePermission addModelResourcePermission =
-				new AddModelResourcePermission(
+		try (AddDLFileEntryResourcePermission addDLFileEntryResourcePermission =
+				new AddDLFileEntryResourcePermission(
 					_powerUserRole, ActionKeys.UPDATE);
 			ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertTrue(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.UPDATE)));
 		}
 	}
@@ -342,17 +336,16 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(_groupUser);
 
-		try (AddModelResourcePermission addModelResourcePermission =
-				new AddModelResourcePermission(_powerUserRole, ActionKeys.VIEW);
+		try (AddDLFileEntryResourcePermission addDLFileEntryResourcePermission =
+				new AddDLFileEntryResourcePermission(
+					_powerUserRole, ActionKeys.VIEW);
 			ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertFalse(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.ADD_DISCUSSION)));
 		}
 	}
@@ -364,17 +357,16 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(_groupUser);
 
-		try (AddModelResourcePermission addModelResourcePermission =
-				new AddModelResourcePermission(_powerUserRole, ActionKeys.VIEW);
+		try (AddDLFileEntryResourcePermission addDLFileEntryResourcePermission =
+				new AddDLFileEntryResourcePermission(
+					_powerUserRole, ActionKeys.VIEW);
 			ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertFalse(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.UPDATE)));
 		}
 	}
@@ -384,27 +376,19 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(_groupUser);
 
-		try (AddModelResourcePermission addModelResourcePermission =
-				new AddModelResourcePermission(_powerUserRole, ActionKeys.VIEW);
+		try (AddDLFileEntryResourcePermission addDLFileEntryResourcePermission =
+				new AddDLFileEntryResourcePermission(
+					_powerUserRole, ActionKeys.VIEW);
 			ContextUserReplace contextUserReplace = new ContextUserReplace(
 				_groupUser, permissionChecker)) {
 
-			T model = getModel(_user, _group);
-
 			Assert.assertTrue(
 				_sharingPermissionChecker.hasPermission(
-					permissionChecker, (Long)model.getPrimaryKeyObj(),
-					_group.getGroupId(),
+					permissionChecker, _fileEntry.getFileEntryId(),
+					_fileEntry.getGroupId(),
 					Arrays.asList(SharingEntryAction.VIEW)));
 		}
 	}
-
-	protected abstract String getClassName();
-
-	protected abstract T getModel(User user, Group group)
-		throws PortalException;
-
-	protected abstract SharingPermissionChecker getSharingPermissionChecker();
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		BaseSharingPermissionCheckerTestCase.class);
@@ -415,6 +399,10 @@ public abstract class BaseSharingPermissionCheckerTestCase
 	@DeleteAfterTestRun
 	private Company _company;
 
+	@Inject
+	private DLAppLocalService _dlAppLocalService;
+
+	private FileEntry _fileEntry;
 	private Group _group;
 	private User _groupUser;
 	private Role _powerUserRole;
@@ -425,19 +413,26 @@ public abstract class BaseSharingPermissionCheckerTestCase
 	@Inject
 	private RoleLocalService _roleLocalService;
 
+	@Inject(
+		filter = "model.class.name=com.liferay.document.library.kernel.model.DLFileEntry"
+	)
 	private SharingPermissionChecker _sharingPermissionChecker;
+
 	private User _user;
 
-	private class AddModelResourcePermission implements AutoCloseable {
+	private class AddDLFileEntryResourcePermission implements AutoCloseable {
 
-		public AddModelResourcePermission(Role role, String... actionKeys) {
+		public AddDLFileEntryResourcePermission(
+			Role role, String... actionKeys) {
+
 			_role = role;
 			_actionKeys = actionKeys;
 
 			for (String actionKey : actionKeys) {
 				try {
 					_resourcePermissionLocalService.addResourcePermission(
-						role.getCompanyId(), getClassName(),
+						role.getCompanyId(),
+						DLFileEntryConstants.getClassName(),
 						ResourceConstants.SCOPE_GROUP_TEMPLATE,
 						String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
 						role.getRoleId(), actionKey);
@@ -452,7 +447,7 @@ public abstract class BaseSharingPermissionCheckerTestCase
 		public void close() throws Exception {
 			for (String actionKey : _actionKeys) {
 				_resourcePermissionLocalService.removeResourcePermission(
-					_role.getCompanyId(), getClassName(),
+					_role.getCompanyId(), DLFileEntryConstants.getClassName(),
 					ResourceConstants.SCOPE_GROUP_TEMPLATE,
 					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
 					_role.getRoleId(), actionKey);
