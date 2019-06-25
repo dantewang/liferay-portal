4f37287b6f8c0cab904c2ebb7c91c83dbc1e8d4d
tina.tian <tina.tian@liferay.com>
2019-06-24 23:52:26 -0700

    LPS-97259 Clean up

--- modules/apps/portal-template/portal-template-soy-renderer-impl/src/main/java/com/liferay/portal/template/soy/renderer/internal/SoyRendererImpl.java
+++ modules/apps/portal-template/portal-template-soy-renderer-impl/src/main/java/com/liferay/portal/template/soy/renderer/internal/SoyRendererImpl.java
@@ -18,9 +18,7 @@ import com.liferay.portal.kernel.template.Template;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.template.TemplateException;
 import com.liferay.portal.kernel.template.TemplateManager;
-import com.liferay.portal.template.soy.SoyTemplateResourceFactory;
 import com.liferay.portal.template.soy.renderer.SoyRenderer;
-import com.liferay.portal.template.soy.util.SoyTemplateResourcesProvider;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -96,12 +94,6 @@ public class SoyRendererImpl implements SoyRenderer {
 
 		return _templateManager.getTemplate(null, false);
 	}
-
-	@Reference
-	private SoyTemplateResourceFactory _soyTemplateResourceFactory;
-
-	@Reference
-	private SoyTemplateResourcesProvider _soyTemplateResourcesProvider;
 
 	private volatile TemplateManager _templateManager;
 
