ff609c5f9863280c615b41d3760b33d41ca6de95
Dante Wang <dante.wang@liferay.com>
2019-08-20 16:47:46 +0800

    LPD-36036 Clean up, remove other unused

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspCompiler.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspCompiler.java
@@ -57,7 +57,6 @@ import org.apache.jasper.JspCompilationContext;
 import org.apache.jasper.Options;
 import org.apache.jasper.compiler.ErrorDispatcher;
 import org.apache.jasper.compiler.JavacErrorDetail;
-import org.apache.jasper.compiler.JspRuntimeContext;
 import org.apache.jasper.compiler.Node;
 
 import org.osgi.framework.Bundle;
@@ -154,14 +153,9 @@ public class JspCompiler {
 		return javacErrorDetails;
 	}
 
-	public long getClassLastModified() {
-		return _jspRuntimeContext.getBytecodeBirthTime(
-			_jspCompilationContext.getFullClassName());
-	}
-
 	public void init(
 		JspCompilationContext jspCompilationContext,
-		ErrorDispatcher errorDispatcher, boolean suppressLogging) {
+		ErrorDispatcher errorDispatcher) {
 
 		_compilerOptions.add("-XDuseUnsharedTable");
 
@@ -266,12 +260,6 @@ public class JspCompiler {
 		_jspCompilationContext = jspCompilationContext;
 
 		_errorDispatcher = errorDispatcher;
-
-		_jspRuntimeContext = jspCompilationContext.getRuntimeContext();
-	}
-
-	public void release() {
-		_bytecodeJavaFileObjects = null;
 	}
 
 	public void saveClassFile(String className, String classFileName) {
@@ -307,9 +295,6 @@ public class JspCompiler {
 				servletContext.log("Unable to save class file", ioException);
 			}
 		}
-	}
-
-	public void setClassPath(List<File> classPath) {
 	}
 
 	private static Set<String> _collectPackageNames(BundleWiring bundleWiring) {
@@ -535,6 +520,5 @@ public class JspCompiler {
 	private final List<JavaFileObjectResolver> _javaFileObjectResolvers =
 		new ArrayList<>();
 	private JspCompilationContext _jspCompilationContext;
-	private JspRuntimeContext _jspRuntimeContext;
 
 }
