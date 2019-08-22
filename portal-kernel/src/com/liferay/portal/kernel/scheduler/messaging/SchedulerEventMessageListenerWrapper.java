60a722ccdafa96181fce64ad6e8f785925fe7555
hai.yu <8219754@liferay.com>
2019-08-22 12:03:21 +0800

    LPS-88710 Copy SchedulerEventMessageListenerWrapper to portal-scheduler and deprecate the original one.

--- portal-kernel/src/com/liferay/portal/kernel/scheduler/messaging/SchedulerEventMessageListenerWrapper.java
+++ portal-kernel/src/com/liferay/portal/kernel/scheduler/messaging/SchedulerEventMessageListenerWrapper.java
@@ -14,34 +14,22 @@
 
 package com.liferay.portal.kernel.scheduler.messaging;
 
-import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.messaging.MessageBusUtil;
 import com.liferay.portal.kernel.messaging.MessageListener;
 import com.liferay.portal.kernel.messaging.MessageListenerException;
 import com.liferay.portal.kernel.scheduler.JobState;
 import com.liferay.portal.kernel.scheduler.SchedulerEngine;
-import com.liferay.portal.kernel.scheduler.SchedulerEngineHelperUtil;
 import com.liferay.portal.kernel.scheduler.SchedulerEntry;
-import com.liferay.portal.kernel.scheduler.SchedulerException;
-import com.liferay.portal.kernel.scheduler.StorageType;
-import com.liferay.portal.kernel.scheduler.Trigger;
-import com.liferay.portal.kernel.scheduler.TriggerState;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.PropsUtil;
 
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of Athanasius (7.3.x), replaced by {@link
+ * com.liferay.portal.scheduler.internal.messaging.
+ * SchedulerEventMessageListenerWrapper}
  */
+@Deprecated
 public class SchedulerEventMessageListenerWrapper
 	implements SchedulerEventMessageListener {
 
@@ -52,54 +40,7 @@ public class SchedulerEventMessageListenerWrapper
 
 	@Override
 	public void receive(Message message) throws MessageListenerException {
-		String destinationName = GetterUtil.getString(
-			message.getString(SchedulerEngine.DESTINATION_NAME));
-		String jobName = message.getString(SchedulerEngine.JOB_NAME);
-		String groupName = message.getString(SchedulerEngine.GROUP_NAME);
-
-		if (destinationName.equals(DestinationNames.SCHEDULER_DISPATCH)) {
-			Trigger trigger = _schedulerEntry.getTrigger();
-
-			if (!jobName.equals(trigger.getJobName()) ||
-				!groupName.equals(trigger.getGroupName())) {
-
-				return;
-			}
-		}
-
-		if (_SCHEDULER_EVENT_MESSAGE_LISTENER_LOCK_TIMEOUT <= 0) {
-			_lock.lock();
-		}
-		else {
-			try {
-				if (!_lock.tryLock(
-						_SCHEDULER_EVENT_MESSAGE_LISTENER_LOCK_TIMEOUT,
-						TimeUnit.MILLISECONDS)) {
-
-					MessageBusUtil.sendMessage(destinationName, message);
-
-					return;
-				}
-			}
-			catch (InterruptedException ie) {
-				if (_log.isInfoEnabled()) {
-					_log.info(
-						"Unable to wait " +
-							_SCHEDULER_EVENT_MESSAGE_LISTENER_LOCK_TIMEOUT +
-								" milliseconds before retry",
-						ie);
-				}
-
-				return;
-			}
-		}
-
-		try {
-			_processMessage(message, destinationName, jobName, groupName);
-		}
-		finally {
-			_lock.unlock();
-		}
+		_messageListener.receive(message);
 	}
 
 	public void setMessageListener(MessageListener messageListener) {
@@ -118,78 +59,6 @@ public class SchedulerEventMessageListenerWrapper
 		}
 	}
 
-	private void _processMessage(
-			Message message, String destinationName, String jobName,
-			String groupName)
-		throws MessageListenerException {
-
-		try {
-			_messageListener.receive(message);
-		}
-		catch (Exception e) {
-			handleException(message, e);
-
-			if (e instanceof MessageListenerException) {
-				throw (MessageListenerException)e;
-			}
-
-			throw new MessageListenerException(e);
-		}
-		finally {
-			TriggerState triggerState = null;
-
-			if (message.getBoolean(SchedulerEngine.DISABLE)) {
-				triggerState = TriggerState.COMPLETE;
-
-				if (destinationName.equals(
-						DestinationNames.SCHEDULER_DISPATCH)) {
-
-					MessageBusUtil.unregisterMessageListener(
-						destinationName, this);
-				}
-
-				StorageType storageType = (StorageType)message.get(
-					SchedulerEngine.STORAGE_TYPE);
-
-				try {
-					SchedulerEngineHelperUtil.delete(
-						jobName, groupName, storageType);
-				}
-				catch (SchedulerException se) {
-					if (_log.isInfoEnabled()) {
-						_log.info(
-							StringBundler.concat(
-								"Unable to delete job ", jobName, " in group ",
-								groupName),
-							se);
-					}
-				}
-			}
-			else {
-				triggerState = TriggerState.NORMAL;
-			}
-
-			try {
-				SchedulerEngineHelperUtil.auditSchedulerJobs(
-					message, triggerState);
-			}
-			catch (Exception e) {
-				if (_log.isInfoEnabled()) {
-					_log.info("Unable to send audit message", e);
-				}
-			}
-		}
-	}
-
-	private static final int _SCHEDULER_EVENT_MESSAGE_LISTENER_LOCK_TIMEOUT =
-		GetterUtil.getInteger(
-			PropsUtil.get(
-				PropsKeys.SCHEDULER_EVENT_MESSAGE_LISTENER_LOCK_TIMEOUT));
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		SchedulerEventMessageListenerWrapper.class);
-
-	private final Lock _lock = new ReentrantLock();
 	private MessageListener _messageListener;
 	private volatile SchedulerEntry _schedulerEntry;
 
