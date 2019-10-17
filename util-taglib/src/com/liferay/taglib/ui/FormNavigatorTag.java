dcc9dea0fd0aa4b852d1b10d6beaa797f4695970
hai.yu <8219754@liferay.com>
2019-10-17 14:01:54 +0800

    LPS-103269 inline

--- util-taglib/src/com/liferay/taglib/ui/FormNavigatorTag.java
+++ util-taglib/src/com/liferay/taglib/ui/FormNavigatorTag.java
@@ -189,10 +189,6 @@ public class FormNavigatorTag extends IncludeTag {
 		return categorySectionLabels;
 	}
 
-	protected String[] getDeprecatedCategorySections() {
-		return new String[0];
-	}
-
 	@Override
 	protected String getPage() {
 		if (Validator.isNotNull(_markupView)) {
@@ -219,7 +215,7 @@ public class FormNavigatorTag extends IncludeTag {
 			getCategorySectionLabels());
 		httpServletRequest.setAttribute(
 			"liferay-ui:form-navigator:deprecatedCategorySections",
-			getDeprecatedCategorySections());
+			new String[0]);
 		httpServletRequest.setAttribute(
 			"liferay-ui:form-navigator:displayStyle", _displayStyle);
 		httpServletRequest.setAttribute(
