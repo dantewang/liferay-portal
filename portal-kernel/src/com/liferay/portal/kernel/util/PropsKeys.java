76b7ba1bf008a507d5fcf9010fdbdcb424edea71
hai.yu <8219754@liferay.com>
2019-11-05 17:41:55 +0800

    LPS-103869 Remove deprecated properties "search.container.show.pagination.bottom", "search.container.show.pagination.top", "search.container.show.pagination.top.delta"

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -2704,27 +2704,6 @@ public interface PropsKeys {
 	public static final String SEARCH_CONTAINER_PAGE_ITERATOR_MAX_PAGES =
 		"search.container.page.iterator.max.pages";
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static final String SEARCH_CONTAINER_SHOW_PAGINATION_BOTTOM =
-		"search.container.show.pagination.bottom";
-
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static final String SEARCH_CONTAINER_SHOW_PAGINATION_TOP =
-		"search.container.show.pagination.top";
-
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static final String SEARCH_CONTAINER_SHOW_PAGINATION_TOP_DELTA =
-		"search.container.show.pagination.top.delta";
-
 	public static final String SERVLET_CONTEXT_CLASS_LOADER_POOL_FALLBACK =
 		"servlet.context.class.loader.pool.fallback";
 
