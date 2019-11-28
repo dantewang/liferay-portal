627ee62ad388cbd9badc689e7b6599ff57046ea9
lily.chi <lily.chi@liferay.com>
2019-11-28 14:29:29 +0800

    LPS-74148 Extract create Journal's DDMStructureLayoutModel logic, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -1140,10 +1140,9 @@ public class DataFactory {
 		_defaultJournalDDMStructureVersionModel = newDDMStructureVersionModel(
 			_defaultJournalDDMStructureModel);
 
-		_defaultJournalDDMStructureLayoutModel = newDDMStructureLayoutModel(
-			_globalGroupId, _defaultUserId,
-			_defaultJournalDDMStructureVersionModel.getStructureVersionId(),
-			_journalDDMStructureLayoutContent);
+		_defaultJournalDDMStructureLayoutModel =
+			newDefaultJournalDDMStructureLayoutModel(
+				_defaultJournalDDMStructureVersionModel);
 
 		_defaultJournalDDMTemplateModel = newDDMTemplateModel(
 			_globalGroupId, _defaultUserId,
@@ -1939,6 +1938,15 @@ public class DataFactory {
 		return newDDMStructureModel(
 			_globalGroupId, _defaultUserId, getClassNameId(DLFileEntry.class),
 			RawMetadataProcessor.TIKA_RAW_METADATA, _dlDDMStructureContent);
+	}
+
+	public DDMStructureLayoutModel newDefaultJournalDDMStructureLayoutModel(
+		DDMStructureVersionModel defaultJournalDDMStructureVersionModel) {
+
+		return newDDMStructureLayoutModel(
+			_globalGroupId, _defaultUserId,
+			defaultJournalDDMStructureVersionModel.getStructureVersionId(),
+			_journalDDMStructureLayoutContent);
 	}
 
 	public DDMStructureModel newDefaultJournalDDMStructureModel() {
