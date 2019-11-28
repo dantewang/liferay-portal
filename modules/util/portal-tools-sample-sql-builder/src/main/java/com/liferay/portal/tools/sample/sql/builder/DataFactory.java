7e392efdae7ca1aa079be70a3ab5afce52b29c05
lily.chi <lily.chi@liferay.com>
2019-11-28 14:41:53 +0800

    LPS-74148 Transfer parameter (structureKey) from ftl

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -2285,7 +2285,7 @@ public class DataFactory {
 
 	public JournalArticleModel newJournalArticleModel(
 			JournalArticleResourceModel journalArticleResourceModel,
-			int articleIndex, int versionIndex)
+			int articleIndex, int versionIndex, String ddmStructureKey)
 		throws PortalException {
 
 		JournalArticleModel journalArticleModel = new JournalArticleModelImpl();
@@ -2319,8 +2319,7 @@ public class DataFactory {
 
 		journalArticleModel.setContent(initJournalArticleContent());
 		journalArticleModel.setDefaultLanguageId("en_US");
-		journalArticleModel.setDDMStructureKey(
-			_defaultJournalDDMStructureModel.getStructureKey());
+		journalArticleModel.setDDMStructureKey(ddmStructureKey);
 		journalArticleModel.setDDMTemplateKey(
 			_defaultJournalDDMTemplateModel.getTemplateKey());
 		journalArticleModel.setDisplayDate(new Date());
