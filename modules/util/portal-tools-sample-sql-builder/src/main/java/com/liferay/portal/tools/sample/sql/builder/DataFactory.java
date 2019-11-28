7f9af6fe156ff1208bbd04251999fe589e6cad1e
lily.chi <lily.chi@liferay.com>
2019-11-28 14:56:51 +0800

    LPS-74148 Make these ddm models about Journal generation consistent with other models

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -369,7 +369,6 @@ public class DataFactory {
 		initCommerceCatalogModel();
 		initCommerceChannelModel();
 		initCommerceProductModels();
-		initDLFileEntryTypeModel();
 		initGroupModels();
 
 		initRoleModels();
@@ -594,28 +593,6 @@ public class DataFactory {
 		}
 
 		return writer;
-	}
-
-	public DDMStructureLayoutModel getDefaultJournalDDMStructureLayoutModel() {
-		return _defaultJournalDDMStructureLayoutModel;
-	}
-
-	public DDMStructureModel getDefaultJournalDDMStructureModel() {
-		return _defaultJournalDDMStructureModel;
-	}
-
-	public DDMStructureVersionModel
-		getDefaultJournalDDMStructureVersionModel() {
-
-		return _defaultJournalDDMStructureVersionModel;
-	}
-
-	public DDMTemplateModel getDefaultJournalDDMTemplateModel() {
-		return _defaultJournalDDMTemplateModel;
-	}
-
-	public DDMTemplateVersionModel getDefaultJournalDDMTemplateVersionModel() {
-		return _defaultJournalDDMTemplateVersionModel;
 	}
 
 	public long getDLFileEntryClassNameId() {
@@ -1132,23 +1109,6 @@ public class DataFactory {
 
 				});
 		}
-	}
-
-	public void initDLFileEntryTypeModel() {
-		_defaultJournalDDMStructureModel = newDefaultJournalDDMStructureModel();
-
-		_defaultJournalDDMStructureVersionModel = newDDMStructureVersionModel(
-			_defaultJournalDDMStructureModel);
-
-		_defaultJournalDDMStructureLayoutModel =
-			newDefaultJournalDDMStructureLayoutModel(
-				_defaultJournalDDMStructureVersionModel);
-
-		_defaultJournalDDMTemplateModel = newDefaultJournalDDMTemplateModel(
-			_defaultJournalDDMStructureModel);
-
-		_defaultJournalDDMTemplateVersionModel = newDDMTemplateVersionModel(
-			_defaultJournalDDMTemplateModel);
 	}
 
 	public void initGroupModels() {
@@ -4511,11 +4471,6 @@ public class DataFactory {
 		_defaultAssetPublisherPortletPreferencesImpl;
 	private AssetVocabularyModel _defaultAssetVocabularyModel;
 	private String _defaultJournalArticleId;
-	private DDMStructureLayoutModel _defaultJournalDDMStructureLayoutModel;
-	private DDMStructureModel _defaultJournalDDMStructureModel;
-	private DDMStructureVersionModel _defaultJournalDDMStructureVersionModel;
-	private DDMTemplateModel _defaultJournalDDMTemplateModel;
-	private DDMTemplateVersionModel _defaultJournalDDMTemplateVersionModel;
 	private final long _defaultUserId;
 	private final String _dlDDMStructureContent;
 	private final String _dlDDMStructureLayoutContent;
