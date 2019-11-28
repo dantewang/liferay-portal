06b4399ec464d47ccc9edb9587125f86f1e16b7a
lily.chi <lily.chi@liferay.com>
2019-11-28 14:09:43 +0800

    LPS-74148 Make DLFileEntryTypeModel generation consistent with other models

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -613,10 +613,6 @@ public class DataFactory {
 		return _defaultDLDDMStructureVersionModel;
 	}
 
-	public DLFileEntryTypeModel getDefaultDLFileEntryTypeModel() {
-		return _defaultDLFileEntryTypeModel;
-	}
-
 	public DDMStructureLayoutModel getDefaultJournalDDMStructureLayoutModel() {
 		return _defaultJournalDDMStructureLayoutModel;
 	}
@@ -1156,8 +1152,6 @@ public class DataFactory {
 	}
 
 	public void initDLFileEntryTypeModel() {
-		_defaultDLFileEntryTypeModel = newDLFileEntryTypeModel();
-
 		_defaultDLDDMStructureModel = newDDMStructureModel(
 			_globalGroupId, _defaultUserId, getClassNameId(DLFileEntry.class),
 			RawMetadataProcessor.TIKA_RAW_METADATA, _dlDDMStructureContent);
@@ -2058,12 +2052,15 @@ public class DataFactory {
 	}
 
 	public List<DLFolderModel> newDLFolderModels(
-		long groupId, long parentFolderId) {
+		long groupId, long parentFolderId,
+		DLFileEntryTypeModel defaultDLFileEntryTypeModel) {
 
 		List<DLFolderModel> dlFolderModels = new ArrayList<>(_maxDLFolderCount);
 
 		for (int i = 1; i <= _maxDLFolderCount; i++) {
-			dlFolderModels.add(newDLFolderModel(groupId, parentFolderId, i));
+			dlFolderModels.add(
+				newDLFolderModel(
+					groupId, parentFolderId, i, defaultDLFileEntryTypeModel));
 		}
 
 		return dlFolderModels;
@@ -3941,7 +3938,8 @@ public class DataFactory {
 	}
 
 	protected DLFolderModel newDLFolderModel(
-		long groupId, long parentFolderId, int index) {
+		long groupId, long parentFolderId, int index,
+		DLFileEntryTypeModel defaultDLFileEntryTypeModel) {
 
 		DLFolderModel dlFolderModel = new DLFolderModelImpl();
 
@@ -3958,7 +3956,7 @@ public class DataFactory {
 		dlFolderModel.setName("Test Folder " + index);
 		dlFolderModel.setLastPostDate(nextFutureDate());
 		dlFolderModel.setDefaultFileEntryTypeId(
-			_defaultDLFileEntryTypeModel.getFileEntryTypeId());
+			defaultDLFileEntryTypeModel.getFileEntryTypeId());
 		dlFolderModel.setLastPublishDate(nextFutureDate());
 		dlFolderModel.setStatusDate(nextFutureDate());
 
@@ -4500,7 +4498,6 @@ public class DataFactory {
 	private DDMStructureLayoutModel _defaultDLDDMStructureLayoutModel;
 	private DDMStructureModel _defaultDLDDMStructureModel;
 	private DDMStructureVersionModel _defaultDLDDMStructureVersionModel;
-	private DLFileEntryTypeModel _defaultDLFileEntryTypeModel;
 	private String _defaultJournalArticleId;
 	private DDMStructureLayoutModel _defaultJournalDDMStructureLayoutModel;
 	private DDMStructureModel _defaultJournalDDMStructureModel;
