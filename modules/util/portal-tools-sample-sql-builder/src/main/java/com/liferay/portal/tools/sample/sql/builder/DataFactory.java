0b1f42e0f2249fa87bac7d98e5cfdd0b40d2eaf7
lily.chi <lily.chi@liferay.com>
2019-11-28 15:51:14 +0800

    LPS-74148 Extract create CommerceChannelGroupModel logic, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -1133,10 +1133,8 @@ public class DataFactory {
 	}
 
 	public void initGroupModels() {
-		_commerceChannelGroupModel = newGroupModel(
-			_commerceChannelGroupId, getClassNameId(CommerceChannel.class),
-			_commerceChannelModel.getCommerceChannelId(),
-			_commerceChannelModel.getName(), false);
+		_commerceChannelGroupModel = newCommerceChannelGroupModel(
+			_commerceChannelModel);
 
 		_commerceCatalogGroupModel = newGroupModel(
 			_commerceCatalogGroupId, getClassNameId(CommerceCatalog.class),
@@ -1418,6 +1416,15 @@ public class DataFactory {
 		blogsStatsUserModel.setLastPostDate(new Date());
 
 		return blogsStatsUserModel;
+	}
+
+	public GroupModel newCommerceChannelGroupModel(
+		CommerceChannelModel commerceChannelModel) {
+
+		return newGroupModel(
+			_commerceChannelGroupId, getClassNameId(CommerceChannel.class),
+			commerceChannelModel.getCommerceChannelId(),
+			commerceChannelModel.getName(), false);
 	}
 
 	public CompanyModel newCompanyModel() {
