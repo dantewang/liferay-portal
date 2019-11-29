dantewang/liferay-portal

commit cbb83b05a86cf9d5788925971ea2582050e1b9d8

LPS-96599 Use a single query to set version rather than iterating over every row.

modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/upgrade/v1_1_0/UpgradeDDLRecord.java
@@ -15,11 +15,7 @@
 package com.liferay.dynamic.data.lists.internal.upgrade.v1_1_0;
 
 import com.liferay.dynamic.data.lists.model.DDLRecordSetConstants;
-import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 /**
  * @author Pedro Queiroz
@@ -28,25 +24,9 @@ public class UpgradeDDLRecord extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		try (PreparedStatement ps1 = connection.prepareStatement(
-				"select recordId from DDLRecord");
-			PreparedStatement ps2 =
-				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
-					connection,
-					"update DDLRecord set recordSetVersion = ? where " +
-						"recordId = ?")) {
-
-			try (ResultSet rs = ps1.executeQuery()) {
-				while (rs.next()) {
-					ps2.setString(1, DDLRecordSetConstants.VERSION_DEFAULT);
-					ps2.setLong(2, rs.getLong(1));
-
-					ps2.addBatch();
-				}
-
-				ps2.executeBatch();
-			}
-		}
+		runSQL(
+			"update DDLRecord set recordSetVersion = " +
+				DDLRecordSetConstants.VERSION_DEFAULT);
 	}
 
 }

modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/upgrade/v1_1_0/UpgradeDDLRecordSet.java
@@ -15,11 +15,7 @@
 package com.liferay.dynamic.data.lists.internal.upgrade.v1_1_0;
 
 import com.liferay.dynamic.data.lists.model.DDLRecordSetConstants;
-import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 /**
  * @author Pedro Queiroz
@@ -28,25 +24,9 @@ public class UpgradeDDLRecordSet extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		try (PreparedStatement ps1 = connection.prepareStatement(
-				"select recordSetId from DDLRecordSet");
-			PreparedStatement ps2 =
-				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
-					connection,
-					"update DDLRecordSet set version = ? where recordSetId = " +
-						"?")) {
-
-			try (ResultSet rs = ps1.executeQuery()) {
-				while (rs.next()) {
-					ps2.setString(1, DDLRecordSetConstants.VERSION_DEFAULT);
-					ps2.setLong(2, rs.getLong(1));
-
-					ps2.addBatch();
-				}
-
-				ps2.executeBatch();
-			}
-		}
+		runSQL(
+			"update DDLRecordSet set version = " +
+				DDLRecordSetConstants.VERSION_DEFAULT);
 	}
 
 }

commit cde801228e3f49b64f3c79b0873c4794c4b5fc63

LPS-105126 Explicitly quote value as string to avoid Sybase error "Implicit conversion from datatype 'NUMERIC' to 'VARCHAR' is not allowed"; this was caused by cbb83b05a86cf9d5788925971ea2582050e1b9d8

modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/upgrade/v1_1_0/UpgradeDDLRecord.java
@@ -25,8 +25,8 @@ public class UpgradeDDLRecord extends UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		runSQL(
-			"update DDLRecord set recordSetVersion = " +
-				DDLRecordSetConstants.VERSION_DEFAULT);
+			"update DDLRecord set recordSetVersion = '" +
+				DDLRecordSetConstants.VERSION_DEFAULT + "'");
 	}
 
 }

modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/upgrade/v1_1_0/UpgradeDDLRecordSet.java
@@ -25,8 +25,8 @@ public class UpgradeDDLRecordSet extends UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		runSQL(
-			"update DDLRecordSet set version = " +
-				DDLRecordSetConstants.VERSION_DEFAULT);
+			"update DDLRecordSet set version = '" +
+				DDLRecordSetConstants.VERSION_DEFAULT + "'");
 	}
 
 }