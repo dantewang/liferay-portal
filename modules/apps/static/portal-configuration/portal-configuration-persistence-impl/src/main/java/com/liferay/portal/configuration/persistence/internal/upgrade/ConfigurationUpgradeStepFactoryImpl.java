a39bb3891cc7479e55d5f55004f268ac3b5a4ed1
hai.yu <8219754@liferay.com>
2019-12-13 15:58:47 +0800

    LPS-89497 Add new upgrade method to take care "factory = true" of Meta.OCD configuration.

--- modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/upgrade/ConfigurationUpgradeStepFactoryImpl.java
+++ modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/upgrade/ConfigurationUpgradeStepFactoryImpl.java
@@ -15,11 +15,13 @@
 package com.liferay.portal.configuration.persistence.internal.upgrade;
 
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.persistence.upgrade.ConfigurationUpgradeStepFactory;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeException;
 import com.liferay.portal.kernel.upgrade.UpgradeStep;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.util.PropsValues;
 
 import java.io.File;
@@ -31,6 +33,11 @@ import java.util.Dictionary;
 
 import org.apache.felix.cm.PersistenceManager;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
 /**
  * @author Preston Crary
  */
@@ -38,9 +45,80 @@ public class ConfigurationUpgradeStepFactoryImpl
 	implements ConfigurationUpgradeStepFactory {
 
 	public ConfigurationUpgradeStepFactoryImpl(
-		PersistenceManager persistenceManager) {
+		BundleContext bundleContext, PersistenceManager persistenceManager) {
 
+		_bundleContext = bundleContext;
 		_persistenceManager = persistenceManager;
+	}
+
+	@Override
+	public UpgradeStep createFactoryUpgradeStep(String oldPid, String newPid) {
+		return dbProcessContext -> {
+			ServiceReference<ConfigurationAdmin> serviceReference =
+				_bundleContext.getServiceReference(ConfigurationAdmin.class);
+
+			ConfigurationAdmin configurationAdmin = _bundleContext.getService(
+				serviceReference);
+
+			String filter = StringBundler.concat(
+				StringPool.OPEN_PARENTHESIS,
+				ConfigurationAdmin.SERVICE_FACTORYPID, StringPool.EQUAL, oldPid,
+				StringPool.CLOSE_PARENTHESIS);
+
+			try {
+				Configuration[] configurations =
+					configurationAdmin.listConfigurations(filter);
+
+				if ((configurations == null) || (configurations.length == 0)) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							StringBundler.concat(
+								"Unable to upgrade the oldPid ", oldPid,
+								" because the related data not exist in the ",
+								"Configuration_ table"));
+					}
+
+					return;
+				}
+
+				for (Configuration configuration : configurations) {
+					Dictionary<String, String> dictionary =
+						_persistenceManager.load(configuration.getPid());
+
+					dictionary.put("service.factoryPid", newPid);
+
+					String oldServicePid = (String)dictionary.get(
+						"service.pid");
+
+					String newServicePid = StringUtil.replace(
+						oldServicePid, oldPid, newPid);
+
+					dictionary.put("service.pid", newServicePid);
+
+					String oldFileName = (String)dictionary.get(
+						"felix.fileinstall.filename");
+
+					dictionary.put(
+						"felix.fileinstall.filename",
+						StringUtil.replace(oldFileName, oldPid, newPid));
+
+					_persistenceManager.store(newServicePid, dictionary);
+
+					_persistenceManager.delete(oldServicePid);
+
+					_renameConfigurationFile(
+						oldServicePid, newServicePid, "cfg");
+					_renameConfigurationFile(
+						oldServicePid, newServicePid, "config");
+				}
+			}
+			catch (Exception e) {
+				throw new UpgradeException(e);
+			}
+			finally {
+				_bundleContext.ungetService(serviceReference);
+			}
+		};
 	}
 
 	@Override
@@ -105,6 +183,7 @@ public class ConfigurationUpgradeStepFactoryImpl
 	private static final Log _log = LogFactoryUtil.getLog(
 		ConfigurationUpgradeStepFactoryImpl.class);
 
+	private final BundleContext _bundleContext;
 	private final PersistenceManager _persistenceManager;
 
 }
