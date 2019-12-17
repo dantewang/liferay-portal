03885b480b84eaa37ed3644867e556cf73753a8c
lily.chi <lily.chi@liferay.com>
2019-12-17 17:12:29 +0800

    LPS-74148 Make CPDefinitionLocalizationModels generation consistent with others

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -545,12 +545,6 @@ public class DataFactory {
 		return _counter.get();
 	}
 
-	public List<CPDefinitionLocalizationModel>
-		getCPDefinitionLocalizationModels() {
-
-		return new ArrayList<>(_cpDefinitionLocalizationModels);
-	}
-
 	public List<CPDefinitionModel> getCPDefinitionModels() {
 		return new ArrayList<>(_cpDefinitionModels);
 	}
@@ -862,7 +856,6 @@ public class DataFactory {
 	}
 
 	public void initCommerceProductModels() {
-		_cpDefinitionLocalizationModels = new ArrayList<>(_cpDefinitionCount);
 		_cpDefinitionModels = new ArrayList<>(_cpDefinitionCount);
 		_cpFriendlyURLEntryModels = new ArrayList<>(_cpDefinitionCount);
 		_cpInstanceModels = new ArrayList<>(
@@ -878,9 +871,6 @@ public class DataFactory {
 				 definitionIndex < _maxCPDefinitionCount; definitionIndex++) {
 
 				long cpDefinitionId = cpDefinitionIds[definitionIndex];
-
-				_cpDefinitionLocalizationModels.add(
-					newCPDefinitionLocalizationModel(cpDefinitionId));
 
 				_cpDefinitionModels.add(
 					newCPDefinitionModel(
@@ -1551,6 +1541,31 @@ public class DataFactory {
 		counterModels.add(counterModel);
 
 		return counterModels;
+	}
+
+	public List<CPDefinitionLocalizationModel>
+		newCPDefinitionLocalizationModels() {
+
+		List<CPDefinitionLocalizationModel> cpDefinitionLocalizationModels =
+			new ArrayList<>(_cpDefinitionCount);
+
+		for (int productIndex = 0; productIndex < _maxCProductCount;
+			 productIndex++) {
+
+			long[] cpDefinitionIds = (long[])_cpDefinitionIdList.get(
+				productIndex);
+
+			for (int definitionIndex = 0;
+				 definitionIndex < _maxCPDefinitionCount; definitionIndex++) {
+
+				long cpDefinitionId = cpDefinitionIds[definitionIndex];
+
+				cpDefinitionLocalizationModels.add(
+					newCPDefinitionLocalizationModel(cpDefinitionId));
+			}
+		}
+
+		return cpDefinitionLocalizationModels;
 	}
 
 	public List<CProductModel> newCProductModels() {
@@ -4488,7 +4503,6 @@ public class DataFactory {
 	private final SimpleCounter _counter;
 	private int _cpDefinitionCount;
 	private List _cpDefinitionIdList;
-	private List<CPDefinitionLocalizationModel> _cpDefinitionLocalizationModels;
 	private final Map<Long, String> _cpDefinitionLocalizationNames =
 		new HashMap<>();
 	private List<CPDefinitionModel> _cpDefinitionModels;
