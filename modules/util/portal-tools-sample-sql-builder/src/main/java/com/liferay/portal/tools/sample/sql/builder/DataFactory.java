2ecb26791411b058adaecfeb8c77b610c1149d30
lily.chi <lily.chi@liferay.com>
2019-12-17 17:23:17 +0800

    LPS-74148 Make CPFriendlyURLEntryModels generation consistent with other models

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -549,10 +549,6 @@ public class DataFactory {
 		return _counter.get();
 	}
 
-	public List<CPFriendlyURLEntryModel> getCPFriendlyURLEntryModels() {
-		return new ArrayList<>(_cpFriendlyURLEntryModels);
-	}
-
 	public List<CPInstanceModel> getCPInstanceModels() {
 		return new ArrayList<>(_cpInstanceModels);
 	}
@@ -847,7 +843,6 @@ public class DataFactory {
 	}
 
 	public void initCommerceProductModels() {
-		_cpFriendlyURLEntryModels = new ArrayList<>(_cpDefinitionCount);
 		_cpInstanceModels = new ArrayList<>(
 			_cpDefinitionCount * _maxCPInstanceCount);
 
@@ -861,11 +856,6 @@ public class DataFactory {
 				 definitionIndex < _maxCPDefinitionCount; definitionIndex++) {
 
 				long cpDefinitionId = cpDefinitionIds[definitionIndex];
-
-				_cpFriendlyURLEntryModels.add(
-					newCPFriendlyURLEntryModel(
-						_cProductIds.get(productIndex),
-						_publishedCPDefinitionIds.get(productIndex)));
 
 				for (int instanceIndex = 0; instanceIndex < _maxCPInstanceCount;
 					 instanceIndex++) {
@@ -1579,6 +1569,26 @@ public class DataFactory {
 		}
 
 		return cpDefinitionModels;
+	}
+
+	public List<CPFriendlyURLEntryModel> newCPFriendlyURLEntryModels() {
+		List<CPFriendlyURLEntryModel> cpFriendlyURLEntryModels =
+			new ArrayList<>(_cpDefinitionCount);
+
+		for (int productIndex = 0; productIndex < _maxCProductCount;
+			 productIndex++) {
+
+			for (int definitionIndex = 0;
+				 definitionIndex < _maxCPDefinitionCount; definitionIndex++) {
+
+				cpFriendlyURLEntryModels.add(
+					newCPFriendlyURLEntryModel(
+						_cProductIds.get(productIndex),
+						_publishedCPDefinitionIds.get(productIndex)));
+			}
+		}
+
+		return cpFriendlyURLEntryModels;
 	}
 
 	public List<CProductModel> newCProductModels() {
@@ -4566,7 +4576,6 @@ public class DataFactory {
 	private List _cpDefinitionIdList;
 	private final Map<Long, String> _cpDefinitionLocalizationNames =
 		new HashMap<>();
-	private List<CPFriendlyURLEntryModel> _cpFriendlyURLEntryModels;
 	private List<CPInstanceModel> _cpInstanceModels;
 	private final List<Long> _cProductIds = new ArrayList<>();
 	private final long _cPTaxCategoryId;
