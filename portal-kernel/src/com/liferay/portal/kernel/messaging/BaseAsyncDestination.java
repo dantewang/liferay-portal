a26a163f978c45c7f8613c17ca1c26d7300f9b95
hai.yu <8219754@liferay.com>
2019-12-18 18:05:14 +0800

    LPS-106103 Remove deprecated methods getRejectedExecutionHandler and setRejectedExecutionHandler from DestinationConfiguration class

--- portal-kernel/src/com/liferay/portal/kernel/messaging/BaseAsyncDestination.java
+++ portal-kernel/src/com/liferay/portal/kernel/messaging/BaseAsyncDestination.java
@@ -20,7 +20,6 @@ import com.liferay.petra.concurrent.ThreadPoolHandlerAdapter;
 import com.liferay.petra.executor.PortalExecutorManager;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.cluster.ClusterInvokeThreadLocal;
-import com.liferay.portal.kernel.concurrent.RejectedExecutionHandler;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
@@ -201,16 +200,6 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 		_maximumQueueSize = maximumQueueSize;
 	}
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public void setRejectedExecutionHandler(
-		RejectedExecutionHandler rejectedExecutionHandler) {
-
-		_rejectedExecutionHandler = rejectedExecutionHandler;
-	}
-
 	public void setWorkersCoreSize(int workersCoreSize) {
 		_workersCoreSize = workersCoreSize;
 
@@ -360,7 +349,6 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 
 	private int _maximumQueueSize = Integer.MAX_VALUE;
 	private NoticeableThreadPoolExecutor _noticeableThreadPoolExecutor;
-	private RejectedExecutionHandler _rejectedExecutionHandler;
 	private int _workersCoreSize = _WORKERS_CORE_SIZE;
 	private int _workersMaxSize = _WORKERS_MAX_SIZE;
 
