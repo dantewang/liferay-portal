c7490cb757e41a5b79751d739a6daab121c17735
Dante Wang <dante.wang@liferay.com>
2019-12-27 11:33:30 +0800

    LPS-X Remove deprecated method DDMExpression.getVariableDependenciesMap() and deprecated class VariableDependencies

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/expression/DDMExpression.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/expression/DDMExpression.java
@@ -31,17 +31,6 @@ public interface DDMExpression<T> {
 
 	public Expression getModel();
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public default Map<String, VariableDependencies>
-			getVariableDependenciesMap()
-		throws DDMExpressionException {
-
-		return Collections.emptyMap();
-	}
-
 	public void setVariable(String name, Object value);
 
 }
