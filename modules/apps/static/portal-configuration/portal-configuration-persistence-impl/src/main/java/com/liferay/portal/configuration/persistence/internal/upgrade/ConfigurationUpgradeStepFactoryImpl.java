brandonyee333/test

LPS-89497 Add logic to handle with when factory config files exist and data of configuration_ table not exist, we need to rename the config files.

modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/upgrade/ConfigurationUpgradeStepFactoryImpl.java
@@ -51,6 +51,8 @@ public class ConfigurationUpgradeStepFactoryImpl
 	public UpgradeStep createUpgradeStep(String oldPid, String newPid) {
 		return dbProcessContext -> {
 			try {
+				boolean scanConfigFiles = true;
+
 				if (_persistenceManager.exists(oldPid)) {
 					Dictionary<String, String> dictionary =
 						_persistenceManager.load(oldPid);
@@ -62,6 +64,8 @@ public class ConfigurationUpgradeStepFactoryImpl
 					_persistenceManager.store(newPid, dictionary);
 
 					_persistenceManager.delete(oldPid);
+
+					scanConfigFiles = false;
 				}
 				else {
 					Enumeration<Dictionary<String, String>> dictionaries =
@@ -110,6 +114,23 @@ public class ConfigurationUpgradeStepFactoryImpl
 
 				_renameConfigurationFile(oldPid, newPid, "cfg");
 				_renameConfigurationFile(oldPid, newPid, "config");
+
+				if (scanConfigFiles) {
+					File configResouecesDir = new File(
+						PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR);
+
+					for (File file : configResouecesDir.listFiles()) {
+						String fileName = file.getName();
+
+						if (fileName.startsWith(oldPid + "-")) {
+							String newConfigFilePath = StringUtil.replace(
+								file.getPath(), oldPid, newPid);
+
+							_renameConfigurationFile(
+								file, new File(newConfigFilePath));
+						}
+					}
+				}
 			}
 			catch (IOException ioe) {
 				throw new UpgradeException(ioe);

dantewang/liferay-portal

modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/upgrade/ConfigurationUpgradeStepFactoryImpl.java
@@ -51,6 +51,8 @@ public class ConfigurationUpgradeStepFactoryImpl
 	public UpgradeStep createUpgradeStep(String oldPid, String newPid) {
 		return dbProcessContext -> {
 			try {
+				boolean scanConfigFiles = true;
+
 				if (_persistenceManager.exists(oldPid)) {
 					Dictionary<String, String> dictionary =
 						_persistenceManager.load(oldPid);
@@ -62,6 +64,8 @@ public class ConfigurationUpgradeStepFactoryImpl
 					_persistenceManager.store(newPid, dictionary);
 
 					_persistenceManager.delete(oldPid);
+
+					scanConfigFiles = false;
 				}
 				else {
 					Enumeration<Dictionary<String, String>> dictionaries =
@@ -110,6 +114,23 @@ public class ConfigurationUpgradeStepFactoryImpl
 
 				_renameConfigurationFile(oldPid, newPid, "cfg");
 				_renameConfigurationFile(oldPid, newPid, "config");
+
+				if (scanConfigFiles) {
+					File configResourcesDir = new File(
+						PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR);
+
+					for (File file : configResourcesDir.listFiles()) {
+						String fileName = file.getName();
+
+						if (fileName.startsWith(oldPid + "-")) {
+							String newConfigFilePath = StringUtil.replace(
+								file.getPath(), oldPid, newPid);
+
+							_renameConfigurationFile(
+								file, new File(newConfigFilePath));
+						}
+					}
+				}
 			}
 			catch (IOException ioException) {
 				throw new UpgradeException(ioException);