f168859ffcb98f7e357c55ff3829f2cb86893324
hai.yu <8219754@liferay.com>
2020-06-11 16:18:34 +0800

    LPS-111104 Change CaptureAppender and LogAssertionAppender by using log4j2 api

--- portal-test/src/com/liferay/portal/test/log/Log4JLoggerTestUtil.java
+++ portal-test/src/com/liferay/portal/test/log/Log4JLoggerTestUtil.java
@@ -19,8 +19,8 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.log.LogWrapper;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.Logger;
 
 /**
  * @author Shuyang Zhou
