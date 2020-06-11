0ecfb7d38ce9867af4dec0b7874118fa86938447
Yang Cao <yang.cao@liferay.com>
2020-06-11 17:53:29 +0800

    LPS Integration-test Homework

--- modules/test/resource-actions-test/src/testIntegration/java/com/liferay/resource/actions/test/ResourceActionsTest.java
+++ modules/test/resource-actions-test/src/testIntegration/java/com/liferay/resource/actions/test/ResourceActionsTest.java
@@ -15,16 +15,25 @@
 package com.liferay.resource.actions.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.security.permission.ResourceActions;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.kernel.xml.UnsecureSAXReaderUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.io.InputStream;
 
 import java.net.URL;
 
 import java.util.Dictionary;
+import java.util.List;
 
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -47,13 +56,17 @@ public class ResourceActionsTest {
 		new LiferayIntegrationTestRule();
 
 	@Test
-	public void testAllXmlsAreRead() {
+	public void testAllXmlsAreRead() throws Exception {
 		Bundle currentBundle = FrameworkUtil.getBundle(
 			ResourceActionsDefinitionTest.class);
 
 		BundleContext bundleContext = currentBundle.getBundleContext();
 
 		Bundle[] bundles = bundleContext.getBundles();
+
+		StringBundler sb = new StringBundler();
+
+		List<String> modelNamesList = _resourceActions.getModelNames();
 
 		for (Bundle bundle : bundles) {
 			Dictionary<String, String> headers = bundle.getHeaders(
@@ -70,14 +83,67 @@ public class ResourceActionsTest {
 			URL url = classLoader.getResource("resource-actions/default.xml");
 
 			if (url == null) {
+				System.out.println(
+					"\n\t\t" + bundle.getSymbolicName() +
+						": resource-actions/default.xml is not found");
+
 				continue;
 			}
 
-			// Read from default.xml, collect model-names, and assert that
-			// every model name you read from the file is in the list returned
-			// by _resourceActions.getModelNames()
+			Document document = null;
 
+			try (InputStream inputStream = url.openStream()) {
+				document = UnsecureSAXReaderUtil.read(inputStream, false);
+			}
+
+			Element rootElement = document.getRootElement();
+
+			for (Element resourceElement :
+					rootElement.elements("model-resource")) {
+
+				for (Element modelNameElement :
+						resourceElement.elements("model-name")) {
+
+					String modelName = modelNameElement.getTextTrim();
+
+					if (!modelNamesList.contains(modelName)) {
+						sb.append("\n\t\t");
+						sb.append(bundle.getSymbolicName());
+						sb.append(" ");
+						sb.append(modelName);
+						sb.append(": is not found in the modelNamesList.");
+					}
+				}
+
+				for (Element compositeModelNameElement :
+						resourceElement.elements("composite-model-name")) {
+
+					String compositeModelName = _getCompositeModelName(
+						compositeModelNameElement);
+
+					if (!modelNamesList.contains(compositeModelName)) {
+						sb.append("\n\t\t");
+						sb.append(bundle.getSymbolicName());
+						sb.append(" ");
+						sb.append(compositeModelName);
+						sb.append(": is not found in the modelNamesList.");
+					}
+				}
+			}
+
+			String errors = sb.toString();
+
+			Assert.assertTrue(
+				"The following resource action issues are found:".concat(
+					errors),
+				errors.isEmpty());
 		}
+	}
+
+	private String _getCompositeModelName(Element compositeModelNameElement) {
+		return ReflectionTestUtil.invoke(
+			_resourceActions, "_getCompositeModelName",
+			new Class<?>[] {Element.class}, compositeModelNameElement);
 	}
 
 	@Inject
