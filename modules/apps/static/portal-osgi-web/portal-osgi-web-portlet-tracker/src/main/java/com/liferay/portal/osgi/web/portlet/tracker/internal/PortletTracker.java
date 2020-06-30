d2ad04c02ad2fa10efb6bf3929bea47a2c942179
hai.yu <8219754@liferay.com>
2020-06-30 14:50:37 +0800

    LPS-117531 Apply to PortletTracker and PortletHotDeployListener to postpone portlet resource read after deployment

--- modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
@@ -372,6 +372,24 @@ public class PortletTracker
 
 			portletBagFactory.create(portletModel, portlet, true);
 
+			Configuration configuration =
+				serviceRegistrations.getConfiguration();
+
+			if (configuration != null) {
+				Properties properties = configuration.getProperties();
+
+				try {
+					ResourceActionsUtil.readPortletResource(
+						portletModel, bundleClassLoader,
+						StringUtil.split(
+							properties.getProperty(
+								PropsKeys.RESOURCE_ACTIONS_CONFIGS)));
+				}
+				catch (Exception exception) {
+					_log.error(exception, exception);
+				}
+			}
+
 			deployPortlet(
 				serviceReference, portletModel,
 				_companyLocalService.getCompanies());
@@ -1325,23 +1343,6 @@ public class PortletTracker
 		return serviceRegistrations;
 	}
 
-	protected void readResourceActions(
-		Configuration configuration, ClassLoader classLoader) {
-
-		Properties properties = configuration.getProperties();
-
-		try {
-			ResourceActionsUtil.read(
-				classLoader,
-				StringUtil.split(
-					properties.getProperty(
-						PropsKeys.RESOURCE_ACTIONS_CONFIGS)));
-		}
-		catch (Exception exception) {
-			_log.error(exception, exception);
-		}
-	}
-
 	@Reference(unbind = "-")
 	protected void setHttpServiceRuntime(
 		HttpServiceRuntime httpServiceRuntime, Map<String, Object> properties) {
@@ -1522,13 +1523,16 @@ public class PortletTracker
 		}
 
 		protected synchronized void doConfiguration(ClassLoader classLoader) {
-			if (classLoader.getResource("portlet.properties") != null) {
-				Configuration configuration =
-					ConfigurationFactoryUtil.getConfiguration(
-						classLoader, "portlet");
+			if ((_configuration == null) &&
+				(classLoader.getResource("portlet.properties") != null)) {
 
-				readResourceActions(configuration, classLoader);
+				_configuration = ConfigurationFactoryUtil.getConfiguration(
+					classLoader, "portlet");
 			}
+		}
+
+		protected synchronized Configuration getConfiguration() {
+			return _configuration;
 		}
 
 		protected synchronized PortletApp getPortletApp() {
@@ -1540,6 +1544,7 @@ public class PortletTracker
 		}
 
 		private final Bundle _bundle;
+		private Configuration _configuration;
 		private PortletApp _portletApp;
 		private final List<ServiceReference<Portlet>> _serviceReferences =
 			new ArrayList<>();
