1dd1150838f1f55463ac94acbef728eb622b88ef
Dante Wang <dante.wang@liferay.com>
2020-07-07 10:52:49 +0800

    LPS-X decode portlet name

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -1088,7 +1088,8 @@ public class ResourceActionsImpl implements ResourceActions {
 					portletResourceElement.elementTextTrim("portlet-name"));
 
 				if (portlet != null) {
-					String deployPortletName = portlet.getPortletId();
+					String deployPortletName = PortletIdCodec.decodePortletName(
+						portlet.getPortletId());
 
 					if (!deployPortletName.equals(portletName)) {
 						continue;
