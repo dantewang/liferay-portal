5624e4a0667f5a551ec076e57933872a2823ecf3
Dante Wang <dante.wang@liferay.com>
2020-07-21 13:55:29 +0800

    LPS-117531 SF, use referenced service

--- modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
@@ -44,7 +44,6 @@ import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
 import com.liferay.portal.kernel.portlet.PortletInstanceFactory;
 import com.liferay.portal.kernel.security.permission.ResourceActions;
-import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.service.ResourceActionLocalService;
@@ -346,7 +345,7 @@ public class PortletTracker
 
 			if (sources != null) {
 				try {
-					ResourceActionsUtil.readPortletResource(
+					_resourceActions.readPortletResource(
 						portletModel, null, bundleClassLoader, sources);
 				}
 				catch (Exception exception) {
@@ -1460,7 +1459,7 @@ public class PortletTracker
 							PropsKeys.RESOURCE_ACTIONS_CONFIGS));
 
 					try {
-						ResourceActionsUtil.readModelResource(
+						_resourceActions.readModelResource(
 							null, classLoader, _sources);
 					}
 					catch (Exception exception) {
