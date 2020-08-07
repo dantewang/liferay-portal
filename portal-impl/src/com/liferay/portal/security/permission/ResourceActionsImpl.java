c890532dfe1122bc6b16bc3fd5b46cad87a69156
hai.yu <8219754@liferay.com>
2020-08-07 23:31:05 +0800

    LPS-117531 Deprecated unused method.

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -615,6 +615,12 @@ public class ResourceActionsImpl implements ResourceActions {
 		return false;
 	}
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
+	 *             #readPortletResource(String, ClassLoader, String[])} and
+	 *             {@link #readModelResource(String, ClassLoader, String[])}
+	 */
+	@Deprecated
 	@Override
 	public void read(
 			String servletContextName, ClassLoader classLoader, String source)
@@ -630,6 +636,12 @@ public class ResourceActionsImpl implements ResourceActions {
 				servletContextName, rootElement, null));
 	}
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
+	 *             #readPortletResource(String, ClassLoader, String[])} and
+	 *             {@link #readModelResource(String, ClassLoader, String[])}
+	 */
+	@Deprecated
 	@Override
 	public void read(
 			String servletContextName, ClassLoader classLoader,
@@ -641,6 +653,12 @@ public class ResourceActionsImpl implements ResourceActions {
 		}
 	}
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
+	 *             #readModelResource(String, ClassLoader, String[])} and
+	 *             {@link #checkResourceActions(Set)}
+	 */
+	@Deprecated
 	@Override
 	public void readAndCheck(
 			String servletContextName, ClassLoader classLoader,
