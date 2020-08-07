cc27fd6a4f7f8bb398de4a1dfd50cf58d8d518dc
hai.yu <8219754@liferay.com>
2020-08-07 14:31:09 +0800

    LPS-117531 Add method to check resource actions on given resource names

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -133,6 +133,14 @@ public class ResourceActionsImpl implements ResourceActions {
 	}
 
 	@Override
+	public void checkResourceActions(Set<String> resourceNames) {
+		for (String resourceName : resourceNames) {
+			resourceActionLocalService.checkResourceActions(
+				resourceName, getResourceActions(resourceName));
+		}
+	}
+
+	@Override
 	public String getAction(
 		HttpServletRequest httpServletRequest, String action) {
 
@@ -658,7 +666,7 @@ public class ResourceActionsImpl implements ResourceActions {
 	}
 
 	@Override
-	public void readModelResource(
+	public Set<String> readModelResource(
 			String servletContextName, ClassLoader classLoader,
 			String... sources)
 		throws ResourceActionsException {
@@ -672,10 +680,7 @@ public class ResourceActionsImpl implements ResourceActions {
 					servletContextName, rootElement, resourceNames));
 		}
 
-		for (String resourceName : resourceNames) {
-			resourceActionLocalService.checkResourceActions(
-				resourceName, getResourceActions(resourceName));
-		}
+		return resourceNames;
 	}
 
 	@Override
