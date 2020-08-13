ccdca6e5c8f3e137e7d3343a36f3c04f5a4b8684
hai.yu <8219754@liferay.com>
2020-08-13 12:44:26 +0800

    LPS-117531 Backwards compatibility

--- modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/configuration/ServiceConfigurationInitializer.java
+++ modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/configuration/ServiceConfigurationInitializer.java
@@ -126,12 +126,15 @@ public class ServiceConfigurationInitializer {
 
 	private void _readResourceActions() {
 		try {
+			String[] sources = StringUtil.split(
+				_portletConfiguration.get(PropsKeys.RESOURCE_ACTIONS_CONFIGS));
+
 			_resourceActions.checkResourceActions(
 				_resourceActions.readModelResource(
-					null, _classLoader,
-					StringUtil.split(
-						_portletConfiguration.get(
-							PropsKeys.RESOURCE_ACTIONS_CONFIGS))));
+					null, _classLoader, sources));
+			_resourceActions.checkResourceActions(
+				_resourceActions.readPortletResources(
+					null, _classLoader, sources));
 		}
 		catch (Exception exception) {
 			_log.error(
