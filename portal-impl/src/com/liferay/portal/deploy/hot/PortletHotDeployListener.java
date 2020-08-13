f479427d1356816df28a16d9b185aa3ca9fd072a
Dante Wang <dante.wang@liferay.com>
2020-08-13 17:22:44 +0800

    LPS-117531 SF, simplify

--- portal-impl/src/com/liferay/portal/deploy/hot/PortletHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/PortletHotDeployListener.java
@@ -278,34 +278,24 @@ public class PortletHotDeployListener extends BaseHotDeployListener {
 
 		String[] sources = null;
 
-		boolean readModelResource = true;
-
-		boolean readResource = false;
-
 		if (portletPropertiesConfiguration != null) {
 			portletProperties = portletPropertiesConfiguration.getProperties();
 
 			sources = StringUtil.split(
 				portletProperties.getProperty(
 					PropsKeys.RESOURCE_ACTIONS_CONFIGS));
+		}
 
-			if (!portletProperties.isEmpty()) {
-				readResource = true;
-			}
+		if (sources != null) {
+			ResourceActionsUtil.checkResourceActions(
+				ResourceActionsUtil.readModelResource(
+					servletContextName, classLoader, sources));
 		}
 
 		for (Portlet portlet : portlets) {
-			if (readResource) {
+			if (sources != null) {
 				ResourceActionsUtil.readPortletResource(
 					portlet, servletContextName, classLoader, sources);
-
-				if (readModelResource) {
-					ResourceActionsUtil.checkResourceActions(
-						ResourceActionsUtil.readModelResource(
-							servletContextName, classLoader, sources));
-
-					readModelResource = false;
-				}
 			}
 
 			ResourceActionsUtil.check(portlet.getPortletId());
