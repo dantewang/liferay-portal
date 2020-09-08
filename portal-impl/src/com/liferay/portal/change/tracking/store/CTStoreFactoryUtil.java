0c9572f97bf17754b3d64d53d1a551e0c85d42c0
hai.yu <8219754@liferay.com>
2020-09-08 17:35:22 +0800

    LPS-120256 Deprecated unused class

--- portal-impl/src/com/liferay/portal/change/tracking/store/CTStoreFactoryUtil.java
+++ portal-impl/src/com/liferay/portal/change/tracking/store/CTStoreFactoryUtil.java
@@ -18,8 +18,10 @@ import com.liferay.document.library.kernel.store.Store;
 import com.liferay.portal.kernel.util.ServiceProxyFactory;
 
 /**
- * @author Shuyang Zhou
+ * @author     Shuyang Zhou
+ * @deprecated As of Athanasius (7.3.x), with no direct replacement
  */
+@Deprecated
 public class CTStoreFactoryUtil {
 
 	public static Store createCTStore(Store store, String storeType) {
