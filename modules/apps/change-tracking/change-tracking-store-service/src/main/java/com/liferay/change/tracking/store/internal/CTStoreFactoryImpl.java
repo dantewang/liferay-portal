96e58cfaab0d8d4d43313a59b3aecd172b0ca199
hai.yu <8219754@liferay.com>
2020-09-08 17:06:36 +0800

    LPS-120256 Track non-ct.aware Store, wrap them with CTStore and register CTStore as ct.aware Store

--- modules/apps/change-tracking/change-tracking-store-service/src/main/java/com/liferay/change/tracking/store/internal/CTStoreFactoryImpl.java
+++ modules/apps/change-tracking/change-tracking-store-service/src/main/java/com/liferay/change/tracking/store/internal/CTStoreFactoryImpl.java
@@ -18,11 +18,24 @@ import com.liferay.change.tracking.service.CTEntryLocalService;
 import com.liferay.change.tracking.store.model.CTSContent;
 import com.liferay.change.tracking.store.service.CTSContentLocalService;
 import com.liferay.document.library.kernel.store.Store;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.change.tracking.store.CTStoreFactory;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Shuyang Zhou
@@ -38,6 +51,30 @@ public class CTStoreFactoryImpl implements CTStoreFactory {
 			_ctsContentLocalService, store, storeType);
 	}
 
+	@Activate
+	protected void activate(BundleContext bundleContext)
+		throws InvalidSyntaxException {
+
+		_bundleContext = bundleContext;
+
+		String filterString = StringBundler.concat(
+			"(&(objectClass=", Store.class.getName(), ")(store.type=*)",
+			"(!(ct.aware=*)))");
+
+		_serviceTracker = new ServiceTracker<>(
+			bundleContext, bundleContext.createFilter(filterString),
+			new StoreServiceTrackerCustomizer());
+
+		_serviceTracker.open();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTracker.close();
+	}
+
+	private BundleContext _bundleContext;
+
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
 
@@ -46,5 +83,49 @@ public class CTStoreFactoryImpl implements CTStoreFactory {
 
 	@Reference
 	private CTSContentLocalService _ctsContentLocalService;
+
+	private ServiceTracker<Store, ServiceRegistration<Store>> _serviceTracker;
+
+	private class StoreServiceTrackerCustomizer
+		implements ServiceTrackerCustomizer<Store, ServiceRegistration<Store>> {
+
+		@Override
+		public ServiceRegistration<Store> addingService(
+			ServiceReference<Store> serviceReference) {
+
+			Store store = createCTStore(
+				_bundleContext.getService(serviceReference),
+				GetterUtil.getString(
+					serviceReference.getProperty("store.type")));
+
+			Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+			properties.put("ct.aware", "true");
+			properties.put(
+				"store.type", serviceReference.getProperty("store.type"));
+
+			return _bundleContext.registerService(
+				Store.class, store, properties);
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<Store> serviceReference,
+			ServiceRegistration<Store> serviceRegistration) {
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<Store> serviceReference,
+			ServiceRegistration<Store> serviceRegistration) {
+
+			_bundleContext.ungetService(serviceReference);
+
+			if (serviceRegistration != null) {
+				serviceRegistration.unregister();
+			}
+		}
+
+	}
 
 }
