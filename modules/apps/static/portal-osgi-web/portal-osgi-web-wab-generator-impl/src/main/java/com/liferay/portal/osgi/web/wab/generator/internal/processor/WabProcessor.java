359376bcfb1d63ec1c6bff17841a08043fc376a7
Dante Wang <dante.wang@liferay.com>
2020-09-24 19:25:28 +0800

    LPS-X Util-java in not exporting com.liferay.util, inline JS.getSafeName for now

--- modules/apps/static/portal-osgi-web/portal-osgi-web-wab-generator-impl/src/main/java/com/liferay/portal/osgi/web/wab/generator/internal/processor/WabProcessor.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-wab-generator-impl/src/main/java/com/liferay/portal/osgi/web/wab/generator/internal/processor/WabProcessor.java
@@ -32,6 +32,7 @@ import aQute.bnd.version.Version;
 import aQute.lib.filter.Filter;
 
 import com.liferay.ant.bnd.jsp.JspAnalyzerPlugin;
+import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.events.GlobalStartupAction;
@@ -67,7 +68,6 @@ import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.kernel.xml.UnsecureSAXReaderUtil;
 import com.liferay.portal.kernel.xml.XPath;
 import com.liferay.portal.util.PropsValues;
-import com.liferay.util.JS;
 import com.liferay.whip.util.ReflectionUtil;
 
 import java.io.File;
@@ -1396,10 +1396,37 @@ public class WabProcessor {
 			return;
 		}
 
-		portletNameElement.setText(
-			JS.getSafeName(
-				StringBundler.concat(
-					portletName, PortletConstants.WAR_SEPARATOR, _context)));
+		portletName = StringBundler.concat(
+			portletName, PortletConstants.WAR_SEPARATOR, _context);
+
+		String safeName = portletName;
+
+		StringBuilder sb = null;
+
+		int index = 0;
+
+		for (int i = 0; i < portletName.length(); i++) {
+			char c = portletName.charAt(i);
+
+			if ((c == CharPool.DASH) || (c == CharPool.PERIOD) ||
+				(c == CharPool.SPACE)) {
+
+				if (sb == null) {
+					sb = new StringBuilder(portletName.length() - 1);
+
+					sb.append(portletName, index, i);
+				}
+			}
+			else if (sb != null) {
+				sb.append(c);
+			}
+		}
+
+		if (sb != null) {
+			safeName = sb.toString();
+		}
+
+		portletNameElement.setText(safeName);
 	}
 
 	private void _processExcludedJSPs(Analyzer analyzer) {
