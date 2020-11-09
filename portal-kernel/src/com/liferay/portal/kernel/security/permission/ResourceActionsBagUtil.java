69fae67b60a488c8cbd6813c340e656c1f8da61e
hai.yu <8219754@liferay.com>
2020-11-09 23:37:21 +0800

    LPS-112056 Create ResourceActionsBagUtil class (the following commit will create getPortletResourceActions() realted methods), including getModelResource*() method, getResourceActionsBag() and putResourceActionsBags() methods.

---
+++ portal-kernel/src/com/liferay/portal/kernel/security/permission/ResourceActionsBagUtil.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.security.permission;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Hai Yu
+ */
+public class ResourceActionsBagUtil {
+
+	public static List<String> getModelResourceActions(String name) {
+		ResourceActionsBag modelResourceActionsBag = getResourceActionsBag(
+			name);
+
+		if (modelResourceActionsBag == null) {
+			return new ArrayList<>();
+		}
+
+		return modelResourceActionsBag.getModelResourceActions();
+	}
+
+	public static List<String> getModelResourceGroupDefaultActions(
+		String name) {
+
+		ResourceActionsBag modelResourceActionsBag = getResourceActionsBag(
+			name);
+
+		if (modelResourceActionsBag == null) {
+			return new ArrayList<>();
+		}
+
+		return modelResourceActionsBag.getModelResourceGroupDefaultActions();
+	}
+
+	public static List<String> getModelResourceGuestDefaultActions(
+		String name) {
+
+		ResourceActionsBag modelResourceActionsBag = getResourceActionsBag(
+			name);
+
+		if (modelResourceActionsBag == null) {
+			return new ArrayList<>();
+		}
+
+		return modelResourceActionsBag.getModelResourceGuestDefaultActions();
+	}
+
+	public static List<String> getModelResourceGuestUnsupportedActions(
+		String name) {
+
+		ResourceActionsBag modelResourceActionsBag = getResourceActionsBag(
+			name);
+
+		if (modelResourceActionsBag == null) {
+			return new ArrayList<>();
+		}
+
+		return modelResourceActionsBag.
+			getModelResourceGuestUnsupportedActions();
+	}
+
+	public static List<String> getModelResourceOwnerDefaultActions(
+		String name) {
+
+		ResourceActionsBag modelResourceActionsBag = getResourceActionsBag(
+			name);
+
+		if (modelResourceActionsBag == null) {
+			return new ArrayList<>();
+		}
+
+		return modelResourceActionsBag.getModelResourceOwnerDefaultActions();
+	}
+
+	public static ResourceActionsBag getResourceActionsBag(String name) {
+		return _resourceActionsBag.getResourceActionsBag(name);
+	}
+
+	public static ResourceActionsBag putResourceActionsBags(
+		String name, ResourceActionsBag resourceActionsBag, boolean force) {
+
+		return _resourceActionsBag.putResourceActionsBags(
+			name, resourceActionsBag, force);
+	}
+
+	public void setResourceActionsBag(ResourceActionsBag resourceActionsBag) {
+		_resourceActionsBag = resourceActionsBag;
+	}
+
+	private static ResourceActionsBag _resourceActionsBag;
+
+}
