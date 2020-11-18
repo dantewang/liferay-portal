a28a2390ec38609614789485bc4e4b70c43621c6
Dante Wang <dante.wang@liferay.com>
2020-11-18 10:50:49 +0800

    temp

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -662,7 +662,8 @@ public class ResourceActionsImpl implements ResourceActions {
 		}
 
 		for (String portletName : portletNames) {
-			check(portletName);
+			resourceActionLocalService.checkResourceActions(
+				portletName, getResourceActions(portletName));
 		}
 	}
 
@@ -1158,7 +1159,7 @@ public class ResourceActionsImpl implements ResourceActions {
 				Collections.singleton(ActionKeys.PERMISSIONS));
 
 			if (portletNames != null) {
-				portletNames.addAll(_resourceReferences.get(modelName));
+				portletNames.add(modelName);
 			}
 		}
 	}
