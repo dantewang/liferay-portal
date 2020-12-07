1878e3ba906b32cd66dc7b30bf069177346da856
Dante Wang <dante.wang@liferay.com>
2020-12-07 14:53:07 +0800

    LPS-58936 SF

--- portal-impl/src/com/liferay/portal/plugin/PluginUtil.java
+++ portal-impl/src/com/liferay/portal/plugin/PluginUtil.java
@@ -30,21 +30,7 @@ public class PluginUtil {
 	public static <P extends Plugin> List<P> restrictPlugins(
 		List<P> plugins, long companyId, long userId) {
 
-		List<P> visiblePlugins = new ArrayList<>(plugins.size());
-
-		for (P plugin : plugins) {
-			PluginSetting pluginSetting =
-				PluginSettingLocalServiceUtil.getPluginSetting(
-					companyId, plugin.getPluginId(), plugin.getPluginType());
-
-			if (pluginSetting.isActive() &&
-				pluginSetting.hasPermission(userId)) {
-
-				visiblePlugins.add(plugin);
-			}
-		}
-
-		return visiblePlugins;
+		return restrictPlugins(plugins, companyId, 0, userId);
 	}
 
 	public static <P extends Plugin> List<P> restrictPlugins(
