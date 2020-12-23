941e46c6b2e91c4130e91eba804970e8e3b7b843
Dante Wang <dante.wang@liferay.com>
2020-12-23 15:49:50 +0800

    LPS-125170 Simplify, if lockKey is not included in status message then the task is not serial and will not be queued

--- modules/apps/portal-background-task/portal-background-task-service/src/main/java/com/liferay/portal/background/task/internal/messaging/BackgroundTaskQueuingMessageListener.java
+++ modules/apps/portal-background-task/portal-background-task-service/src/main/java/com/liferay/portal/background/task/internal/messaging/BackgroundTaskQueuingMessageListener.java
@@ -43,6 +43,12 @@ public class BackgroundTaskQueuingMessageListener extends BaseMessageListener {
 
 	@Override
 	protected void doReceive(Message message) throws Exception {
+		String lockKey = (String)message.get("lockKey");
+
+		if (Validator.isNull(lockKey)) {
+			return;
+		}
+
 		String taskExecutorClassName = (String)message.get(
 			"taskExecutorClassName");
 
@@ -56,13 +62,16 @@ public class BackgroundTaskQueuingMessageListener extends BaseMessageListener {
 			return;
 		}
 
+		int isolationLevel = (int)message.get("isolationLevel");
+
 		int status = (Integer)message.get("status");
 
 		if ((status == BackgroundTaskConstants.STATUS_CANCELLED) ||
 			(status == BackgroundTaskConstants.STATUS_FAILED) ||
 			(status == BackgroundTaskConstants.STATUS_SUCCESSFUL)) {
 
-			_executeQueuedBackgroundTasks(message, taskExecutorClassName);
+			_executeQueuedBackgroundTasks(
+				isolationLevel, lockKey, taskExecutorClassName);
 		}
 		else if (status == BackgroundTaskConstants.STATUS_QUEUED) {
 			long backgroundTaskId = (Long)message.get(
@@ -74,18 +83,19 @@ public class BackgroundTaskQueuingMessageListener extends BaseMessageListener {
 			if (!_backgroundTaskLockHelper.isLockedBackgroundTask(
 					backgroundTask)) {
 
-				_executeQueuedBackgroundTasks(message, taskExecutorClassName);
+				_executeQueuedBackgroundTasks(
+					isolationLevel, lockKey, taskExecutorClassName);
 			}
 		}
 	}
 
 	private void _executeQueuedBackgroundTasks(
-		Message message, String taskExecutorClassName) {
-
-		String lockKey = (String)message.get("lockKey");
+		int isolationLevel, String lockKey, String taskExecutorClassName) {
 
 		if (_log.isDebugEnabled()) {
-			if (Validator.isNotNull(lockKey)) {
+			if (isolationLevel !=
+					BackgroundTaskConstants.ISOLATION_LEVEL_CUSTOM) {
+
 				_log.debug(
 					"Acquiring next queued background task for " + lockKey);
 			}
@@ -98,46 +108,32 @@ public class BackgroundTaskQueuingMessageListener extends BaseMessageListener {
 
 		BackgroundTask backgroundTask = null;
 
-		if (Validator.isNotNull(lockKey)) {
-			int isolationLevel = (Integer)message.get("isolationLevel");
+		if (isolationLevel == BackgroundTaskConstants.ISOLATION_LEVEL_COMPANY) {
+			long companyId = GetterUtil.getLong(
+				StringUtil.extractLast(lockKey, CharPool.POUND));
 
-			if (isolationLevel ==
-					BackgroundTaskConstants.ISOLATION_LEVEL_COMPANY) {
+			backgroundTask =
+				_backgroundTaskManager.fetchFirstBackgroundTaskByCompanyId(
+					companyId, taskExecutorClassName,
+					BackgroundTaskConstants.STATUS_QUEUED);
+		}
+		else if (isolationLevel ==
+					BackgroundTaskConstants.ISOLATION_LEVEL_GROUP) {
 
-				long companyId = GetterUtil.getLong(
-					StringUtil.extractLast(lockKey, CharPool.POUND));
+			long groupId = GetterUtil.getLong(
+				StringUtil.extractLast(lockKey, CharPool.POUND));
 
-				backgroundTask =
-					_backgroundTaskManager.fetchFirstBackgroundTaskByCompanyId(
-						companyId, taskExecutorClassName,
-						BackgroundTaskConstants.STATUS_QUEUED);
-			}
-			else if (isolationLevel ==
-						BackgroundTaskConstants.ISOLATION_LEVEL_GROUP) {
+			backgroundTask =
+				_backgroundTaskManager.fetchFirstBackgroundTaskByGroupId(
+					groupId, taskExecutorClassName,
+					BackgroundTaskConstants.STATUS_QUEUED);
+		}
+		else if (isolationLevel ==
+					BackgroundTaskConstants.ISOLATION_LEVEL_TASK_NAME) {
 
-				long groupId = GetterUtil.getLong(
-					StringUtil.extractLast(lockKey, CharPool.POUND));
-
-				backgroundTask =
-					_backgroundTaskManager.fetchFirstBackgroundTaskByGroupId(
-						groupId, taskExecutorClassName,
-						BackgroundTaskConstants.STATUS_QUEUED);
-			}
-			else if (isolationLevel ==
-						BackgroundTaskConstants.ISOLATION_LEVEL_TASK_NAME) {
-
-				backgroundTask =
-					_backgroundTaskManager.fetchFirstBackgroundTask(
-						StringUtil.extractLast(lockKey, CharPool.POUND),
-						taskExecutorClassName,
-						BackgroundTaskConstants.STATUS_QUEUED);
-			}
-			else {
-				backgroundTask =
-					_backgroundTaskManager.fetchFirstBackgroundTask(
-						taskExecutorClassName,
-						BackgroundTaskConstants.STATUS_QUEUED);
-			}
+			backgroundTask = _backgroundTaskManager.fetchFirstBackgroundTask(
+				StringUtil.extractLast(lockKey, CharPool.POUND),
+				taskExecutorClassName, BackgroundTaskConstants.STATUS_QUEUED);
 		}
 		else {
 			backgroundTask = _backgroundTaskManager.fetchFirstBackgroundTask(
