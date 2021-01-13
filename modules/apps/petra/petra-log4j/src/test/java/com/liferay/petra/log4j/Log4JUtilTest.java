5221d2c1e22afc6511e0795616a3965fb0d29041
Dante Wang <dante.wang@liferay.com>
2021-01-13 17:26:26 +0800

    LPS-118542 SF

--- modules/apps/petra/petra-log4j/src/test/java/com/liferay/petra/log4j/Log4JUtilTest.java
+++ modules/apps/petra/petra-log4j/src/test/java/com/liferay/petra/log4j/Log4JUtilTest.java
@@ -163,24 +163,25 @@ public class Log4JUtilTest {
 			LoggerName.LOGGER_WARN.toString());
 
 		_assertLogLevel("WARN", log);
-		_assertJDKLogLevel("WARNING", jdkLogger);
+		_assertJDKLogLevel(Level.WARNING, jdkLogger);
 
 		Log4JUtil.setLevel(LoggerName.LOGGER_WARN.toString(), "DEBUG", false);
 
 		_assertLogLevel("DEBUG", log);
-		_assertJDKLogLevel("FINE", jdkLogger);
+		_assertJDKLogLevel(Level.FINE, jdkLogger);
 
 		Log childLog = LogFactoryUtil.getLog("com.test.parent.child");
+
 		java.util.logging.Logger childJDKLogger =
 			java.util.logging.Logger.getLogger("com.test.parent.child");
 
 		_assertLogLevel("INFO", childLog);
-		_assertJDKLogLevel("INFO", childJDKLogger);
+		_assertJDKLogLevel(Level.INFO, childJDKLogger);
 
 		Log4JUtil.setLevel("com.test.parent", "DEBUG", false);
 
 		_assertLogLevel("DEBUG", childLog);
-		_assertJDKLogLevel("FINE", childJDKLogger);
+		_assertJDKLogLevel(Level.FINE, childJDKLogger);
 	}
 
 	@NewEnv(type = NewEnv.Type.JVM)
@@ -205,68 +206,44 @@ public class Log4JUtilTest {
 
 	private void _assertJDKLogEnable() {
 		_assertJDKLogLevel(
-			"ALL",
+			Level.ALL,
 			java.util.logging.Logger.getLogger(
 				LoggerName.LOGGER_ALL.toString()));
 		_assertJDKLogLevel(
-			"OFF",
+			Level.OFF,
 			java.util.logging.Logger.getLogger(
 				LoggerName.LOGGER_OFF.toString()));
 		_assertJDKLogLevel(
-			"INFO",
+			Level.INFO,
 			java.util.logging.Logger.getLogger(
 				LoggerName.LOGGER_FATAL.toString()));
 		_assertJDKLogLevel(
-			"SEVERE",
+			Level.SEVERE,
 			java.util.logging.Logger.getLogger(
 				LoggerName.LOGGER_ERROR.toString()));
 		_assertJDKLogLevel(
-			"WARNING",
+			Level.WARNING,
 			java.util.logging.Logger.getLogger(
 				LoggerName.LOGGER_WARN.toString()));
 		_assertJDKLogLevel(
-			"INFO",
+			Level.INFO,
 			java.util.logging.Logger.getLogger(
 				LoggerName.LOGGER_INFO.toString()));
 		_assertJDKLogLevel(
-			"FINE",
+			Level.FINE,
 			java.util.logging.Logger.getLogger(
 				LoggerName.LOGGER_DEBUG.toString()));
 		_assertJDKLogLevel(
-			"INFO",
+			Level.INFO,
 			java.util.logging.Logger.getLogger(
 				LoggerName.LOGGER_TRACE.toString()));
 	}
 
 	private void _assertJDKLogLevel(
-		String expectedLevel, java.util.logging.Logger jdkLog) {
-
-		String actualLevel = null;
-
-		if (jdkLog.isLoggable(Level.ALL)) {
-			actualLevel = "ALL";
-		}
-		else if (jdkLog.isLoggable(Level.FINE)) {
-			actualLevel = "FINE";
-		}
-		else if (jdkLog.isLoggable(Level.INFO)) {
-			actualLevel = "INFO";
-		}
-		else if (jdkLog.isLoggable(Level.WARNING)) {
-			actualLevel = "WARNING";
-		}
-		else if (jdkLog.isLoggable(Level.SEVERE)) {
-			actualLevel = "SEVERE";
-		}
-		else if (!jdkLog.isLoggable(Level.SEVERE)) {
-			actualLevel = "OFF";
-		}
-		else {
-			actualLevel = "INFO";
-		}
+		Level expectedLevel, java.util.logging.Logger jdkLog) {
 
 		Assert.assertEquals(
-			"Logging level is wrong", expectedLevel, actualLevel);
+			"Logging level is wrong", expectedLevel, jdkLog.getLevel());
 	}
 
 	private void _assertLogEnable() {
