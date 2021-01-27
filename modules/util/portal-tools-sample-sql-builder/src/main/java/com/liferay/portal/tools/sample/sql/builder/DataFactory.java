7826042682666f2519a67d4dcc3d638197fb3c8b
lily.chi <lily.chi@liferay.com>
2021-01-27 15:59:44 +0800

    LPS-127055 Init the _userPersonalSiteGroupId field during creating userPersonalSiteGroupModel, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -355,7 +355,6 @@ public class DataFactory {
 		_defaultJournalDDMStructureId = _counter.get();
 		_defaultJournalDDMStructureVersionId = _counter.get();
 		_defaultJournalDDMTemplateId = _counter.get();
-		_userPersonalSiteGroupId = _counter.get();
 
 		_dlDDMStructureContent = _readFile("ddm_structure_basic_document.json");
 		_dlDDMStructureLayoutContent = _readFile(
@@ -3912,6 +3911,8 @@ public class DataFactory {
 	}
 
 	public GroupModel newUserPersonalSiteGroupModel() {
+		_userPersonalSiteGroupId = _counter.get();
+
 		return newGroupModel(
 			_userPersonalSiteGroupId, getClassNameId(UserPersonalSite.class),
 			_defaultUserId, GroupConstants.USER_PERSONAL_SITE, false);
@@ -5332,7 +5333,7 @@ public class DataFactory {
 	private RoleModel _siteMemberRoleModel;
 	private final SimpleCounter _socialActivityCounter;
 	private final SimpleCounter _timeCounter;
-	private final long _userPersonalSiteGroupId;
+	private long _userPersonalSiteGroupId;
 	private RoleModel _userRoleModel;
 	private final SimpleCounter _userScreenNameCounter;
 
