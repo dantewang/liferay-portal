26662d23600a190632fcc79504b2ae5a0644c845
Eric Yan <eric.yan@liferay.com>
2021-03-02 17:16:15 -0800

    LPS-X Store the virtual host default languageId instead of directly assigning it as the i18n languageId

--- portal-impl/src/com/liferay/portal/util/PortalInstances.java
+++ portal-impl/src/com/liferay/portal/util/PortalInstances.java
@@ -481,7 +481,7 @@ public class PortalInstances {
 					}
 
 					httpServletRequest.setAttribute(
-						WebKeys.I18N_LANGUAGE_ID, languageId);
+						WebKeys.VIRTUAL_HOST_DEFAULT_LANGUAGE_ID, languageId);
 				}
 			}
 
