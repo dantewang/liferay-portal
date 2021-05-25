7d1e802607358508750c5882a7ea1e7fd7879bbd
lily.chi <lily.chi@liferay.com>
2021-05-25 16:51:10 +0800

    Increase the currentId of Layout counter

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -2374,7 +2374,7 @@ public class DataFactory {
 		}
 
 		counterModels.add(
-			_newCounterModel(Layout.class.getName(), totalLayoutId + 1));
+			_newCounterModel(Layout.class.getName(), _counter.get() + totalLayoutId + 1));
 
 		return counterModels;
 	}
