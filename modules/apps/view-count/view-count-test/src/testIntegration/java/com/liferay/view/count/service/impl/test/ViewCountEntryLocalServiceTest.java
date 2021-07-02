733ff3e39b575b6a4bfb4ae049c5fd7d29b563c8
Dante Wang <dante.wang@liferay.com>
2021-07-02 15:36:51 +0800

    LPS-X Fix failure of ViewCountEntryLocalServiceTest caused by double logging (BatchingBatch)

--- modules/apps/view-count/view-count-test/src/testIntegration/java/com/liferay/view/count/service/impl/test/ViewCountEntryLocalServiceTest.java
+++ modules/apps/view-count/view-count-test/src/testIntegration/java/com/liferay/view/count/service/impl/test/ViewCountEntryLocalServiceTest.java
@@ -44,6 +44,7 @@ import java.util.Objects;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.FutureTask;
 
+import org.hibernate.engine.jdbc.batch.internal.BatchingBatch;
 import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 
 import org.junit.Assert;
@@ -98,8 +99,10 @@ public class ViewCountEntryLocalServiceTest {
 			_viewCountEntryFinder, "_sessionFactory",
 			_createSessionFactoryProxy(sessionFactory, cyclicBarrier));
 
-		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
-				SqlExceptionHelper.class.getName(), LoggerTestUtil.OFF)) {
+		try (LogCapture logCapture1 = LoggerTestUtil.configureLog4JLogger(
+				SqlExceptionHelper.class.getName(), LoggerTestUtil.OFF);
+			LogCapture logCapture2 = LoggerTestUtil.configureLog4JLogger(
+				BatchingBatch.class.getName(), LoggerTestUtil.OFF)) {
 
 			FutureTask<Void> futureTask = new FutureTask<>(
 				() -> {
