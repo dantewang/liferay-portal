b58248495c9ddb3e1a2e449f16571b7eeebcc006
Dante Wang <dante.wang@liferay.com>
2021-07-13 17:25:03 +0800

    LPS-X Temp: skip some permission initializations 2

--- modules/apps/account/account-service/src/main/java/com/liferay/account/internal/instance/lifecycle/AddDefaultAccountRolesPortalInstanceLifecycleListener.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/instance/lifecycle/AddDefaultAccountRolesPortalInstanceLifecycleListener.java
@@ -41,7 +41,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Drew Brokke
  */
 @Component(
-	immediate = true, property = "service.ranking:Integer=200",
+	enabled = false, immediate = true, property = "service.ranking:Integer=200",
 	service = PortalInstanceLifecycleListener.class
 )
 public class AddDefaultAccountRolesPortalInstanceLifecycleListener
