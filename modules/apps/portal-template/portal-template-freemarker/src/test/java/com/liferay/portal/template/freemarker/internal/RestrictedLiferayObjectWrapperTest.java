dantewang/liferay-portal

commit 474a24207d4c92f804ba99c63815684b1889b909

Revert "LPS-138387 temp default for now"
This reverts commit 4c3862d7b511114e8a6ad8768602f5c1cdb9ab89.

portal-kernel/src/com/liferay/announcements/kernel/model/packageinfo
@@ -1,1 +1,1 @@
-version 2.3.0
+version 3.0.0

portal-kernel/src/com/liferay/asset/kernel/model/adapter/packageinfo
@@ -1,1 +1,1 @@
-version 2.2.0
+version 3.0.0

portal-kernel/src/com/liferay/asset/kernel/model/packageinfo
@@ -1,1 +1,1 @@
-version 4.2.0
+version 5.0.0

portal-kernel/src/com/liferay/counter/kernel/model/packageinfo
@@ -1,1 +1,1 @@
-version 2.3.0
+version 3.0.0

portal-kernel/src/com/liferay/document/library/kernel/model/packageinfo
@@ -1,1 +1,1 @@
-version 3.5.0
+version 4.0.0

portal-kernel/src/com/liferay/expando/kernel/model/adapter/packageinfo
@@ -1,1 +1,1 @@
-version 2.4.0
+version 3.0.0

portal-kernel/src/com/liferay/expando/kernel/model/packageinfo
@@ -1,1 +1,1 @@
-version 3.5.0
+version 4.0.0

portal-kernel/src/com/liferay/exportimport/kernel/model/packageinfo
@@ -1,1 +1,1 @@
-version 2.3.0
+version 3.0.0

commit 0aeeb037846eba4d1d13d974309e6e5e9dbdc011

LPS-137608 Solve compile error which was caused by the commit 474a24207d4c92f804ba99c63815684b1889b909.

modules/apps/portal-template/portal-template-freemarker/src/test/java/com/liferay/portal/template/freemarker/internal/RestrictedLiferayObjectWrapperTest.java
@@ -499,6 +499,11 @@ public class RestrictedLiferayObjectWrapperTest
 		}
 
 		@Override
+		public TestBaseModel cloneWithOriginalValues() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
 		public int compareTo(TestBaseModel testBaseModel) {
 			return 0;
 		}
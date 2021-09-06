0aeeb037846eba4d1d13d974309e6e5e9dbdc011
hai.yu <8219754@liferay.com>
2021-09-06 11:21:42 +0800

    LPS-137608 Solve compile error which was caused by the commit 474a24207d4c92f804ba99c63815684b1889b909.

--- modules/apps/portal-template/portal-template-freemarker/src/test/java/com/liferay/portal/template/freemarker/internal/RestrictedLiferayObjectWrapperTest.java
+++ modules/apps/portal-template/portal-template-freemarker/src/test/java/com/liferay/portal/template/freemarker/internal/RestrictedLiferayObjectWrapperTest.java
@@ -499,6 +499,11 @@ public class RestrictedLiferayObjectWrapperTest
 		}
 
 		@Override
+		public TestBaseModel cloneWithOriginalValues() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
 		public int compareTo(TestBaseModel testBaseModel) {
 			return 0;
 		}
