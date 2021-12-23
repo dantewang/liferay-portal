db082a3c85192cac2c345fa81877dc39c4ecad13
Dante Wang <dante.wang@liferay.com>
2021-12-23 11:04:54 +0800

    LPS-X SF, inline fields

--- modules/apps/static/portal-file-install/portal-file-install-impl/src/main/java/com/liferay/portal/file/install/internal/DirectoryWatcher.java
+++ modules/apps/static/portal-file-install/portal-file-install-impl/src/main/java/com/liferay/portal/file/install/internal/DirectoryWatcher.java
@@ -93,17 +93,8 @@ public class DirectoryWatcher extends Thread implements BundleListener {
 		_activeLevel = GetterUtil.getInteger(
 			bundleContext.getProperty(ACTIVE_LEVEL));
 		_filter = bundleContext.getProperty(FILTER);
-		_noInitialDelay =
-			PropsValues.MODULE_FRAMEWORK_FILE_INSTALL_NO_INITIAL_DELAY;
-		_startBundles =
-			PropsValues.MODULE_FRAMEWORK_FILE_INSTALL_BUNDLES_START_NEW;
 		_systemBundle = bundleContext.getBundle(
 			Constants.SYSTEM_BUNDLE_LOCATION);
-		_useStartActivationPolicy =
-			PropsValues.
-				MODULE_FRAMEWORK_FILE_INSTALL_BUNDLES_START_ACTIVATION_POLICY;
-		_useStartTransient =
-			PropsValues.MODULE_FRAMEWORK_FILE_INSTALL_BUNDLES_START_TRANSIENT;
 
 		Set<String> dirs = new LinkedHashSet<>();
 
@@ -218,7 +209,7 @@ public class DirectoryWatcher extends Thread implements BundleListener {
 
 	@Override
 	public void run() {
-		if (!_noInitialDelay) {
+		if (!PropsValues.MODULE_FRAMEWORK_FILE_INSTALL_NO_INITIAL_DELAY) {
 			try {
 				Thread.sleep(PropsValues.MODULE_FRAMEWORK_AUTO_DEPLOY_INTERVAL);
 			}
@@ -281,7 +272,7 @@ public class DirectoryWatcher extends Thread implements BundleListener {
 
 	@Override
 	public void start() {
-		if (_noInitialDelay) {
+		if (PropsValues.MODULE_FRAMEWORK_FILE_INSTALL_NO_INITIAL_DELAY) {
 			_initializeCurrentManagedBundles();
 
 			Set<File> files = _scanner.scan(true);
@@ -1053,7 +1044,7 @@ public class DirectoryWatcher extends Thread implements BundleListener {
 			}
 		}
 
-		if (_startBundles) {
+		if (PropsValues.MODULE_FRAMEWORK_FILE_INSTALL_BUNDLES_START_NEW) {
 			FrameworkStartLevel frameworkStartLevel = _systemBundle.adapt(
 				FrameworkStartLevel.class);
 
@@ -1139,7 +1130,8 @@ public class DirectoryWatcher extends Thread implements BundleListener {
 
 					if ((state != Bundle.STARTING) &&
 						(state != Bundle.ACTIVE) &&
-						(_useStartTransient ||
+						(PropsValues.
+							MODULE_FRAMEWORK_FILE_INSTALL_BUNDLES_START_TRANSIENT ||
 						 bundleStartLevel.isPersistentlyStarted()) &&
 						(frameworkStartLevel.getStartLevel() >=
 							bundleStartLevel.getStartLevel())) {
@@ -1157,18 +1149,22 @@ public class DirectoryWatcher extends Thread implements BundleListener {
 		BundleStartLevel bundleStartLevel = bundle.adapt(
 			BundleStartLevel.class);
 
-		if (_startBundles && (bundle.getState() != Bundle.UNINSTALLED) &&
-			!_isFragment(bundle) &&
+		if (PropsValues.MODULE_FRAMEWORK_FILE_INSTALL_BUNDLES_START_NEW &&
+			(bundle.getState() != Bundle.UNINSTALLED) && !_isFragment(bundle) &&
 			(_frameworkStartLevel >= bundleStartLevel.getStartLevel())) {
 
 			try {
 				int options = 0;
 
-				if (_useStartTransient) {
+				if (PropsValues.
+						MODULE_FRAMEWORK_FILE_INSTALL_BUNDLES_START_TRANSIENT) {
+
 					options = Bundle.START_TRANSIENT;
 				}
 
-				if (_useStartActivationPolicy) {
+				if (PropsValues.
+						MODULE_FRAMEWORK_FILE_INSTALL_BUNDLES_START_ACTIVATION_POLICY) {
+
 					options |= Bundle.START_ACTIVATION_POLICY;
 				}
 
@@ -1222,7 +1218,9 @@ public class DirectoryWatcher extends Thread implements BundleListener {
 	}
 
 	private void _stopTransient(Bundle bundle) throws BundleException {
-		if (_startBundles && !_isFragment(bundle)) {
+		if (PropsValues.MODULE_FRAMEWORK_FILE_INSTALL_BUNDLES_START_NEW &&
+			!_isFragment(bundle)) {
+
 			bundle.stop(Bundle.STOP_TRANSIENT);
 		}
 	}
@@ -1385,14 +1383,10 @@ public class DirectoryWatcher extends Thread implements BundleListener {
 	private final String _filter;
 	private int _frameworkStartLevel;
 	private final Map<File, Artifact> _installationFailures = new HashMap<>();
-	private final boolean _noInitialDelay;
 	private final Set<File> _processingFailures = new HashSet<>();
 	private final Scanner _scanner;
-	private final boolean _startBundles;
 	private final AtomicBoolean _stateChanged = new AtomicBoolean();
 	private final Bundle _systemBundle;
-	private final boolean _useStartActivationPolicy;
-	private final boolean _useStartTransient;
 	private final List<File> _watchedDirs;
 
 }
