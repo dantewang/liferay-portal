f7cc3ed2d0ef5c4e6d3fd5dce3c1ce2a4f1048d2
hai.yu <8219754@liferay.com>
2022-01-17 17:14:13 +0800

    LPS-X When transaction Callback executes, TransactionDefinition is PROPAGATION_SUPPORTS and autoCommit of connection is true. For POSTGRESQL, at that time, when resultSet.getClob() executes, the error "org.postgresql.util.PSQLException: Large Objects may not be used in auto-commit mode." will happen. The reason is that the autoCommit of connection is true. Please see org.postgresql.largeobject.LargeObjectManager.open(). For example, the issue will happen when CompanyLocalServiceImpl#registerCompany executes. The commit sets SqlTypeDescriptor when the database is POSTGRESQL. And then only copy DEFAULT of org.hibernate.type.descriptor.sql.ClobTypeDescriptor as one subClass and let subClass to replace DEFAULT(no logic change). The next commit will add the fix in the subClass.

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/StringClobType.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/StringClobType.java
@@ -14,12 +14,30 @@
 
 package com.liferay.portal.dao.orm.hibernate;
 
+import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.lang.reflect.Method;
+
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import java.util.Objects;
 
 import org.hibernate.type.MaterializedClobType;
+import org.hibernate.type.descriptor.ValueExtractor;
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.type.descriptor.java.JavaTypeDescriptor;
 import org.hibernate.type.descriptor.java.StringTypeDescriptor;
+import org.hibernate.type.descriptor.sql.BasicBinder;
+import org.hibernate.type.descriptor.sql.BasicExtractor;
+import org.hibernate.type.descriptor.sql.ClobTypeDescriptor;
 
 /**
  * @author Shuyang Zhou
@@ -28,6 +46,10 @@ import org.hibernate.type.descriptor.java.StringTypeDescriptor;
 public class StringClobType extends MaterializedClobType {
 
 	public StringClobType() {
+		if (_dbType.equals(DBType.POSTGRESQL)) {
+			setSqlTypeDescriptor(new ClobTypeDescriptorImpl());
+		}
+
 		setJavaTypeDescriptor(
 			new StringTypeDescriptor() {
 
@@ -46,6 +68,83 @@ public class StringClobType extends MaterializedClobType {
 				}
 
 			});
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(StringClobType.class);
+
+	private static final DBType _dbType;
+
+	static {
+		DB db = DBManagerUtil.getDB();
+
+		_dbType = db.getDBType();
+	}
+
+	private class ClobTypeDescriptorImpl extends ClobTypeDescriptor {
+
+		@Override
+		public boolean canBeRemapped() {
+			return false;
+		}
+
+		@Override
+		public <X> BasicBinder<X> getClobBinder(
+			JavaTypeDescriptor<X> javaTypeDescriptor) {
+
+			try {
+				Method method = ReflectionUtil.getDeclaredMethod(
+					ClobTypeDescriptor.class, "getClobBinder",
+					JavaTypeDescriptor.class);
+
+				return (BasicBinder<X>)method.invoke(
+					DEFAULT, javaTypeDescriptor);
+			}
+			catch (Exception exception) {
+				_log.error(exception, exception);
+
+				return null;
+			}
+		}
+
+		@Override
+		public <X> ValueExtractor<X> getExtractor(
+			final JavaTypeDescriptor<X> javaTypeDescriptor) {
+
+			return new BasicExtractor<X>(javaTypeDescriptor, this) {
+
+				@Override
+				protected X doExtract(
+						CallableStatement statement, int index,
+						WrapperOptions options)
+					throws SQLException {
+
+					return javaTypeDescriptor.wrap(
+						statement.getClob(index), options);
+				}
+
+				@Override
+				protected X doExtract(
+						CallableStatement statement, String name,
+						WrapperOptions options)
+					throws SQLException {
+
+					return javaTypeDescriptor.wrap(
+						statement.getClob(name), options);
+				}
+
+				@Override
+				protected X doExtract(
+						ResultSet resultSet, String name,
+						WrapperOptions options)
+					throws SQLException {
+
+					return javaTypeDescriptor.wrap(
+						resultSet.getClob(name), options);
+				}
+
+			};
+		}
+
 	}
 
 }
