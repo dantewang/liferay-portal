d9e7e633a1f0bd9ee4ab6993fbc68f184fa257af
Janis Zhang <janis.zhang@liferay.com>
2022-02-18 16:00:25 +0800

    LPS-96118 Replace map proxy creation method using getProxyProviderFunction

--- modules/apps/frontend-editor/frontend-editor-taglib/src/main/java/com/liferay/frontend/editor/taglib/servlet/taglib/EditorTag.java
+++ modules/apps/frontend-editor/frontend-editor-taglib/src/main/java/com/liferay/frontend/editor/taglib/servlet/taglib/EditorTag.java
@@ -46,6 +46,7 @@ import java.lang.reflect.Method;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
@@ -382,9 +383,7 @@ public class EditorTag extends BaseValidatorTagSupport {
 
 		setNamespacedAttribute(
 			httpServletRequest, "data",
-			ProxyUtil.newProxyInstance(
-				ClassLoader.getSystemClassLoader(), new Class<?>[] {Map.class},
-				new LazyDataInvocationHandler()));
+				_mapProxyProviderFunction.apply(new LazyDataInvocationHandler()));
 	}
 
 	private String _getConfigKey() {
@@ -547,6 +546,10 @@ public class EditorTag extends BaseValidatorTagSupport {
 		PropsKeys.EDITOR_WYSIWYG_DEFAULT);
 
 	private static final String _TOOLBAR_SET_DEFAULT = "liferay";
+
+	private static final Function<InvocationHandler, Map<?, ?>>
+			_mapProxyProviderFunction = ProxyUtil.getProxyProviderFunction(
+			Map.class);
 
 	private boolean _allowBrowseDocuments = true;
 	private boolean _autoCreate = true;
