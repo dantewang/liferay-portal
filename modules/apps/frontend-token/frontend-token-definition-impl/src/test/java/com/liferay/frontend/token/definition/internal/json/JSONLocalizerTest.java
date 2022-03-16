2e8064782315d1148a5ccaa0997672fa1deafe60
lily.chi <lily.chi@liferay.com>
2022-03-16 18:33:18 +0800

    LPS-149061 Update the JSONLocalizerTest

--- modules/apps/frontend-token/frontend-token-definition-impl/src/test/java/com/liferay/frontend/token/definition/internal/json/JSONLocalizerTest.java
+++ modules/apps/frontend-token/frontend-token-definition-impl/src/test/java/com/liferay/frontend/token/definition/internal/json/JSONLocalizerTest.java
@@ -19,6 +19,7 @@ import com.liferay.frontend.token.definition.internal.FrontendTokenDefinitionReg
 import com.liferay.portal.json.JSONFactoryImpl;
 import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoader;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -66,9 +67,11 @@ public class JSONLocalizerTest {
 			_FRONTEND_TOKEN_DEFINITION_JSON, new JSONFactoryImpl(),
 			resourceBundleLoader, "theme_id");
 
+		JSONObject actualJSONObject = jsonLocalizer.getJSON(LocaleUtil.ENGLISH);
+
 		Assert.assertEquals(
-			_TRANSLATED_FRONTEND_TOKEN_DEFINITION_JSON,
-			jsonLocalizer.getJSON(LocaleUtil.ENGLISH));
+			_TRANSLATED_FRONTEND_TOKEN_DEFINITION_JSON_OBJECT.toMap(),
+			actualJSONObject.toMap());
 	}
 
 	@Test
@@ -77,9 +80,11 @@ public class JSONLocalizerTest {
 			_FRONTEND_TOKEN_DEFINITION_JSON, new JSONFactoryImpl(),
 			Mockito.mock(ResourceBundleLoader.class), "theme_id");
 
+		JSONObject actualJSONObject = jsonLocalizer.getJSON(LocaleUtil.SPAIN);
+
 		Assert.assertEquals(
-			_FRONTEND_TOKEN_DEFINITION_JSON,
-			jsonLocalizer.getJSON(LocaleUtil.SPAIN));
+			_FRONTEND_TOKEN_DEFINITION_JSON_OBJECT.toMap(),
+			actualJSONObject.toMap());
 	}
 
 	@Test
@@ -88,8 +93,11 @@ public class JSONLocalizerTest {
 			_FRONTEND_TOKEN_DEFINITION_JSON, new JSONFactoryImpl(),
 			Mockito.mock(ResourceBundleLoader.class), "theme_id");
 
+		JSONObject actualJSONObject = jsonLocalizer.getJSON(null);
+
 		Assert.assertEquals(
-			_FRONTEND_TOKEN_DEFINITION_JSON, jsonLocalizer.getJSON(null));
+			_FRONTEND_TOKEN_DEFINITION_JSON_OBJECT.toMap(),
+			actualJSONObject.toMap());
 	}
 
 	@Test
@@ -110,14 +118,19 @@ public class JSONLocalizerTest {
 			_FRONTEND_TOKEN_DEFINITION_JSON, new JSONFactoryImpl(),
 			resourceBundleLoader, "theme_id");
 
-		Assert.assertSame(
-			jsonLocalizer.getJSON(LocaleUtil.ENGLISH),
-			jsonLocalizer.getJSON(LocaleUtil.ENGLISH));
+		JSONObject expectJSONObject = jsonLocalizer.getJSON(LocaleUtil.ENGLISH);
+
+		JSONObject actualJSONObject = jsonLocalizer.getJSON(LocaleUtil.ENGLISH);
+
+		Assert.assertEquals(expectJSONObject.toMap(), actualJSONObject.toMap());
 	}
 
 	private static final String _FRONTEND_TOKEN_DEFINITION_JSON;
 
-	private static final String _TRANSLATED_FRONTEND_TOKEN_DEFINITION_JSON;
+	private static final JSONObject _FRONTEND_TOKEN_DEFINITION_JSON_OBJECT;
+
+	private static final JSONObject
+		_TRANSLATED_FRONTEND_TOKEN_DEFINITION_JSON_OBJECT;
 
 	static {
 		JSONFactory jsonFactory = new JSONFactoryImpl();
@@ -126,8 +139,11 @@ public class JSONLocalizerTest {
 			"dependencies/frontend-token-definition.json");
 
 		try (InputStream inputStream = url.openStream()) {
+			_FRONTEND_TOKEN_DEFINITION_JSON_OBJECT =
+				jsonFactory.createJSONObject(StringUtil.read(inputStream));
+
 			_FRONTEND_TOKEN_DEFINITION_JSON = jsonFactory.looseSerializeDeep(
-				jsonFactory.createJSONObject(StringUtil.read(inputStream)));
+				_FRONTEND_TOKEN_DEFINITION_JSON_OBJECT);
 		}
 		catch (IOException | JSONException exception) {
 			throw new RuntimeException(exception);
@@ -137,9 +153,8 @@ public class JSONLocalizerTest {
 			"dependencies/translated-frontend-token-definition.json");
 
 		try (InputStream inputStream = url.openStream()) {
-			_TRANSLATED_FRONTEND_TOKEN_DEFINITION_JSON =
-				jsonFactory.looseSerializeDeep(
-					jsonFactory.createJSONObject(StringUtil.read(inputStream)));
+			_TRANSLATED_FRONTEND_TOKEN_DEFINITION_JSON_OBJECT =
+				jsonFactory.createJSONObject(StringUtil.read(inputStream));
 		}
 		catch (IOException | JSONException exception) {
 			throw new RuntimeException(exception);
