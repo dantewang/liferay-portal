96e843059e53e57d4376af1df8ed9f15620059fe
Janis Zhang <janis.zhang@liferay.com>
2022-03-16 20:27:32 +0800

    LPS-148593 SF, Add LiferayUnitTestRule in JSONWebServiceTest

--- portal-impl/test/unit/com/liferay/portal/jsonwebservice/JSONWebServiceTest.java
+++ portal-impl/test/unit/com/liferay/portal/jsonwebservice/JSONWebServiceTest.java
@@ -32,6 +32,7 @@ import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.ProxyFactory;
 import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -40,6 +41,8 @@ import java.util.Collections;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 import org.mockito.Mockito;
@@ -50,6 +53,11 @@ import org.springframework.mock.web.MockHttpServletRequest;
  * @author Igor Spasic
  */
 public class JSONWebServiceTest extends BaseJSONWebServiceTestCase {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
