962de237cb0a17c4cf6726df35c70126143671c8
Dante Wang <dante.wang@liferay.com>
2022-04-06 15:19:55 +0800

    LPS-150076 SF

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactory.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactory.java
@@ -152,12 +152,12 @@ public class SearchBarPortletDisplayContextFactory {
 				true);
 		}
 
-		String paginationStartParameterName =
-			searchRequest.getPaginationStartParameterName();
-
 		searchBarPortletDisplayContext.setPaginationStartParameterName(
-			(paginationStartParameterName != null) ?
-				paginationStartParameterName : StringPool.BLANK);
+			Optional.ofNullable(
+				searchRequest.getPaginationStartParameterName()
+			).orElse(
+				StringPool.BLANK
+			));
 
 		String scopeParameterName = _getScopeParameterName(
 			portletPreferencesLookup, searchBarPrecedenceHelper,
@@ -168,19 +168,17 @@ public class SearchBarPortletDisplayContextFactory {
 			portletSharedSearchResponse.getParameter(
 				scopeParameterName, _renderRequest);
 
-		String scopeParameterValue = scopeParameterValueOptional.orElse(null);
-
 		searchBarPortletDisplayContext.setScopeParameterName(
 			scopeParameterName);
+
 		searchBarPortletDisplayContext.setScopeParameterValue(
-			(scopeParameterValue != null) ? scopeParameterValue :
-				StringPool.BLANK);
+			scopeParameterValueOptional.orElse(StringPool.BLANK));
 		searchBarPortletDisplayContext.setSearchBarPortletInstanceConfiguration(
 			searchBarPortletInstanceConfiguration);
 
 		_setSelectedSearchScope(
 			searchBarPortletDisplayContext, searchScopePreference,
-			scopeParameterValue);
+			scopeParameterValueOptional.orElse(null));
 
 		if (searchBarPortletPreferences.isInvisible()) {
 			searchBarPortletDisplayContext.setRenderNothing(true);
