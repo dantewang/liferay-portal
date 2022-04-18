43fc53c1cb6a70ea3e3a50714ccfc874f26d640d
Dante Wang <dante.wang@liferay.com>
2022-04-18 12:59:24 +0800

    LPS-X SF, move package

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/log/UpgradeReportLogListener.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/log/UpgradeReportLogListener.java
@@ -1,3 +1,19 @@
+/*
+ *
+  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+  *
+  * This library is free software; you can redistribute it and/or modify it under
+  * the terms of the GNU Lesser General Public License as published by the Free
+  * Software Foundation; either version 2.1 of the License, or (at your option)
+  * any later version.
+  *
+  * This library is distributed in the hope that it will be useful, but WITHOUT
+  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+  * details.
+
+ */
+
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
@@ -12,7 +28,7 @@
  * details.
  */
 
-package com.liferay.portal.upgrade.internal.apache.logging.log4j.core;
+package com.liferay.portal.upgrade.internal.log;
 
 import com.liferay.portal.kernel.log.LogListener;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
