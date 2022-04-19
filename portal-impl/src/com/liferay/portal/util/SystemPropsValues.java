3fe983b0ee5e113be5fa47e36feec6d5006f70f0
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-04-19 11:02:08 +0800

    LPS-151639 add two util classes and move properties: module.framework.auto.deploy.dirs

---
+++ portal-impl/src/com/liferay/portal/util/SystemPropsValues.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.util;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.SystemProperties;
+import com.liferay.portal.kernel.util.SystemPropsKeys;
+
+/**
+ * @author Jiaxu Wei
+ */
+public class SystemPropsValues {
+
+	public static final String[] MODULE_FRAMEWORK_AUTO_DEPLOY_DIRS =
+		StringUtil.split(SystemProperties.get(SystemPropsKeys.MODULE_FRAMEWORK_AUTO_DEPLOY_DIRS));
+}
