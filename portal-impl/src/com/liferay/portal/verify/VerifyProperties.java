4ba2ca2376505ea472086563530104c3cbfd1b79
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-04-28 10:45:16 +0800

    LPS-151639 moved property 'module.framework.static.jars', replaced PropsValues  with SystemPropsValues

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -338,6 +338,7 @@ public class VerifyProperties extends VerifyProcess {
 			"module.framework.services.ignored.interfaces"
 		},
 		{"module.framework.state.dir", "module.framework.state.dir"},
+		{"module.framework.static.jars", "module.framework.static.jars"},
 		{"resource.repositories.root", "resource.repositories.root"}
 	};
 
