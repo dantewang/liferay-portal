f95f2967c171db3bd9534ae6af6f25a0dda48a07
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-04-28 11:15:40 +0800

    LPS-151639 moved property 'module.framework.system.packages.extra', replaced PropsValues  with SystemPropsValues

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -343,6 +343,10 @@ public class VerifyProperties extends VerifyProcess {
 			"module.framework.stop.wait.timeout",
 			"module.framework.stop.wait.timeout"
 		},
+		{
+			"module.framework.system.packages.extra",
+			"module.framework.system.packages.extra"
+		},
 		{"resource.repositories.root", "resource.repositories.root"}
 	};
 
