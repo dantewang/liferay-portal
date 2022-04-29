ea33af02e6767bd8bf3e88096e9a688fc140cb7e
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-04-29 16:10:04 +0800

    LPS-151639 add a new method _decode and added a new variable _log

--- portal-kernel/src/com/liferay/portal/kernel/util/EnvPropertiesUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/EnvPropertiesUtil.java
@@ -15,6 +15,9 @@
 package com.liferay.portal.kernel.util;
 
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -48,6 +51,60 @@ public class EnvPropertiesUtil {
 			throw new ExceptionInInitializerError(exception);
 		}
 	}
+
+	private static String _decode(String s) {
+		int index = -1;
+		int openUnderLine = -1;
+		int position = 0;
+		StringBundler sb = new StringBundler();
+
+		while ((index = s.indexOf(CharPool.UNDERLINE, index + 1)) != -1) {
+			if (openUnderLine == -1) {
+				sb.append(s.substring(position, index));
+
+				openUnderLine = index;
+				position = index;
+
+				continue;
+			}
+
+			String encoded = s.substring(openUnderLine + 1, index);
+
+			Character character = _charPoolChars.get(
+				StringUtil.toUpperCase(encoded));
+
+			if (character == null) {
+				int value = GetterUtil.get(encoded, -1);
+
+				if (Character.isDefined(value)) {
+					sb.append(new String(Character.toChars(value)));
+				}
+				else {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							StringBundler.concat(
+								"Unable to decode part \"", encoded,
+								"\" from \"", s, "\", preserve it literally"));
+					}
+
+					sb.append(s.substring(openUnderLine, index + 1));
+				}
+			}
+			else {
+				sb.append(character);
+			}
+
+			openUnderLine = -1;
+			position = index + 1;
+		}
+
+		sb.append(s.substring(position));
+
+		return sb.toString();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EnvPropertiesUtil.class);
 
 	private static final Map<String, Character> _charPoolChars =
 		_getCharPoolChars();
