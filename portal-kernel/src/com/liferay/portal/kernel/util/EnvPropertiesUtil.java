39cc9a6b42e9c0ca283b56c1f64a27e5417f269b
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-04-29 16:01:43 +0800

    LPS-151639 added a new method _getCharPoolChars and added a new variable _charPoolChars

--- portal-kernel/src/com/liferay/portal/kernel/util/EnvPropertiesUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/EnvPropertiesUtil.java
@@ -14,8 +14,42 @@
 
 package com.liferay.portal.kernel.util;
 
+import com.liferay.petra.string.CharPool;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Jiaxu Wei
  */
 public class EnvPropertiesUtil {
+
+	private static Map<String, Character> _getCharPoolChars() {
+		try {
+			Map<String, Character> charPoolChars = new HashMap<>();
+
+			for (Field field : CharPool.class.getFields()) {
+				if (Modifier.isStatic(field.getModifiers()) &&
+					(field.getType() == char.class)) {
+
+					charPoolChars.put(
+						StringUtil.removeChar(
+							field.getName(), CharPool.UNDERLINE),
+						field.getChar(null));
+				}
+			}
+
+			return charPoolChars;
+		}
+		catch (Exception exception) {
+			throw new ExceptionInInitializerError(exception);
+		}
+	}
+
+	private static final Map<String, Character> _charPoolChars =
+		_getCharPoolChars();
+
 }
