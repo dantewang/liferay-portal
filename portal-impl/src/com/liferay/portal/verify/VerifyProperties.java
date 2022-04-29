b4a77a5463c6e2e9411c6bc67a002d5e222ec740
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-04-29 19:37:06 +0800

    LPS-151639 moved property 'module.framework.state.dir', replaced Props*  with SystemProps*

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -303,7 +303,8 @@ public class VerifyProperties extends VerifyProcess {
 		},
 		{"module.framework.modules.dir", "module.framework.modules.dir"},
 		{"module.framework.portal.dir", "module.framework.portal.dir"},
-		{"module.framework.war.dir", "module.framework.war.dir"}
+		{"module.framework.war.dir", "module.framework.war.dir"},
+		{"module.framework.state.dir", "module.framework.state.dir"}
 	};
 
 	private static final String[][] _MIGRATED_SYSTEM_KEYS = {
