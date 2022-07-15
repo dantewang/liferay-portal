63e7a52a3fac1209b53f92a6dc00d9f77c6e3ef6
Janis Zhang <janis.zhang@liferay.com>
2022-07-15 12:31:46 +0800

    LPS-X Create ClusterSampleCacheReplicationPortlet for test replication portlet

---
+++ modules/apps/portal/portal-cluster-multiple-sample-web/src/main/java/com/liferay/portal/cluster/multiple/sample/web/internal/portlet/ClusterSampleCacheReplicationPortlet.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cluster.multiple.sample.web.internal.portlet;
+
+import com.liferay.portal.cluster.multiple.sample.web.internal.constants.ClusterPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Janis Zhang
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=ClusterSampleCache",
+		"javax.portlet.init-param.mvc-action-command-package-prefix=com.liferay.portal.cluster.multiple.sample.web.internal.portlet.action",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=/cluster_cache_replication/cache.jsp",
+		"javax.portlet.name=" + ClusterPortletKeys.CACHE,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.version=3.0"
+	},
+	service = Portlet.class
+)
+public class ClusterSampleCacheReplicationPortlet extends MVCPortlet {
+}
