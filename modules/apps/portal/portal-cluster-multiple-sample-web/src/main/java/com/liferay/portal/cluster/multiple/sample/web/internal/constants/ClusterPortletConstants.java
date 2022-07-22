1e73ee80cce6e65ab24ef31b67c5892a3f2bd582
Janis Zhang <janis.zhang@liferay.com>
2022-07-22 17:55:55 +0800

    LPS-158075 Create ClusterPortletConstants add String PORTAL_CACHE_NAME and cache actions command

---
+++ modules/apps/portal/portal-cluster-multiple-sample-web/src/main/java/com/liferay/portal/cluster/multiple/sample/web/internal/constants/ClusterPortletConstants.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cluster.multiple.sample.web.internal.constants;
+
+/**
+ * @author Janis Zhang
+ */
+public class ClusterPortletConstants {
+
+	public static final String CMD = "cmd";
+
+	public static final String PORTAL_CACHE_NAME = "test.cache";
+
+	public static final String PUT = "put";
+
+	public static final String REMOVE = "remove";
+
+	public static final String REMOVE_ALL = "remove_all";
+
+}
