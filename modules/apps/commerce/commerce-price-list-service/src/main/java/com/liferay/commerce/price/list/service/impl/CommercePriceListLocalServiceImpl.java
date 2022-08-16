e7bef801acdf1ebe3cd4b7cc137a6a30941becae
Dante Wang <dante.wang@liferay.com>
2022-08-16 16:18:59 +0800

    LPS-159586 Clean up, no need to return an Optional here

--- modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/service/impl/CommercePriceListLocalServiceImpl.java
+++ modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/service/impl/CommercePriceListLocalServiceImpl.java
@@ -96,7 +96,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.LongStream;
 
 /**
@@ -565,7 +564,7 @@ public class CommercePriceListLocalServiceImpl
 	}
 
 	@Override
-	public Optional<CommercePriceList> getCommercePriceList(
+	public CommercePriceList getCommercePriceList(
 			long groupId, long commerceAccountId,
 			long[] commerceAccountGroupIds)
 		throws PortalException {
@@ -586,7 +585,7 @@ public class CommercePriceListLocalServiceImpl
 		CommercePriceList commercePriceList = _portalCache.get(cacheKey);
 
 		if (commercePriceList == _dummyCommercePriceList) {
-			return Optional.empty();
+			return null;
 		}
 
 		SearchContext searchContext = buildSearchContext(
@@ -603,7 +602,7 @@ public class CommercePriceListLocalServiceImpl
 		if (documents.isEmpty()) {
 			_portalCache.put(cacheKey, _dummyCommercePriceList);
 
-			return Optional.empty();
+			return null;
 		}
 
 		Document document = documents.get(0);
@@ -616,12 +615,12 @@ public class CommercePriceListLocalServiceImpl
 		if (commercePriceList == null) {
 			_portalCache.put(cacheKey, _dummyCommercePriceList);
 
-			return Optional.empty();
+			return null;
 		}
 
 		_portalCache.put(cacheKey, commercePriceList);
 
-		return Optional.of(commercePriceList);
+		return commercePriceList;
 	}
 
 	@Override
