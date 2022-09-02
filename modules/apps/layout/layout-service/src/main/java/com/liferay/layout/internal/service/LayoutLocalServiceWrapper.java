4447658f727792681edc86b6e171714af2cd045a
Dante Wang <dante.wang@liferay.com>
2022-09-02 16:20:50 +0800

    LPS-151623 Is this needed? When allowing to save friendly url entries for private and public layouts with the same url in the database, groupId + classNameId + url can not uniquely identify 1 friendly url entry, but 2. So we need to fetch all of these entries and match with the privateLayout boolean.

--- modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/service/LayoutLocalServiceWrapper.java
+++ modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/service/LayoutLocalServiceWrapper.java
@@ -14,13 +14,17 @@
 
 package com.liferay.layout.internal.service;
 
-import com.liferay.friendly.url.model.FriendlyURLEntry;
+import com.liferay.friendly.url.model.FriendlyURLEntryLocalization;
+import com.liferay.friendly.url.model.FriendlyURLEntryLocalizationTable;
 import com.liferay.friendly.url.service.FriendlyURLEntryLocalService;
+import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.portal.kernel.exception.NoSuchLayoutException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.service.ServiceWrapper;
 import com.liferay.portal.kernel.util.Portal;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -43,7 +47,7 @@ public class LayoutLocalServiceWrapper
 			return layout;
 		}
 
-		return _fetchLayoutByFriendlyURL(groupId, friendlyURL);
+		return _fetchLayoutByFriendlyURL(groupId, privateLayout, friendlyURL);
 	}
 
 	@Override
@@ -56,7 +60,8 @@ public class LayoutLocalServiceWrapper
 				groupId, privateLayout, friendlyURL);
 		}
 		catch (NoSuchLayoutException noSuchLayoutException) {
-			Layout layout = _fetchLayoutByFriendlyURL(groupId, friendlyURL);
+			Layout layout = _fetchLayoutByFriendlyURL(
+				groupId, privateLayout, friendlyURL);
 
 			if (layout != null) {
 				return layout;
@@ -66,16 +71,38 @@ public class LayoutLocalServiceWrapper
 		}
 	}
 
-	private Layout _fetchLayoutByFriendlyURL(long groupId, String friendlyURL) {
-		FriendlyURLEntry friendlyURLEntry =
-			_friendlyURLEntryLocalService.fetchFriendlyURLEntry(
-				groupId, _portal.getClassNameId(Layout.class), friendlyURL);
+	private Layout _fetchLayoutByFriendlyURL(
+		long groupId, boolean privateLayout, String friendlyURL) {
 
-		if (friendlyURLEntry != null) {
-			Layout layout = fetchLayout(friendlyURLEntry.getClassPK());
+		List<FriendlyURLEntryLocalization> friendlyURLEntryLocalizations =
+			_friendlyURLEntryLocalService.dslQuery(
+				DSLQueryFactoryUtil.select(
+				).from(
+					FriendlyURLEntryLocalizationTable.INSTANCE
+				).where(
+					FriendlyURLEntryLocalizationTable.INSTANCE.groupId.eq(
+						groupId
+					).and(
+						FriendlyURLEntryLocalizationTable.INSTANCE.classNameId.
+							eq(_portal.getClassNameId(Layout.class.getName()))
+					).and(
+						FriendlyURLEntryLocalizationTable.INSTANCE.urlTitle.eq(
+							friendlyURL)
+					)
+				));
 
-			if (layout != null) {
-				return layout;
+		for (FriendlyURLEntryLocalization friendlyURLEntryLocalization :
+				friendlyURLEntryLocalizations) {
+
+			if (friendlyURLEntryLocalization != null) {
+				Layout layout = fetchLayout(
+					friendlyURLEntryLocalization.getClassPK());
+
+				if ((layout != null) &&
+					(layout.isPrivateLayout() == privateLayout)) {
+
+					return layout;
+				}
 			}
 		}
 
