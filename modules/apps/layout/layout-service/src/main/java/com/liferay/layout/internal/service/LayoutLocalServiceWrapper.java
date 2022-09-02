6a72d96eec379b7d0fcb7a9bf4902f472afd8ee8
Tina Tian <tina.tian@liferay.com>
2022-09-01 18:28:00 -0700

    Revert LPS-112397

--- modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/service/LayoutLocalServiceWrapper.java
+++ modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/service/LayoutLocalServiceWrapper.java
@@ -16,11 +16,11 @@ package com.liferay.layout.internal.service;
 
 import com.liferay.friendly.url.model.FriendlyURLEntry;
 import com.liferay.friendly.url.service.FriendlyURLEntryLocalService;
-import com.liferay.layout.friendly.url.LayoutFriendlyURLEntryHelper;
 import com.liferay.portal.kernel.exception.NoSuchLayoutException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.util.Portal;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -43,7 +43,7 @@ public class LayoutLocalServiceWrapper
 			return layout;
 		}
 
-		return _fetchLayoutByFriendlyURL(groupId, privateLayout, friendlyURL);
+		return _fetchLayoutByFriendlyURL(groupId, friendlyURL);
 	}
 
 	@Override
@@ -56,8 +56,7 @@ public class LayoutLocalServiceWrapper
 				groupId, privateLayout, friendlyURL);
 		}
 		catch (NoSuchLayoutException noSuchLayoutException) {
-			Layout layout = _fetchLayoutByFriendlyURL(
-				groupId, privateLayout, friendlyURL);
+			Layout layout = _fetchLayoutByFriendlyURL(groupId, friendlyURL);
 
 			if (layout != null) {
 				return layout;
@@ -67,14 +66,10 @@ public class LayoutLocalServiceWrapper
 		}
 	}
 
-	private Layout _fetchLayoutByFriendlyURL(
-		long groupId, boolean privateLayout, String friendlyURL) {
-
+	private Layout _fetchLayoutByFriendlyURL(long groupId, String friendlyURL) {
 		FriendlyURLEntry friendlyURLEntry =
 			_friendlyURLEntryLocalService.fetchFriendlyURLEntry(
-				groupId,
-				_layoutFriendlyURLEntryHelper.getClassNameId(privateLayout),
-				friendlyURL);
+				groupId, _portal.getClassNameId(Layout.class), friendlyURL);
 
 		if (friendlyURLEntry != null) {
 			Layout layout = fetchLayout(friendlyURLEntry.getClassPK());
@@ -91,6 +86,6 @@ public class LayoutLocalServiceWrapper
 	private FriendlyURLEntryLocalService _friendlyURLEntryLocalService;
 
 	@Reference
-	private LayoutFriendlyURLEntryHelper _layoutFriendlyURLEntryHelper;
+	private Portal _portal;
 
 }
