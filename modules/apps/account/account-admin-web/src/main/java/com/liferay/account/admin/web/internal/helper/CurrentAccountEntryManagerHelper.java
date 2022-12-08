a20d77e4af36dc219f7414ce286ffc7ad4b73eb3
Lily.Chi <lily.chi@liferay.com>
2022-12-08 11:17:39 +0800

    LPS-170398 Add a new component class to initialize the static field of AccountEntryDisplay

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/helper/CurrentAccountEntryManagerHelper.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.helper;
+
+import com.liferay.account.admin.web.internal.display.AccountEntryDisplay;
+import com.liferay.account.manager.CurrentAccountEntryManager;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lily Chi
+ */
+@Component(service = {})
+public class CurrentAccountEntryManagerHelper {
+
+	@Activate
+	protected void activate() {
+		AccountEntryDisplay.setCurrentAccountEntryManager(
+			_currentAccountEntryManager);
+	}
+
+	@Reference
+	private CurrentAccountEntryManager _currentAccountEntryManager;
+
+}
