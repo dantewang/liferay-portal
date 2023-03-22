ce414d91d5fce29b317b4eb67eec0045a5a85033
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-03-22 13:17:34 +0800

    LPS-160434 Remove the method setPortlet, prepare for next

--- modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/PanelAppRegistry.java
+++ modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/PanelAppRegistry.java
@@ -97,13 +97,6 @@ public class PanelAppRegistry {
 				Portlet portlet = panelApp.getPortlet();
 
 				if (portlet == null) {
-					portlet = _portletLocalService.getPortletById(
-						panelApp.getPortletId());
-
-					panelApp.setPortlet(portlet);
-				}
-
-				if (portlet == null) {
 					return false;
 				}
 
@@ -182,16 +175,13 @@ public class PanelAppRegistry {
 
 					panelApp.setGroupProvider(_groupProvider);
 
-					Portlet portlet = _portletLocalService.getPortletById(
-						panelApp.getPortletId());
+					Portlet portlet = panelApp.getPortlet();
 
 					if (portlet != null) {
 						portlet.setControlPanelEntryCategory(
 							String.valueOf(
 								serviceReference.getProperty(
 									"panel.category.key")));
-
-						panelApp.setPortlet(portlet);
 					}
 					else if (_log.isDebugEnabled()) {
 						_log.debug(
