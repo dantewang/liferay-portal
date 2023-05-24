0673133c5c00b4e050af4ec27eec08d63391e9ed
Dante Wang <dante.wang@liferay.com>
2023-05-24 10:13:37 +0800

    LPS-184766 Make SF happy: com.liferay.portal.osgi.web.http.servlet and com.liferay.portal.osgi.web.http.servlet.context

--- modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet/src/main/java/com/liferay/portal/osgi/web/http/servlet/HttpServiceServlet.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet/src/main/java/com/liferay/portal/osgi/web/http/servlet/HttpServiceServlet.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 /*******************************************************************************
  * Copyright (c) 2005, 2014 Cognos Incorporated, IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
@@ -10,17 +24,19 @@
  *     IBM Corporation - bug fixes and enhancements
  *     Raymond Augé <raymond.auge@liferay.com> - Bug 436698
  *******************************************************************************/
+
 package com.liferay.portal.osgi.web.http.servlet;
 
 import com.liferay.portal.osgi.web.http.servlet.internal.servlet.ProxyServlet;
 
 /**
- * The HttpServiceServlet is the "public" side of a Servlet that when registered (and init() called) in a servlet container
- * will in-turn register and provide an OSGi Http Service implementation.
- * This class is not meant for extending or even using directly and is purely meant for registering
- * in a servlet container.
- * @noextend This class is not intended to be subclassed by clients.
+ * @author Dante Wang
+ * @author Cognos Incorporated
+ * @author IBM Corporation
+ * @author Raymond Augé
  */
 public class HttpServiceServlet extends ProxyServlet {
+
 	private static final long serialVersionUID = -3647550992964861187L;
+
 }
