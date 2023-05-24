e85e9c5240d597adf55371bbe4694ddc3c116ec1
Dante Wang <dante.wang@liferay.com>
2023-05-24 13:54:02 +0800

    LPS-184766 Make SF happy: com.liferay.portal.osgi.web.http.servlet.internal

--- modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/HttpServiceFactory.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/HttpServiceFactory.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 /*******************************************************************************
  * Copyright (c) 2005, 2014 Cognos Incorporated, IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
@@ -13,26 +27,31 @@
 
 package com.liferay.portal.osgi.web.http.servlet.internal;
 
-import org.osgi.framework.*;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.ServiceFactory;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.http.HttpService;
 
-// Factory to create http services. This is because the service needs to be
-// customized for each bundle in order to implement the default resource
-// lookups.
+/**
+ * @author Dante Wang
+ * @author Cognos Incorporated
+ * @author IBM Corporation
+ * @author Raymond Augé
+ */
 public class HttpServiceFactory implements ServiceFactory<HttpService> {
 
-	private final HttpServiceRuntimeImpl httpServiceRuntime;
-
-	public HttpServiceFactory(HttpServiceRuntimeImpl httpServiceRuntime) {
-		this.httpServiceRuntime = httpServiceRuntime;
+	public HttpServiceFactory(HttpServiceRuntimeImpl httpServiceRuntimeImpl) {
+		_httpServiceRuntimeImpl = httpServiceRuntimeImpl;
 	}
 
+	@Override
 	public HttpService getService(
 		Bundle bundle, ServiceRegistration<HttpService> serviceRegistration) {
 
-		return new HttpServiceImpl(bundle, httpServiceRuntime);
+		return new HttpServiceImpl(bundle, _httpServiceRuntimeImpl);
 	}
 
+	@Override
 	public void ungetService(
 		Bundle bundle, ServiceRegistration<HttpService> serviceRegistration,
 		HttpService httpService) {
@@ -41,5 +60,7 @@ public class HttpServiceFactory implements ServiceFactory<HttpService> {
 
 		httpServiceImpl.shutdown();
 	}
+
+	private final HttpServiceRuntimeImpl _httpServiceRuntimeImpl;
 
 }
--- modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/HttpServiceObjectRegistration.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/HttpServiceObjectRegistration.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 /*******************************************************************************
  * Copyright (c) Dec 1, 2014 Liferay, Inc.
  * All rights reserved. This program and the accompanying materials
@@ -6,27 +20,36 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Liferay, Inc. - initial API and implementation and/or initial 
+ *    Liferay, Inc. - initial API and implementation and/or initial
  *                    documentation
  ******************************************************************************/
 
 package com.liferay.portal.osgi.web.http.servlet.internal;
 
 import com.liferay.portal.osgi.web.http.servlet.internal.context.HttpContextHelperFactory;
+
 import org.osgi.framework.Bundle;
 import org.osgi.framework.ServiceRegistration;
 
+/**
+ * @author Dante Wang
+ * @author Liferay, Inc.
+ */
 public class HttpServiceObjectRegistration {
-	public final Object serviceKey;
-	public final ServiceRegistration<?> registration;
-	public final HttpContextHelperFactory factory;
-	public final Bundle bundle;
+
 	public HttpServiceObjectRegistration(
-		Object serviceKey, ServiceRegistration<?> registration,
+		Object serviceKey, ServiceRegistration<?> serviceRegistration,
 		HttpContextHelperFactory factory, Bundle bundle) {
+
 		this.serviceKey = serviceKey;
-		this.registration = registration;
+		this.serviceRegistration = serviceRegistration;
 		this.factory = factory;
 		this.bundle = bundle;
 	}
+
+	public final Bundle bundle;
+	public final HttpContextHelperFactory factory;
+	public final Object serviceKey;
+	public final ServiceRegistration<?> serviceRegistration;
+
 }
