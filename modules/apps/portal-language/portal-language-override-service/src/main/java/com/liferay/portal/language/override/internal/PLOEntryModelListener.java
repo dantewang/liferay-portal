dantewang/liferay-portal

commit 1a08c39ca8b966a44090ec35ea5e5a4d00ae6eed

LPS-150699 Sort

modules/apps/portal-language/portal-language-override-service/src/main/java/com/liferay/portal/language/override/internal/PLOEntryModelListener.java
@@ -63,7 +63,7 @@ public class PLOEntryModelListener
 	}
 
 	private static void _onNotify(
-		String osgiServiceIdentifier, MethodType methodType,
+		MethodType methodType, String osgiServiceIdentifier,
 		PLOEntry ploEntry) {
 
 		PLOEntryModelListener ploEntryModelListener =
@@ -115,8 +115,8 @@ public class PLOEntryModelListener
 		PLOEntryModelListener.class.getName());
 
 	private static final MethodKey _onNotifyMethodKey = new MethodKey(
-		PLOEntryModelListener.class, "_onNotify", String.class,
-		MethodType.class, PLOEntry.class);
+		PLOEntryModelListener.class, "_onNotify", MethodType.class,
+		String.class, PLOEntry.class);
 
 	@Reference
 	private ClusterExecutor _clusterExecutor;

commit 61bdbb433ad35eb0e1f216099ee42741dc993c4d

LPS-186271 Fix parameter order, this is caused by 1a08c39ca8b966a44090ec35ea5e5a4d00ae6eed

modules/apps/portal-language/portal-language-override-service/src/main/java/com/liferay/portal/language/override/internal/PLOEntryModelListener.java
@@ -82,7 +82,7 @@ public class PLOEntryModelListener
 
 		try {
 			MethodHandler methodHandler = new MethodHandler(
-				_onNotifyMethodKey, getOSGiServiceIdentifier(), methodType,
+				_onNotifyMethodKey, methodType, getOSGiServiceIdentifier(),
 				ploEntry);
 
 			ClusterRequest clusterRequest =