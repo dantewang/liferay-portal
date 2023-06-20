f1e05a0785da18b210af0be1a8064d286322e1c8
Dante Wang <dante.wang@liferay.com>
2023-06-20 13:57:49 +0800

    LPS-178732 Make cache removal cluster aware

--- modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/company/feature/flags/CompanyFeatureFlagsProvider.java
+++ modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/company/feature/flags/CompanyFeatureFlagsProvider.java
@@ -14,6 +14,11 @@
 
 package com.liferay.feature.flag.web.internal.company.feature.flags;
 
+import com.liferay.portal.kernel.cluster.ClusterExecutor;
+import com.liferay.portal.kernel.cluster.ClusterRequest;
+import com.liferay.portal.kernel.util.MethodHandler;
+import com.liferay.portal.kernel.util.MethodKey;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
@@ -33,7 +38,21 @@ public class CompanyFeatureFlagsProvider {
 	}
 
 	public void removeCompanyFeatureFlags(long companyId) {
-		_companyFeatureFlagsMap.remove(companyId);
+		_removeCompanyFeatureFlags(companyId);
+
+		if (!_clusterExecutor.isEnabled()) {
+			return;
+		}
+
+		MethodHandler methodHandler = new MethodHandler(
+			_removeCompanyFeatureFlagsMethodKey, companyId);
+
+		ClusterRequest clusterRequest = ClusterRequest.createMulticastRequest(
+			methodHandler, true);
+
+		clusterRequest.setFireAndForget(true);
+
+		_clusterExecutor.execute(clusterRequest);
 	}
 
 	public <T> T withCompanyFeatureFlags(
@@ -42,10 +61,21 @@ public class CompanyFeatureFlagsProvider {
 		return function.apply(getOrCreateCompanyFeatureFlags(companyId));
 	}
 
+	private static void _removeCompanyFeatureFlags(long companyId) {
+		_companyFeatureFlagsMap.remove(companyId);
+	}
+
+	private static final Map<Long, CompanyFeatureFlags>
+		_companyFeatureFlagsMap = new ConcurrentHashMap<>();
+	private static final MethodKey _removeCompanyFeatureFlagsMethodKey =
+		new MethodKey(
+			CompanyFeatureFlagsProvider.class, "_removeCompanyFeatureFlags",
+			long.class);
+
+	@Reference
+	private ClusterExecutor _clusterExecutor;
+
 	@Reference
 	private CompanyFeatureFlagsFactory _companyFeatureFlagsFactory;
-
-	private final Map<Long, CompanyFeatureFlags> _companyFeatureFlagsMap =
-		new ConcurrentHashMap<>();
 
 }
