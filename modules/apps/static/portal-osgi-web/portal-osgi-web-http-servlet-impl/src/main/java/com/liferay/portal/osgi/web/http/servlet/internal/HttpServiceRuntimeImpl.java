fa30572cf3b97633c18b920d09668275621403a8
Dante Wang <dante.wang@liferay.com>
2023-07-19 17:07:34 +0800

    LPS-184766 HttpServiceRuntimeController: Use generic to simplify centralized Failed*DTO recording, and move other DTO logic to HttpServiceRuntimeImpl

--- modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet-impl/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/HttpServiceRuntimeImpl.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet-impl/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/HttpServiceRuntimeImpl.java
@@ -14,9 +14,25 @@
 
 package com.liferay.portal.osgi.web.http.servlet.internal;
 
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.osgi.web.http.servlet.internal.context.ContextController;
+import com.liferay.portal.osgi.web.http.servlet.internal.util.DTOUtil;
+
+import java.util.Objects;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.dto.ServiceReferenceDTO;
 import org.osgi.service.http.runtime.HttpServiceRuntime;
+import org.osgi.service.http.runtime.dto.FailedFilterDTO;
+import org.osgi.service.http.runtime.dto.FailedListenerDTO;
+import org.osgi.service.http.runtime.dto.FailedResourceDTO;
+import org.osgi.service.http.runtime.dto.FailedServletContextDTO;
+import org.osgi.service.http.runtime.dto.FailedServletDTO;
 import org.osgi.service.http.runtime.dto.RequestInfoDTO;
 import org.osgi.service.http.runtime.dto.RuntimeDTO;
+import org.osgi.service.http.runtime.dto.ServletContextDTO;
 
 /**
  * @author Dante Wang
@@ -24,8 +40,10 @@ import org.osgi.service.http.runtime.dto.RuntimeDTO;
 public class HttpServiceRuntimeImpl implements HttpServiceRuntime {
 
 	public HttpServiceRuntimeImpl(
+		BundleContext bundleContext,
 		HttpServiceRuntimeController httpServiceRuntimeController) {
 
+		_bundleContext = bundleContext;
 		_httpServiceRuntimeController = httpServiceRuntimeController;
 	}
 
@@ -50,25 +68,51 @@ public class HttpServiceRuntimeImpl implements HttpServiceRuntime {
 	public RuntimeDTO getRuntimeDTO() {
 		RuntimeDTO runtimeDTO = new RuntimeDTO();
 
-		runtimeDTO.serviceDTO = _httpServiceRuntimeController.getServiceDTO();
+		runtimeDTO.serviceDTO = _getServiceDTO();
 
 		runtimeDTO.failedErrorPageDTOs = null;
-		runtimeDTO.failedFilterDTOs =
-			_httpServiceRuntimeController.getFailedFilterDTOs();
-		runtimeDTO.failedListenerDTOs =
-			_httpServiceRuntimeController.getFailedListenerDTOs();
-		runtimeDTO.failedResourceDTOs =
-			_httpServiceRuntimeController.getFailedResourceDTOs();
+		runtimeDTO.failedFilterDTOs = _httpServiceRuntimeController.getDTOs(
+			FailedFilterDTO.class, dto -> DTOUtil.clone((FailedFilterDTO)dto));
+		runtimeDTO.failedListenerDTOs = _httpServiceRuntimeController.getDTOs(
+			FailedListenerDTO.class,
+			dto -> DTOUtil.clone((FailedListenerDTO)dto));
+		runtimeDTO.failedResourceDTOs = _httpServiceRuntimeController.getDTOs(
+			FailedResourceDTO.class,
+			dto -> DTOUtil.clone((FailedResourceDTO)dto));
 		runtimeDTO.failedServletContextDTOs =
-			_httpServiceRuntimeController.getFailedServletContextDTO();
-		runtimeDTO.failedServletDTOs =
-			_httpServiceRuntimeController.getFailedServletDTOs();
-		runtimeDTO.servletContextDTOs =
-			_httpServiceRuntimeController.getServletContextDTOs();
+			_httpServiceRuntimeController.getDTOs(
+				FailedServletContextDTO.class,
+				dto -> DTOUtil.clone((FailedServletContextDTO)dto));
+		runtimeDTO.failedServletDTOs = _httpServiceRuntimeController.getDTOs(
+			FailedServletDTO.class,
+			dto -> DTOUtil.clone((FailedServletDTO)dto));
+		runtimeDTO.servletContextDTOs = TransformUtil.transformToArray(
+			_httpServiceRuntimeController.getContextControllers(),
+			ContextController::getServletContextDTO, ServletContextDTO.class);
 
 		return runtimeDTO;
 	}
 
+	private ServiceReferenceDTO _getServiceDTO() {
+		Bundle bundle = _bundleContext.getBundle();
+
+		for (ServiceReferenceDTO serviceReferenceDTO :
+				bundle.adapt(ServiceReferenceDTO[].class)) {
+
+			for (String type :
+					(String[])serviceReferenceDTO.properties.get(
+						Constants.OBJECTCLASS)) {
+
+				if (Objects.equals(HttpServiceRuntime.class.getName(), type)) {
+					return serviceReferenceDTO;
+				}
+			}
+		}
+
+		return null;
+	}
+
+	private final BundleContext _bundleContext;
 	private final HttpServiceRuntimeController _httpServiceRuntimeController;
 
 }
