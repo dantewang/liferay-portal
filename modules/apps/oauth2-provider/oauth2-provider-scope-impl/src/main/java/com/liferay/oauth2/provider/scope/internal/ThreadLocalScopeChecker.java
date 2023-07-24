8bd46341b9d1aa173abccf41eb3ce952881787db
Dante Wang <dante.wang@liferay.com>
2023-07-24 10:28:45 +0800

    Revert "LPS-187086 SF, move to package that matches the interface it implements"
    This reverts commit 3f96059b08445dcc4ff85b8eb606358623bf8e3e.

--- modules/apps/oauth2-provider/oauth2-provider-scope-impl/src/main/java/com/liferay/oauth2/provider/scope/internal/ThreadLocalScopeChecker.java
+++ modules/apps/oauth2-provider/oauth2-provider-scope-impl/src/main/java/com/liferay/oauth2/provider/scope/internal/ThreadLocalScopeChecker.java
@@ -16,7 +16,6 @@ package com.liferay.oauth2.provider.scope.internal;
 
 import com.liferay.oauth2.provider.model.OAuth2ScopeGrant;
 import com.liferay.oauth2.provider.scope.ScopeChecker;
-import com.liferay.oauth2.provider.scope.internal.liferay.ThreadLocalScopeContext;
 import com.liferay.oauth2.provider.service.OAuth2ScopeGrantLocalService;
 import com.liferay.osgi.util.service.Snapshot;
 import com.liferay.portal.kernel.util.Validator;
