71b4d482c62a7847f6ee0068fd3f9c02760a66ba
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-08-17 10:12:56 +0800

    LPS-171291 We should unregister MessageListener after using it

--- modules/apps/document-library/document-library-test-util/src/main/java/com/liferay/document/library/test/util/BaseDLAppWithRegisteringMessageListenerTestCase.java
+++ modules/apps/document-library/document-library-test-util/src/main/java/com/liferay/document/library/test/util/BaseDLAppWithRegisteringMessageListenerTestCase.java
@@ -8,10 +8,13 @@ package com.liferay.document.library.test.util;
 import com.liferay.portal.kernel.messaging.Destination;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.test.rule.Inject;
 
 import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.After;
 
 /**
  * @author Jiaxu Wei
@@ -19,29 +22,49 @@ import java.util.concurrent.atomic.AtomicInteger;
 public abstract class BaseDLAppWithRegisteringMessageListenerTestCase
 	extends BaseDLAppTestCase {
 
+	@After
+	public void tearDown() throws Exception {
+		if (_messageListener != null) {
+			Destination destination = _messageBus.getDestination(
+				DestinationNames.DOCUMENT_LIBRARY_SYNC_EVENT_PROCESSOR);
+
+			destination.unregister(_messageListener);
+
+			_messageListener = null;
+		}
+
+		super.tearDown();
+	}
+
 	protected AtomicInteger registerDLSyncEventProcessorMessageListener(
 		final String targetEvent) {
 
 		final AtomicInteger counter = new AtomicInteger();
 
-		Destination destination = MessageBusUtil.getDestination(
+		_messageListener = new MessageListener() {
+
+			@Override
+			public void receive(Message message) {
+				Object event = message.get("event");
+
+				if (targetEvent.equals(event)) {
+					counter.incrementAndGet();
+				}
+			}
+
+		};
+
+		Destination destination = _messageBus.getDestination(
 			DestinationNames.DOCUMENT_LIBRARY_SYNC_EVENT_PROCESSOR);
 
-		destination.register(
-			new MessageListener() {
-
-				@Override
-				public void receive(Message message) {
-					Object event = message.get("event");
-
-					if (targetEvent.equals(event)) {
-						counter.incrementAndGet();
-					}
-				}
-
-			});
+		destination.register(_messageListener);
 
 		return counter;
 	}
+
+	@Inject
+	private MessageBus _messageBus;
+
+	private MessageListener _messageListener;
 
 }
