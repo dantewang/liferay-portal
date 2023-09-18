6da58f8d44b700b9dd1291343a791ceb4917a3d1
Dante Wang <dante.wang@liferay.com>
2023-09-18 17:56:25 +0800

    LPS-196987 Override ProxyContext logic; since new ProxyContext is created as per new ContextController, there's no need to support a ContextController-to-dictionary map; just make it simple to avoid synchronization.

--- modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet-impl/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/servlet/ServletContextAttributesHelper.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet-impl/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/servlet/ServletContextAttributesHelper.java
@@ -5,10 +5,22 @@
 
 package com.liferay.portal.osgi.web.http.servlet.internal.servlet;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.ProxyFactory;
+
+import java.io.File;
+
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.ServletContext;
 
+import org.eclipse.equinox.http.servlet.internal.context.ContextController;
 import org.eclipse.equinox.http.servlet.internal.context.ProxyContext;
 
 /**
@@ -17,12 +29,106 @@ import org.eclipse.equinox.http.servlet.internal.context.ProxyContext;
 public class ServletContextAttributesHelper extends ProxyContext {
 
 	public ServletContextAttributesHelper(
-		String contextName, ServletContext servletContext) {
+		String contextName, ServletContext parentServletContext,
+		File parentServletContextTempDir) {
 
 		super(contextName, _dummyServletContext);
+
+		_parentServletContext = parentServletContext;
+
+		if (parentServletContextTempDir == null) {
+			_servletContextTempDir = null;
+
+			return;
+		}
+
+		_servletContextTempDir = new File(
+			parentServletContextTempDir, contextName + hashCode());
+
+		_servletContextTempDir.mkdirs();
+
+		_attributesDictionary.put(
+			JavaConstants.JAVAX_SERVLET_CONTEXT_TEMPDIR,
+			_servletContextTempDir);
+	}
+
+	@Override
+	public void createContextAttributes(ContextController contextController) {
+	}
+
+	@Override
+	public void destroy() {
+		if (_servletContextTempDir != null) {
+			FileUtil.deltree(_servletContextTempDir);
+		}
+	}
+
+	@Override
+	public void destroyContextAttributes(ContextController contextController) {
+	}
+
+	public Dictionary<String, Object> getContextAttributes(
+		ContextController contextController) {
+
+		return _attributesDictionary;
+	}
+
+	public ServletContext getServletContext() {
+		return _parentServletContext;
+	}
+
+	@Override
+	public String getServletPath() {
+		return StringPool.BLANK;
 	}
 
 	private static final ServletContext _dummyServletContext =
 		ProxyFactory.newDummyInstance(ServletContext.class);
+
+	private final Dictionary<String, Object> _attributesDictionary =
+		new Dictionary<String, Object>() {
+
+			@Override
+			public Enumeration<Object> elements() {
+				return Collections.enumeration(_attributesMap.values());
+			}
+
+			@Override
+			public Object get(Object key) {
+				return _attributesMap.get(key);
+			}
+
+			@Override
+			public boolean isEmpty() {
+				return _attributesMap.isEmpty();
+			}
+
+			@Override
+			public Enumeration<String> keys() {
+				return Collections.enumeration(_attributesMap.keySet());
+			}
+
+			@Override
+			public Object put(String key, Object value) {
+				return _attributesMap.put(key, value);
+			}
+
+			@Override
+			public Object remove(Object key) {
+				return _attributesMap.remove(key);
+			}
+
+			@Override
+			public int size() {
+				return _attributesMap.size();
+			}
+
+			private final Map<String, Object> _attributesMap =
+				new ConcurrentHashMap<>();
+
+		};
+
+	private final ServletContext _parentServletContext;
+	private final File _servletContextTempDir;
 
 }
