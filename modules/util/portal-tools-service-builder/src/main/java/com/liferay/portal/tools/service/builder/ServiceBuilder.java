dantewang/liferay-portal

commit 438c945eed616a0356617cdb5e31d845dd8af7a1

LPS-100436 Create service builder support for Change Tracking.

modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -671,6 +671,9 @@ public class ServiceBuilder {
 				rootElement.attributeValue("auto-namespace-tables"),
 				_autoNamespaceTables);
 
+			_changeTrackingEnabled = GetterUtil.getBoolean(
+				rootElement.attributeValue("change-tracking-enabled"));
+
 			String dependencyInjector = rootElement.attributeValue(
 				"dependency-injector");
 
@@ -814,6 +817,8 @@ public class ServiceBuilder {
 						_createFinder(entity);
 						_createFinderBaseImpl(entity);
 						_createFinderUtil(entity);
+
+						_createCTServiceImpl(entity);
 
 						if (entity.hasLocalService()) {
 							_createServiceImpl(entity, _SESSION_TYPE_LOCAL);
@@ -1694,13 +1699,21 @@ public class ServiceBuilder {
 			methodName.equals("deactivate") || methodName.equals("destroy") ||
 			methodName.equals("equals") ||
 			methodName.equals("getAopInterfaces") ||
-			methodName.equals("getClass") || methodName.equals("getService") ||
+			methodName.equals("getCTPersistence") ||
+			methodName.equals("getClass") ||
+			methodName.equals("getModelClass") ||
+			methodName.equals("getService") ||
+			methodName.equals("getUniqueIndexColumnNames") ||
 			methodName.equals("getWrappedService") ||
 			methodName.equals("hashCode") || methodName.equals("notify") ||
 			methodName.equals("notifyAll") ||
+			methodName.equals("removeCTModel") ||
 			methodName.equals("setAopProxy") ||
 			methodName.equals("setWrappedService") ||
-			methodName.equals("toString") || methodName.equals("wait")) {
+			methodName.equals("toString") ||
+			methodName.equals("updateCTModel") ||
+			methodName.equals("updateWithUnsafeFunction") ||
+			methodName.equals("wait")) {
 
 			return false;
 		}
@@ -2252,6 +2265,31 @@ public class ServiceBuilder {
 
 			_write(blobModelFile, content, _modifiedFileNames);
 		}
+	}
+
+	private void _createCTServiceImpl(Entity entity) throws Exception {
+		File file = new File(
+			StringBundler.concat(
+				_outputPath, "/service/impl/", entity.getName(),
+				"CTServiceImpl.java"));
+
+		if (!entity.isChangeTrackingEnabled() || entity.hasLocalService()) {
+			if (file.exists()) {
+				System.out.println("Removing " + file);
+
+				file.delete();
+			}
+
+			return;
+		}
+
+		Map<String, Object> context = _getContext();
+
+		context.put("entity", entity);
+
+		String content = _processTemplate(_tplCTServiceImpl, context);
+
+		_write(file, content, _modifiedFileNames);
 	}
 
 	private void _createEJBPK(Entity entity) throws Exception {
@@ -3669,6 +3707,9 @@ public class ServiceBuilder {
 
 			List<EntityFinder> entityFinders = entity.getEntityFinders();
 
+			List<IndexMetadata> indexMetadatas = indexMetadatasMap.get(
+				entity.getTable());
+
 			for (EntityFinder entityFinder : entityFinders) {
 				if (!entityFinder.isDBIndex()) {
 					continue;
@@ -3689,6 +3730,31 @@ public class ServiceBuilder {
 
 				if (dbNames.isEmpty()) {
 					continue;
+				}
+
+				if (entity.isChangeTrackingEnabled() &&
+					!dbNames.contains("ctCollectionId")) {
+
+					if (indexMetadatas != null) {
+						Iterator<IndexMetadata> iterator =
+							indexMetadatas.iterator();
+
+						while (iterator.hasNext()) {
+							IndexMetadata indexMetadata = iterator.next();
+
+							if (indexMetadata.isUnique() &&
+								dbNames.equals(
+									Arrays.asList(
+										indexMetadata.getColumnNames()))) {
+
+								iterator.remove();
+
+								break;
+							}
+						}
+					}
+
+					dbNames.add("ctCollectionId");
 				}
 
 				IndexMetadata indexMetadata =
@@ -4765,7 +4831,9 @@ public class ServiceBuilder {
 			if (entityColumn.isPrimary()) {
 				sb.append(" not null");
 
-				if (!entity.hasCompoundPK()) {
+				if (!entity.hasCompoundPK() &&
+					!entity.isChangeTrackingEnabled()) {
+
 					sb.append(" primary key");
 				}
 			}
@@ -4779,12 +4847,18 @@ public class ServiceBuilder {
 				sb.append(" IDENTITY");
 			}
 
+			if (entity.isChangeTrackingEnabled() &&
+				Objects.equals(entityColumn.getName(), "ctCollectionId")) {
+
+				sb.append(" default 0 not null");
+			}
+
 			if (Objects.equals(entityColumn.getName(), "mvccVersion")) {
 				sb.append(" default 0 not null");
 			}
 
 			if (((i + 1) != databaseRegularEntityColumns.size()) ||
-				entity.hasCompoundPK()) {
+				entity.hasCompoundPK() || entity.isChangeTrackingEnabled()) {
 
 				sb.append(",");
 			}
@@ -4792,7 +4866,7 @@ public class ServiceBuilder {
 			sb.append("\n");
 		}
 
-		if (entity.hasCompoundPK()) {
+		if (entity.hasCompoundPK() || entity.isChangeTrackingEnabled()) {
 			sb.append("\tprimary key (");
 
 			List<EntityColumn> pkEntityColumns = entity.getPKEntityColumns();
@@ -4805,6 +4879,10 @@ public class ServiceBuilder {
 				if ((j + 1) != pkEntityColumns.size()) {
 					sb.append(", ");
 				}
+			}
+
+			if (entity.isChangeTrackingEnabled()) {
+				sb.append(", ctCollectionId");
 			}
 
 			sb.append(")\n");
@@ -5571,6 +5649,10 @@ public class ServiceBuilder {
 		boolean cacheEnabled = GetterUtil.getBoolean(
 			entityElement.attributeValue("cache-enabled"), true);
 
+		boolean changeTrackingEnabled = GetterUtil.getBoolean(
+			entityElement.attributeValue("change-tracking-enabled"),
+			_changeTrackingEnabled);
+
 		boolean mvccEnabled = GetterUtil.getBoolean(
 			entityElement.attributeValue("mvcc-enabled"), _mvccEnabled);
 
@@ -5641,6 +5723,15 @@ public class ServiceBuilder {
 			Element columnElement = DocumentHelper.createElement("column");
 
 			columnElement.addAttribute("name", "mvccVersion");
+			columnElement.addAttribute("type", "long");
+
+			derivedColumnElements.add(columnElement);
+		}
+
+		if (changeTrackingEnabled) {
+			Element columnElement = DocumentHelper.createElement("column");
+
+			columnElement.addAttribute("name", "ctCollectionId");
 			columnElement.addAttribute("type", "long");
 
 			derivedColumnElements.add(columnElement);
@@ -5877,6 +5968,20 @@ public class ServiceBuilder {
 		List<EntityFinder> entityFinders = new ArrayList<>();
 
 		List<Element> finderElements = entityElement.elements("finder");
+
+		if (changeTrackingEnabled) {
+			Element finderElement = DocumentHelper.createElement("finder");
+
+			finderElement.addAttribute("name", "CTCollectionId");
+			finderElement.addAttribute("return-type", "Collection");
+
+			Element finderColumnElement = finderElement.addElement(
+				"finder-column");
+
+			finderColumnElement.addAttribute("name", "ctCollectionId");
+
+			finderElements.add(0, finderElement);
+		}
 
 		if (uuid) {
 			if (entityColumns.contains(new EntityColumn("companyId"))) {
@@ -6166,7 +6271,35 @@ public class ServiceBuilder {
 			regularEntityColumns, blobEntityColumns, collectionEntityColumns,
 			entityColumns, entityOrder, entityFinders, referenceEntities,
 			unresolvedReferenceEntityNames, txRequiredMethodNames,
-			resourceActionModel);
+			resourceActionModel, changeTrackingEnabled);
+
+		if (changeTrackingEnabled) {
+			if (!mvccEnabled) {
+				throw new ServiceBuilderException(
+					"MVCC must be enabled to use change tracking for " +
+						entityName);
+			}
+
+			if (entity.isHierarchicalTree()) {
+				throw new ServiceBuilderException(
+					"Change tracking with Hierarchical Tree is not yet " +
+						"supported for " + entityName);
+			}
+
+			if (pkEntityColumns.size() > 1) {
+				throw new ServiceBuilderException(
+					"Compound primary key with change tracked columns is not " +
+						"supported for " + entityName);
+			}
+
+			EntityColumn pkEntityColumn = pkEntityColumns.get(0);
+
+			if (!Objects.equals("long", pkEntityColumn.getType())) {
+				throw new ServiceBuilderException(
+					"Primary key must be of type long to enable change " +
+						"tracking for " + entityName);
+			}
+		}
 
 		_entities.add(entity);
 
@@ -7228,6 +7361,7 @@ public class ServiceBuilder {
 	private boolean _build;
 	private long _buildNumber;
 	private boolean _buildNumberIncrement;
+	private boolean _changeTrackingEnabled;
 	private boolean _commercialPlugin;
 	private Properties _compatProperties;
 	private String _currentTplName;
@@ -7271,6 +7405,7 @@ public class ServiceBuilder {
 	private String _tplBaseUADDisplay = _TPL_ROOT + "base_uad_display.ftl";
 	private String _tplBaseUADExporter = _TPL_ROOT + "base_uad_exporter.ftl";
 	private String _tplBlobModel = _TPL_ROOT + "blob_model.ftl";
+	private String _tplCTServiceImpl = _TPL_ROOT + "ct_service_impl.ftl";
 	private String _tplEjbPK = _TPL_ROOT + "ejb_pk.ftl";
 	private String _tplException = _TPL_ROOT + "exception.ftl";
 	private String _tplExtendedModel = _TPL_ROOT + "extended_model.ftl";

modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/Entity.java
@@ -96,7 +96,7 @@ public class Entity implements Comparable<Entity> {
 			serviceBuilder, null, null, null, name, null, null, null, false,
 			false, false, false, true, true, null, null, null, null, null, true,
 			false, false, false, false, null, false, null, null, false, null,
-			null, null, null, null, null, null, null, null, null, false);
+			null, null, null, null, null, null, null, null, null, false, false);
 	}
 
 	public Entity(
@@ -117,7 +117,8 @@ public class Entity implements Comparable<Entity> {
 		List<EntityColumn> entityColumns, EntityOrder entityOrder,
 		List<EntityFinder> entityFinders, List<Entity> referenceEntities,
 		List<String> unresolvedReferenceEntityNames,
-		List<String> txRequiredMethodNames, boolean resourceActionModel) {
+		List<String> txRequiredMethodNames, boolean resourceActionModel,
+		boolean changeTrackingEnabled) {
 
 		_serviceBuilder = serviceBuilder;
 		_packagePath = packagePath;
@@ -154,6 +155,7 @@ public class Entity implements Comparable<Entity> {
 		_unresolvedReferenceEntityNames = unresolvedReferenceEntityNames;
 		_txRequiredMethodNames = txRequiredMethodNames;
 		_resourceActionModel = resourceActionModel;
+		_changeTrackingEnabled = changeTrackingEnabled;
 
 		_humanName = GetterUtil.getString(
 			humanName, ServiceBuilder.toHumanName(name));
@@ -366,6 +368,10 @@ public class Entity implements Comparable<Entity> {
 
 		interfaceNames.add("BaseModel<" + _name + ">");
 
+		if (isChangeTrackingEnabled()) {
+			interfaceNames.add("CTModel<" + _name + ">");
+		}
+
 		if (isContainerModel()) {
 			interfaceNames.add("ContainerModel");
 		}
@@ -459,6 +465,11 @@ public class Entity implements Comparable<Entity> {
 			overrideColumnName.add("userId");
 			overrideColumnName.add("userName");
 			overrideColumnName.add("userUuid");
+		}
+
+		if (isChangeTrackingEnabled()) {
+			overrideColumnName.add("ctCollectionId");
+			overrideColumnName.add("primaryKey");
 		}
 
 		if (isGroupedModel()) {
@@ -874,7 +885,7 @@ public class Entity implements Comparable<Entity> {
 	}
 
 	public boolean hasPrimitivePK(boolean includeWrappers) {
-		if (hasCompoundPK()) {
+		if (_pkEntityColumns.size() != 1) {
 			return false;
 		}
 
@@ -931,6 +942,10 @@ public class Entity implements Comparable<Entity> {
 
 	public boolean isCacheEnabled() {
 		return _cacheEnabled;
+	}
+
+	public boolean isChangeTrackingEnabled() {
+		return _changeTrackingEnabled;
 	}
 
 	public boolean isContainerModel() {
@@ -1259,6 +1274,7 @@ public class Entity implements Comparable<Entity> {
 	private String _apiPackagePath;
 	private List<EntityColumn> _blobEntityColumns;
 	private final boolean _cacheEnabled;
+	private boolean _changeTrackingEnabled;
 	private final List<EntityColumn> _collectionEntityColumns;
 	private final boolean _containerModel;
 	private final List<EntityColumn> _databaseRegularEntityColumns;

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/ct_service_impl.ftl
@@ -1,0 +1,52 @@
+package ${packagePath}.service.impl;
+
+import ${apiPackagePath}.model.${entity.name};
+import ${apiPackagePath}.service.persistence.${entity.name}Persistence;
+import com.liferay.petra.function.UnsafeFunction;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.service.change.tracking.CTService;
+import com.liferay.portal.kernel.service.persistence.change.tracking.CTPersistence;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author ${author}
+<#if entity.isDeprecated()>
+ * @deprecated ${classDeprecatedComment}
+</#if>
+ * @generated
+ */
+<#if dependencyInjectorDS>
+	@Component(service = AopService.class)
+</#if>
+public class ${entity.name}CTServiceImpl implements
+	<#if dependencyInjectorDS>
+		AopService,
+	</#if>
+
+	CTService<${entity.name}> {
+
+	@Override
+	public CTPersistence<${entity.name}> getCTPersistence() {
+		return _${entity.varName}Persistence;
+	}
+
+	@Override
+	public Class<${entity.name}> getModelClass() {
+		return ${entity.name}.class;
+	}
+
+	@Override
+	public <R, E extends Throwable> R updateWithUnsafeFunction(UnsafeFunction<CTPersistence<${entity.name}>, R, E> updateUnsafeFunction) throws E {
+		return updateUnsafeFunction.apply(_${entity.varName}Persistence);
+	}
+
+	<#if dependencyInjectorDS>
+		@Reference
+	<#else>
+		@BeanReference(type = ${entity.name}Persistence.class)
+	</#if>
+	private ${entity.name}Persistence _${entity.varName}Persistence;
+
+}

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/model.ftl
@@ -27,6 +27,7 @@ import com.liferay.portal.kernel.model.StagedModel;
 import com.liferay.portal.kernel.model.TrashedModel;
 import com.liferay.portal.kernel.model.TypedModel;
 import com.liferay.portal.kernel.model.WorkflowedModel;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
 import com.liferay.portal.kernel.model.version.VersionModel;
 import com.liferay.portal.kernel.model.version.VersionedModel;
 import com.liferay.portal.kernel.service.ServiceContext;

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/model_wrapper.ftl
@@ -17,6 +17,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 /**
  * <p>
@@ -188,6 +190,18 @@ public class ${entity.name}Wrapper
 			}
 
 			return false;
+		}
+	</#if>
+
+	<#if entity.isChangeTrackingEnabled()>
+		@Override
+		public Map<String, Function<${entity.name}, Object>> getAttributeGetterFunctions() {
+			return ${entityFieldName}.getAttributeGetterFunctions();
+		}
+
+		@Override
+		public Map<String, BiConsumer<${entity.name}, Object>> getAttributeSetterBiConsumers() {
+			return ${entityFieldName}.getAttributeSetterBiConsumers();
 		}
 	</#if>
 

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/persistence.ftl
@@ -9,6 +9,7 @@ import ${apiPackagePath}.model.${entity.name};
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.persistence.BasePersistence;
+import com.liferay.portal.kernel.service.persistence.change.tracking.CTPersistence;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
 
@@ -40,7 +41,13 @@ import java.util.Set;
 </#if>
 
 @ProviderType
-public interface ${entity.name}Persistence extends BasePersistence<${entity.name}> {
+public interface ${entity.name}Persistence extends BasePersistence<${entity.name}>
+
+	<#if entity.isChangeTrackingEnabled()>
+		, CTPersistence<${entity.name}>
+	</#if>
+
+	{
 
 	/**
 	 * NOTE FOR DEVELOPERS:
@@ -61,13 +68,17 @@ public interface ${entity.name}Persistence extends BasePersistence<${entity.name
 				@Deprecated
 			</#if>
 
+			<#assign parameters = method.parameters />
+
+			<#if entity.isChangeTrackingEnabled() && stringUtil.equals(method.name, "findByCTCollectionId") && (parameters?size == 1)>
+				@Override
+			</#if>
+
 			<#if stringUtil.equals(method.name, "getBadColumnNames")>
 				@Override
 			</#if>
 
 			public ${serviceBuilder.getTypeGenericsName(method.returns)} ${method.name} (
-
-			<#assign parameters = method.parameters />
 
 			<#list parameters as parameter>
 				${serviceBuilder.getTypeGenericsName(parameter.type)} ${parameter.name}

commit fa41ee9d1e67ed80657e97d3f3349d6b1e4e3c52

LPS-X Fix ctCollectionId db index position with respect to BTREE ordering. This is completely broken by 438c945eed616a0356617cdb5e31d845dd8af7a1

modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -4162,6 +4162,8 @@ public class ServiceBuilder {
 				if (entity.isChangeTrackingEnabled() &&
 					!dbNames.contains("ctCollectionId")) {
 
+					dbNames.add(0, "ctCollectionId");
+
 					if (indexMetadatas != null) {
 						Iterator<IndexMetadata> iterator =
 							indexMetadatas.iterator();
@@ -4180,8 +4182,6 @@ public class ServiceBuilder {
 							}
 						}
 					}
-
-					dbNames.add("ctCollectionId");
 				}
 
 				IndexMetadata indexMetadata =