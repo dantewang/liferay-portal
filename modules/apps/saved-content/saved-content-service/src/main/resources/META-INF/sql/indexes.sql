6ab4c6f3ae6ba03c30d1040413d705167c47f7aa
shuyangzhou <shuyang.zhou@liferay.com>
2023-10-30 16:05:17 -0700

    LPS-X Regenerate

--- modules/apps/saved-content/saved-content-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/saved-content/saved-content-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,6 +1,4 @@
-create index IX_2F9B1655 on SavedContentEntry (companyId, classNameId, classPK, ctCollectionId);
-create unique index IX_4715B10F on SavedContentEntry (companyId, userId, classNameId, classPK, ctCollectionId);
-create index IX_EC57682 on SavedContentEntry (groupId, ctCollectionId);
-create index IX_6E4EB6BC on SavedContentEntry (groupId, userId, ctCollectionId);
-create index IX_3F8E562C on SavedContentEntry (userId, classNameId, ctCollectionId);
-create index IX_6031E3DE on SavedContentEntry (userId, ctCollectionId);
+create index IX_D0050DF7 on SavedContentEntry (classNameId, companyId, classPK);
+create index IX_8D476824 on SavedContentEntry (groupId);
+create unique index IX_4715B10F on SavedContentEntry (userId, classNameId, companyId, classPK, ctCollectionId);
+create index IX_26BC5C5E on SavedContentEntry (userId, groupId);
