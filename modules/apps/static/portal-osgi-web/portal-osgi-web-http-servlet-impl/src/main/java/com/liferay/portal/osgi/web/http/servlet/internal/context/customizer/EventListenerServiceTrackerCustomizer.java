4b6789bbefe3f8104f969b9acdf610121e60f7ce
Dante Wang <dante.wang@liferay.com>
2023-11-14 14:15:06 +0800

    LPD-61983 Inline *Customizer and initial SF

---
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet-impl/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/context/customizer/EventListenerServiceTrackerCustomizer.java
@@ -0,0 +1,110 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.osgi.web.http.servlet.internal.context.customizer;
+
+import com.liferay.petra.string.StringBundler;
+
+import java.util.EventListener;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.eclipse.equinox.http.servlet.internal.HttpServletEndpointController;
+import org.eclipse.equinox.http.servlet.internal.context.ContextController;
+import org.eclipse.equinox.http.servlet.internal.error.HttpWhiteboardFailureException;
+import org.eclipse.equinox.http.servlet.internal.registration.ListenerRegistration;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.runtime.dto.DTOConstants;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+
+/**
+ * @author Dante Wang
+ */
+public class EventListenerServiceTrackerCustomizer
+	extends BaseServiceTrackerCustomizer
+		<EventListener, AtomicReference<ListenerRegistration>> {
+
+	public EventListenerServiceTrackerCustomizer(
+		BundleContext bundleContext, ContextController contextController,
+		HttpServletEndpointController httpServletEndpointController) {
+
+		super(bundleContext, contextController, httpServletEndpointController);
+	}
+
+	@Override
+	public AtomicReference<ListenerRegistration> addingService(
+		ServiceReference<EventListener> serviceReference) {
+
+		Object listenerObject = serviceReference.getProperty(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_LISTENER);
+
+		if ((listenerObject == null) ||
+			!contextController.matches(serviceReference) ||
+			!httpServletEndpointController.matches(serviceReference)) {
+
+			return null;
+		}
+
+		AtomicReference<ListenerRegistration> result = new AtomicReference<>();
+
+		try {
+			if (!(listenerObject instanceof Boolean) &&
+				!(listenerObject instanceof String)) {
+
+				throw new HttpWhiteboardFailureException(
+					StringBundler.concat(
+						HttpWhiteboardConstants.HTTP_WHITEBOARD_LISTENER, "=",
+						listenerObject, " is not valid"),
+					DTOConstants.FAILURE_REASON_VALIDATION_FAILED);
+			}
+
+			boolean listener = false;
+
+			if (listenerObject instanceof Boolean) {
+				listener = (boolean)listenerObject;
+			}
+			else {
+				listener = Boolean.parseBoolean((String)listenerObject);
+			}
+
+			if (!listener) {
+				return result;
+			}
+
+			result.set(
+				contextController.addListenerRegistration(serviceReference));
+		}
+		catch (Exception exception) {
+			httpServletEndpointController.log(
+				exception.getMessage(), exception);
+		}
+
+		return result;
+	}
+
+	@Override
+	public void modifiedService(
+		ServiceReference<EventListener> serviceReference,
+		AtomicReference<ListenerRegistration> listenerRegistration) {
+
+		removedService(serviceReference, listenerRegistration);
+
+		addingService(serviceReference);
+	}
+
+	@Override
+	public void removedService(
+		ServiceReference<EventListener> serviceReference,
+		AtomicReference<ListenerRegistration> listenerReference) {
+
+		ListenerRegistration listenerRegistration = listenerReference.get();
+
+		if (listenerRegistration != null) {
+			listenerRegistration.destroy();
+		}
+	}
+
+}
