ff2ac9a104d004b6c237011aa813cfaf89e2b16f
Dante Wang <dante.wang@liferay.com>
2024-02-23 16:40:52 +0800

    LPD-1755 SF, rename the thread local, prepare for next

---
+++ modules/apps/portal-configuration/portal-configuration-cluster/src/main/java/com/liferay/portal/configuration/cluster/internal/ClusterConfigurationThreadLocal.java
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.configuration.cluster.internal;
+
+import com.liferay.petra.lang.CentralizedThreadLocal;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ClusterConfigurationThreadLocal {
+
+	public static boolean isProcessingClusterConfigurationEvent() {
+		return _processingClusterConfigurationEvent.get();
+	}
+
+	public static void setProcessingClusterConfigurationEvent(
+		boolean processingClusterConfigurationEvent) {
+
+		_processingClusterConfigurationEvent.set(
+			processingClusterConfigurationEvent);
+	}
+
+	private static final ThreadLocal<Boolean>
+		_processingClusterConfigurationEvent = new CentralizedThreadLocal<>(
+			ClusterConfigurationThreadLocal.class + "._localUpdate",
+			() -> Boolean.FALSE, false);
+
+}
