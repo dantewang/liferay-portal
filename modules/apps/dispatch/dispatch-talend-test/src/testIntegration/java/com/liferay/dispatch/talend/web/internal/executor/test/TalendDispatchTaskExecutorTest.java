a9ea434ffec7d5665517040abbabeee209fe4099
Dante Wang <dante.wang@liferay.com>
2024-03-01 14:31:00 +0800

    LPD-19640 Add output when assertion fails for better test debugging

--- modules/apps/dispatch/dispatch-talend-test/src/testIntegration/java/com/liferay/dispatch/talend/web/internal/executor/test/TalendDispatchTaskExecutorTest.java
+++ modules/apps/dispatch/dispatch-talend-test/src/testIntegration/java/com/liferay/dispatch/talend/web/internal/executor/test/TalendDispatchTaskExecutorTest.java
@@ -82,6 +82,7 @@ public class TalendDispatchTaskExecutorTest {
 		DispatchLog dispatchLog = dispatchLogs.get(0);
 
 		Assert.assertEquals(
+			dispatchLog.getError(),
 			DispatchTaskStatus.SUCCESSFUL,
 			DispatchTaskStatus.valueOf(dispatchLog.getStatus()));
 	}
