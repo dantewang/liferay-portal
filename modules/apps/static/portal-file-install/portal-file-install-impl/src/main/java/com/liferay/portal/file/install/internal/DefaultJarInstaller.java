ddee5bfa97f60e8c02061dba948744d82765c8fd
Dante Wang <dante.wang@liferay.com>
2024-05-13 15:08:28 +0800

    LPD-X For FileInstaller

--- modules/apps/static/portal-file-install/portal-file-install-impl/src/main/java/com/liferay/portal/file/install/internal/DefaultJarInstaller.java
+++ modules/apps/static/portal-file-install/portal-file-install-impl/src/main/java/com/liferay/portal/file/install/internal/DefaultJarInstaller.java
@@ -5,27 +5,88 @@
 
 package com.liferay.portal.file.install.internal;
 
+import com.liferay.petra.reflect.ReflectionUtil;
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.file.install.FileInstaller;
+import com.liferay.portal.kernel.util.SetUtil;
 
 import java.io.File;
+import java.io.IOException;
 
 import java.net.URI;
 import java.net.URL;
 
+import java.util.Dictionary;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.cm.ManagedService;
+
 /**
  * @author Matthew Tambara
  */
-public class DefaultJarInstaller implements FileInstaller {
+public class DefaultJarInstaller implements FileInstaller, ManagedService {
+
+	public DefaultJarInstaller(ConfigurationAdmin configurationAdmin) {
+		try {
+			Configuration configuration = configurationAdmin.getConfiguration(
+				"com.liferay.portal.bundle.blacklist.internal.configuration." +
+					"BundleBlacklistConfiguration",
+				StringPool.QUESTION);
+
+			Dictionary<String, ?> dictionary = configuration.getProperties();
+
+			_atomicReference.set(
+				SetUtil.fromArray(
+					(String[])dictionary.get("blacklistBundleSymbolicNames")));
+		}
+		catch (IOException ioException) {
+			ReflectionUtil.throwException(ioException);
+		}
+	}
 
 	@Override
 	public boolean canTransformURL(File artifact) {
 		String name = artifact.getName();
 
-		if (name.endsWith(".jar")) {
-			return true;
+		if (!name.endsWith(".jar")) {
+			return false;
 		}
 
-		return false;
+		try (JarFile jarFile = new JarFile(artifact)) {
+			Manifest manifest = jarFile.getManifest();
+
+			Attributes attributes = manifest.getMainAttributes();
+
+			String bundleSymbolicName = attributes.getValue(
+				"Bundle-SymbolicName");
+
+			if (bundleSymbolicName != null) {
+				int index = bundleSymbolicName.indexOf(CharPool.SEMICOLON);
+
+				if (index != -1) {
+					bundleSymbolicName = bundleSymbolicName.substring(0, index);
+				}
+
+				Set<String> blacklistBundleSymbolicNames =
+					_atomicReference.get();
+
+				if (blacklistBundleSymbolicNames.contains(bundleSymbolicName)) {
+					return false;
+				}
+			}
+		}
+		catch (IOException ioException) {
+			ReflectionUtil.throwException(ioException);
+		}
+
+		return true;
 	}
 
 	@Override
@@ -38,5 +99,15 @@ public class DefaultJarInstaller implements FileInstaller {
 	@Override
 	public void uninstall(File file) {
 	}
+
+	@Override
+	public void updated(Dictionary<String, ?> dictionary) {
+		_atomicReference.set(
+			SetUtil.fromArray(
+				(String[])dictionary.get("blacklistBundleSymbolicNames")));
+	}
+
+	private final AtomicReference<Set<String>> _atomicReference =
+		new AtomicReference<>();
 
 }
