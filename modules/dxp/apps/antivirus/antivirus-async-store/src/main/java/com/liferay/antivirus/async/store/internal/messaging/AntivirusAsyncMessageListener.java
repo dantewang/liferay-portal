49fc7a0cb0e845ae47464ccbf3953e38f06ef432
Dante Wang <dante.wang@liferay.com>
2024-08-26 17:54:19 +0800

    LPD-X temp: keep this required config to block it from auto starting

--- modules/dxp/apps/antivirus/antivirus-async-store/src/main/java/com/liferay/antivirus/async/store/internal/messaging/AntivirusAsyncMessageListener.java
+++ modules/dxp/apps/antivirus/antivirus-async-store/src/main/java/com/liferay/antivirus/async/store/internal/messaging/AntivirusAsyncMessageListener.java
@@ -11,12 +11,15 @@ import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Raymond Augé
  */
 @Component(
+	configurationPid = "com.liferay.antivirus.async.store.configuration.AntivirusAsyncConfiguration",
+	configurationPolicy = ConfigurationPolicy.REQUIRE,
 	property = "destination.name=" + AntivirusAsyncDestinationNames.ANTIVIRUS,
 	service = MessageListener.class
 )
