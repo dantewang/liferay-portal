dc2de140121729fe58f90dc459d8d38a83c80148
Dante Wang <dante.wang@liferay.com>
2024-09-27 15:28:49 +0800

    LPD-36036 Patch Tomcat Jasper 3: patch EmbeddedServletOptions to apply to fix to Glassfish Jasper issue 6846; lacking of this patch will cause a lot of error logs about the scratchDir not usable, because at the moment this line is executed, the dir is not yet created.
    See https://github.com/javaee/javaee-jsp-api/commit/e1429bfc065ef5ce1885520bf7786335583a790d#diff-66e279a963205244533f3484203d9e97017ecea6a7336bd88438cf551d6a2359R579

--- modules/third-party/org-apache-tomcat-jasper/src/main/java/org/apache/jasper/EmbeddedServletOptions.java
+++ modules/third-party/org-apache-tomcat-jasper/src/main/java/org/apache/jasper/EmbeddedServletOptions.java
@@ -604,7 +604,7 @@ public final class EmbeddedServletOptions implements Options {
             return;
         }
 
-        if (!(scratchDir.exists() && scratchDir.canRead() &&
+        if (scratchDir.exists() && !(scratchDir.canRead() &&
                 scratchDir.canWrite() && scratchDir.isDirectory())) {
             log.fatal(Localizer.getMessage("jsp.error.bad.scratch.dir",
                     scratchDir.getAbsolutePath()));
@@ -732,3 +732,4 @@ public final class EmbeddedServletOptions implements Options {
 
 }
 
+/* @generated */
