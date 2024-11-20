e2ce4f7e91194f435e33f0b2eda7ef8e95a2efa7
Dante Wang <dante.wang@liferay.com>
2024-11-20 16:38:16 +0800

    LPD-41245 Adapt management

---
+++ modules/apps/portal-cache/portal-cache-ehcache3-impl/src/main/java/com/liferay/portal/cache/ehcache3/internal/management/CacheStatisticsMBean.java
@@ -0,0 +1,67 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.cache.ehcache3.internal.management;
+
+import org.ehcache.core.statistics.CacheStatistics;
+
+/**
+ * @author Dante Wang
+ */
+public class CacheStatisticsMBean {
+
+	public CacheStatisticsMBean(String cacheName, CacheStatistics cacheStatistics) {
+		_cacheName = cacheName;
+		_cacheStatistics = cacheStatistics;
+	}
+
+	public String getName() {
+		return _cacheName;
+	}
+
+	public void clear() {
+		_cacheStatistics.clear();
+	}
+
+	public long getCacheHits() {
+		return _cacheStatistics.getCacheHits();
+	}
+
+	public float getCacheHitPercentage() {
+		return _cacheStatistics.getCacheHitPercentage();
+	}
+
+	public long getCacheMisses() {
+		return _cacheStatistics.getCacheMisses();
+	}
+
+	public float getCacheMissPercentage() {
+		return _cacheStatistics.getCacheMissPercentage();
+	}
+
+	public long getCacheGets() {
+		return _cacheStatistics.getCacheGets();
+	}
+
+	public long getCachePuts() {
+		return _cacheStatistics.getCachePuts();
+	}
+
+	public long getCacheRemovals() {
+		return _cacheStatistics.getCacheRemovals();
+	}
+
+	public long getCacheEvictions() {
+		return _cacheStatistics.getCacheEvictions();
+	}
+
+	public long getCacheExpirations() {
+		return _cacheStatistics.getCacheExpirations();
+	}
+
+	private final String _cacheName;
+	private final CacheStatistics _cacheStatistics;
+
+}
