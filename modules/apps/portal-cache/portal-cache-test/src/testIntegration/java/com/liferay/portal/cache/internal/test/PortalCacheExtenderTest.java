db7673d551e4aa0c0b0662617bff7ebefa0fb0a4
Dante Wang <dante.wang@liferay.com>
2024-12-06 10:33:08 +0800

    LPD-41245 Migrate all Ehcache XML configurations to the version 3 format

--- modules/apps/portal-cache/portal-cache-test/src/testIntegration/java/com/liferay/portal/cache/internal/test/PortalCacheExtenderTest.java
+++ modules/apps/portal-cache/portal-cache-test/src/testIntegration/java/com/liferay/portal/cache/internal/test/PortalCacheExtenderTest.java
@@ -307,29 +307,29 @@ public class PortalCacheExtenderTest {
 	}
 
 	private String _generateXMLContent(
-		int cacheEntries, String[] cacheNames, int maxElementsInMemory,
+		int cacheEntries, String[] cacheNames, int maxHeapEntries,
 		int timeToIdleSeconds) {
 
 		StringBundler sb = new StringBundler();
 
-		sb.append("<ehcache dynamicConfig=\"true\" monitoring=\"off\" ");
-		sb.append("updateCheck=\"false\" xmlns:xsi=\"http://www.w3.org/2001");
-		sb.append("/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"");
-		sb.append("http://www.ehcache.org/ehcache.xsd\">");
+		sb.append("<config xmlns=\"http://www.ehcache.org/v3\" ");
+		sb.append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
+		sb.append("xsi:schemaLocation=\"http://www.ehcache.org/v3 ");
+		sb.append("https://www.ehcache.org/schema/ehcache-core-3.10.xsd\">");
 
 		for (int i = 1; i <= cacheEntries; i++) {
 			for (String cacheName : cacheNames) {
-				sb.append("<cache maxElementsInMemory=\"");
-				sb.append(maxElementsInMemory);
-				sb.append("\" name=\"");
+				sb.append("<cache alias=\"");
 				sb.append(cacheName + i);
-				sb.append("\" timeToIdleSeconds=\"");
+				sb.append("\"><expiry><tti>");
 				sb.append(timeToIdleSeconds);
-				sb.append("\"> </cache>");
+				sb.append("</tti></expiry><heap>");
+				sb.append(maxHeapEntries);
+				sb.append("</heap></cache>");
 			}
 		}
 
-		sb.append("\" </ehcache>");
+		sb.append("</config>");
 
 		return sb.toString();
 	}
