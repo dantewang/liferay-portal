65d2d08118118c3e219220cd7085627079633a1d
Dante Wang <dante.wang@liferay.com>
2025-04-15 15:49:06 +0800

    LPD-48723 SF, simplify

--- modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/ArithmeticEntryTest.java
+++ modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/ArithmeticEntryTest.java
@@ -9,6 +9,7 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.sql.dsl.DSLFunctionFactoryUtil;
 import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.test.rule.Inject;
@@ -18,11 +19,10 @@ import com.liferay.portal.tools.service.builder.test.model.ArithmeticEntry;
 import com.liferay.portal.tools.service.builder.test.model.ArithmeticEntryTable;
 import com.liferay.portal.tools.service.builder.test.service.persistence.ArithmeticEntryPersistence;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -44,79 +44,45 @@ public class ArithmeticEntryTest {
 				Propagation.REQUIRED,
 				"com.liferay.portal.tools.service.builder.test.service"));
 
-	@After
-	public void tearDown() throws Exception {
-		for (ArithmeticEntry arithmeticEntry : _arithmeticEntries) {
-			_arithmeticEntryPersistence.remove(arithmeticEntry);
-		}
-
-		_arithmeticEntries.clear();
-	}
-
 	@Test
 	public void testDivideByDecimal() {
-		ArithmeticEntry arithmeticEntry = _arithmeticEntryPersistence.create(
-			RandomTestUtil.nextLong());
-
-		arithmeticEntry.setNumber1(3L);
-		arithmeticEntry.setNumber2(2L);
-
-		_arithmeticEntries.add(
-			_arithmeticEntryPersistence.update(arithmeticEntry));
-
-		Assert.assertEquals(
-			Collections.singletonList(1.5),
-			_arithmeticEntryPersistence.dslQuery(
-				DSLQueryFactoryUtil.select(
-					DSLFunctionFactoryUtil.divide(
-						ArithmeticEntryTable.INSTANCE.number1,
-						ArithmeticEntryTable.INSTANCE.number2
-					).as(
-						"alias", Double.class
-					)
-				).from(
-					ArithmeticEntryTable.INSTANCE
-				)));
+		_testDivide(
+			arithmeticEntry -> {
+				arithmeticEntry.setNumber1(3L);
+				arithmeticEntry.setNumber2(2L);
+			},
+			Collections.singletonList(1.5));
 	}
 
 	@Test
 	public void testDivideByNull() {
-		ArithmeticEntry arithmeticEntry = _arithmeticEntryPersistence.create(
-			RandomTestUtil.nextLong());
-
-		arithmeticEntry.setNumber1(3L);
-
-		_arithmeticEntries.add(
-			_arithmeticEntryPersistence.update(arithmeticEntry));
-
-		Assert.assertEquals(
-			Collections.singletonList(null),
-			_arithmeticEntryPersistence.dslQuery(
-				DSLQueryFactoryUtil.select(
-					DSLFunctionFactoryUtil.divide(
-						ArithmeticEntryTable.INSTANCE.number1,
-						ArithmeticEntryTable.INSTANCE.number2
-					).as(
-						"alias", Double.class
-					)
-				).from(
-					ArithmeticEntryTable.INSTANCE
-				)));
+		_testDivide(
+			arithmeticEntry -> arithmeticEntry.setNumber1(3L),
+			Collections.singletonList(null));
 	}
 
 	@Test
 	public void testDivideByZero() {
-		ArithmeticEntry arithmeticEntry = _arithmeticEntryPersistence.create(
+		_testDivide(
+			arithmeticEntry -> {
+				arithmeticEntry.setNumber1(3L);
+				arithmeticEntry.setNumber2(0L);
+			},
+			Collections.singletonList(null));
+	}
+
+	private void _testDivide(
+		Consumer<ArithmeticEntry> consumer, List<Double> expectResults) {
+
+		_arithmeticEntry = _arithmeticEntryPersistence.create(
 			RandomTestUtil.nextLong());
 
-		arithmeticEntry.setNumber1(3L);
-		arithmeticEntry.setNumber2(0L);
+		consumer.accept(_arithmeticEntry);
 
-		_arithmeticEntries.add(
-			_arithmeticEntryPersistence.update(arithmeticEntry));
+		_arithmeticEntry = _arithmeticEntryPersistence.update(_arithmeticEntry);
 
 		Assert.assertEquals(
-			Collections.singletonList(null),
+			expectResults,
 			_arithmeticEntryPersistence.dslQuery(
 				DSLQueryFactoryUtil.select(
 					DSLFunctionFactoryUtil.divide(
@@ -130,7 +96,8 @@ public class ArithmeticEntryTest {
 				)));
 	}
 
-	private final List<ArithmeticEntry> _arithmeticEntries = new ArrayList<>();
+	@DeleteAfterTestRun
+	private ArithmeticEntry _arithmeticEntry;
 
 	@Inject
 	private ArithmeticEntryPersistence _arithmeticEntryPersistence;
