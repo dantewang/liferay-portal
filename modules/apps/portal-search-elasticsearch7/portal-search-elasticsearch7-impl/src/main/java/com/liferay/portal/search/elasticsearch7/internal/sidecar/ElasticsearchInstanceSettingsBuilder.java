68aac346dbd44f8bbdb0f4b2a8dc2838d43264bc
Tina Tian <tina.tian@liferay.com>
2025-09-12 09:56:28 -0700

    LPD-59097 Remove unused code

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/ElasticsearchInstanceSettingsBuilder.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/ElasticsearchInstanceSettingsBuilder.java
@@ -12,12 +12,9 @@ import com.liferay.portal.search.elasticsearch7.internal.settings.SettingsHelper
 import com.liferay.portal.search.elasticsearch7.internal.sidecar.constants.SidecarConstants;
 import com.liferay.portal.search.elasticsearch7.internal.util.ResourceUtil;
 
-import java.net.InetAddress;
-
 import java.nio.file.Path;
 
 import java.util.List;
-import java.util.function.Supplier;
 
 import org.elasticsearch.common.settings.Settings;
 
@@ -36,26 +33,10 @@ public class ElasticsearchInstanceSettingsBuilder {
 		return _settingsHelperImpl.build();
 	}
 
-	public ElasticsearchInstanceSettingsBuilder clusterInitialMasterNodes(
-		String clusterInitialMasterNodes) {
-
-		_clusterInitialMasterNodes = clusterInitialMasterNodes;
-
-		return this;
-	}
-
 	public ElasticsearchInstanceSettingsBuilder clusterName(
 		String clusterName) {
 
 		_clusterName = clusterName;
-
-		return this;
-	}
-
-	public ElasticsearchInstanceSettingsBuilder discoverySeedHosts(
-		String discoverySeedHosts) {
-
-		_discoverySeedHosts = discoverySeedHosts;
 
 		return this;
 	}
@@ -86,22 +67,10 @@ public class ElasticsearchInstanceSettingsBuilder {
 		return this;
 	}
 
-	public ElasticsearchInstanceSettingsBuilder networkHost(
-		String networkHost) {
-
-		_networkHost = networkHost;
-
-		return this;
-	}
-
 	public ElasticsearchInstanceSettingsBuilder nodeName(String nodeName) {
 		_nodeName = nodeName;
 
 		return this;
-	}
-
-	public interface LocalBindInetAddressSupplier
-		extends Supplier<InetAddress> {
 	}
 
 	protected Path getHomePath() {
@@ -139,14 +108,6 @@ public class ElasticsearchInstanceSettingsBuilder {
 	private void _configureClustering() {
 		put("cluster.name", _clusterName);
 		put("cluster.routing.allocation.disk.threshold_enabled", false);
-
-		if (!Validator.isBlank(_clusterInitialMasterNodes)) {
-			put("cluster.initial_master_nodes", _clusterInitialMasterNodes);
-		}
-
-		if (!Validator.isBlank(_discoverySeedHosts)) {
-			put("discovery.seed_hosts", _discoverySeedHosts);
-		}
 
 		if (_discoveryTypeSingleNode) {
 			put("discovery.type", "single-node");
@@ -191,10 +152,7 @@ public class ElasticsearchInstanceSettingsBuilder {
 			put("network.bind_host", networkBindHost);
 		}
 
-		if (!Validator.isBlank(_networkHost)) {
-			put("network.host", _networkHost);
-		}
-		else if (Validator.isNotNull(networkHost)) {
+		if (Validator.isNotNull(networkHost)) {
 			put("network.host", networkHost);
 		}
 
@@ -263,14 +221,11 @@ public class ElasticsearchInstanceSettingsBuilder {
 		put("node.store.allow_mmap", false);
 	}
 
-	private String _clusterInitialMasterNodes;
 	private String _clusterName;
-	private String _discoverySeedHosts;
 	private boolean _discoveryTypeSingleNode;
 	private ElasticsearchConfigurationWrapper
 		_elasticsearchConfigurationWrapper;
 	private ElasticsearchInstancePaths _elasticsearchInstancePaths;
-	private String _networkHost;
 	private String _nodeName;
 	private final SettingsHelperImpl _settingsHelperImpl =
 		new SettingsHelperImpl(Settings.builder());
