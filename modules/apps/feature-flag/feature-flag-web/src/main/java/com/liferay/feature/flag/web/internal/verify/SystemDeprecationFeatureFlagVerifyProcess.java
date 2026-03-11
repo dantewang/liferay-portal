224f766e220dd2ddef8beaeb7e4a500ce490fd66
Thiago Buarque <thiago.buarque@liferay.com>
2026-03-11 00:35:12 -0300

    LPD-80539 process system-wide deprecation feature flags once

---
+++ modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/verify/SystemDeprecationFeatureFlagVerifyProcess.java
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.feature.flag.web.internal.verify;
+
+import com.liferay.feature.flag.web.internal.DeprecationFeatureFlagHelper;
+import com.liferay.feature.flag.web.internal.feature.flag.FeatureFlagsBagProvider;
+import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.service.PortalPreferencesLocalService;
+import com.liferay.portal.verify.VerifyProcess;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Thiago Buarque
+ */
+@Component(property = "initial.deployment=true", service = VerifyProcess.class)
+public class SystemDeprecationFeatureFlagVerifyProcess extends VerifyProcess {
+
+	@Override
+	protected void doVerify() throws Exception {
+		DeprecationFeatureFlagHelper deprecationFeatureFlagHelper =
+			new DeprecationFeatureFlagHelper(
+				_featureFlagsBagProvider, _portalPreferencesLocalService);
+
+		deprecationFeatureFlagHelper.processDeprecationFeatureFlags(
+			CompanyConstants.SYSTEM);
+	}
+
+	@Reference
+	private FeatureFlagsBagProvider _featureFlagsBagProvider;
+
+	@Reference
+	private PortalPreferencesLocalService _portalPreferencesLocalService;
+
+}
